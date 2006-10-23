35db201e720fd3dc0c511f85f522986ba7453003
mdawaffe <mdawaffe@330c2647-7620-0410-81c2-8fc9ee8a0def>
2006-10-23 22:34:58 +0000

    Version typo

--- display-name/tags/0.7/display-name.php
+++ display-name/tags/0.7/display-name.php
@@ -5,7 +5,7 @@ Plugin URI: http://trac.bbpress.org/ticket/430
 Description: Uses the display name as set by WordPress for a bbPress moderator's name instead of the login name.
 Author: Michael D Adams
 Author URI: http://blogwaffe.com/
-Version: 0.7;
+Version: 0.7
 
 Requires at least: 0.72
 Tested up to: 0.73

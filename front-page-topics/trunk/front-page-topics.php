7e4169a0879d975079b25a890fd10d84147f0384
mdawaffe <mdawaffe@330c2647-7620-0410-81c2-8fc9ee8a0def>
2006-10-24 23:48:07 +0000

    test the browser with new trunk

--- front-page-topics/trunk/front-page-topics.php
+++ front-page-topics/trunk/front-page-topics.php
@@ -5,7 +5,7 @@ Plugin URI: http://bbpress.org/forums/topic/65#post-333
 Description: Changes the number of topics displafed on the front page only.
 Author: Michael D Adams
 Author URI: http://blogwaffe.com/
-Version: 0.7
+Version: 0.7.1
 
 Requires at least: 0.72
 Tested up to: 0.73
@@ -14,7 +14,7 @@ Tested up to: 0.73
 
 function front_page_topics() {
 	global $bb;
-	$bb->page_topics = 2;
+	$bb->page_topics = 3;
 }
 
 // This is what changes it only on the front page.

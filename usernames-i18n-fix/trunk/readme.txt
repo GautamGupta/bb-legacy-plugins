c8d29534c8f9c6b5f16b7aa086141c6913c68841
Sewar <Sewar@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-01-11 06:22:24 +0000

    First version

---
+++ usernames-i18n-fix/trunk/readme.txt
@@ -0,0 +1,14 @@
+=== Usernames I18N Fix ===
+Contributors: Sewar
+Tags: username, I18N
+Requires at least: 0.73
+Tested up to: 0.74
+Stable tag: 0.0
+
+Allow users to use I18N non-English  names.
+
+== Description ==
+
+== Installation ==
+
+Add `usernames_i18n_fix.php` to your `/my-plugins/` directory.
---
+++ usernames-i18n-fix/trunk/usernames_i18n_fix.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Usernames I18N Fix
+Plugin URI: http://sewar.wordpress.com/bbpress/plugins/usernames-i18n-fix/
+Description: Allow users to use I18N non-English  names.
+Author: Sewar
+Author URI: http://sewar.wordpress.com
+Version: 1.0
+*/
+
+/*
+    Usernames I18N Fix, bbPress plugin allows users to use I18N non-English names.
+    Copyright (C) 2006  Sewar
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+function user_sanitize_i18n_fix($text, $raw, $strict){
+	$new_text = $raw;
+
+	if ( $strict )
+		$new_text = preg_replace('|-+|', '-', $new_text);
+
+	return $new_text;
+}
+
+add_filter('user_sanitize', 'user_sanitize_i18n_fix', 10, 3);
+
+?>

3d06e59fedadaf8e38c0275b19768c0d3dde5373
ardentfrost <ardentfrost@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-01-12 08:44:59 +0000

    Added items remotely
    C:\my website\Plugins\memberlist\bb-memberlist.php
    C:\my website\Plugins\memberlist\memberlist.php
    C:\my website\Plugins\memberlist\mlist.php
    C:\my website\Plugins\memberlist\README.txt

---
+++ memberlist/tags/mlist.php
@@ -0,0 +1,34 @@
+<?php
+require_once('./bb-load.php');
+
+bb_mlist_delete_users();
+
+if ( isset($_GET['usercount']) )
+	$usercount = $_GET['usercount'];
+else
+	$usercount = 10;
+
+if ( isset($_GET['orderby']) ) :
+	$order = $_GET['orderby'];
+else :
+	$order = 'ID';
+endif;
+
+if ( isset($_GET['page']) && ($_GET['page'] <= ceil(bb_mlist_count_users() / $usercount) ) ) :
+	$currentpage = $_GET['page'];
+elseif ( !isset($_GET['page']) ) :
+	$currentpage = 1;
+else :
+	$currentpage = 0;
+endif;
+
+$memberresult = get_memberlist($order, $currentpage, $usercount);
+
+if ( file_exists(BBPATH . 'my-templates/memberlist.php') ) {
+	require( BBPATH . 'my-templates/memberlist.php' );
+} else {
+	echo "Oh Snap! Your memberlist got F'd in the A";
+}
+
+?>
+

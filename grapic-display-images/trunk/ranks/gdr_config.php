270e3de4298810b64caee4fe32d28e8f546fe38d
actorjiml <actorjiml@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-01-27 00:41:42 +0000

    Forgot to include php container and change extension of default graphics to png

--- grapic-display-images/trunk/ranks/gdr_config.php
+++ grapic-display-images/trunk/ranks/gdr_config.php
@@ -1,3 +1,5 @@
+<?php 
+
 /*YOU CAN CONFIGURE THESE VARIABLES */
 
 $num_ranks=6; //Should be equal (or less) than the number of ranks you are going to display and have images for.
@@ -12,14 +14,15 @@ $rank_max[4]=51;
 $rank_max[5]=101;
 
 $rank_img=array();
-$rank_img[1]="metalic_rank1.gif";
-$rank_img[2]="metalic_rank2.gif";
-$rank_img[3]="metalic_rank3.gif";
-$rank_img[4]="metalic_rank4.gif";
-$rank_img[5]="metalic_rank5.gif";
-$rank_img[6]="metalic_rank6.gif";
+$rank_img[1]="metalic_rank1.png";
+$rank_img[2]="metalic_rank2.png";
+$rank_img[3]="metalic_rank3.png";
+$rank_img[4]="metalic_rank4.png";
+$rank_img[5]="metalic_rank5.png";
+$rank_img[6]="metalic_rank6.png";
 
-$image_for_special_rank_1="metalic_admin.gif";  // Image used for Key Master when use_special_rank is set to '1'
-$image_for_special_rank_2="metalic_modo.gif"; // Image used for Moderators when use_special_rank is set to '1'
+$image_for_special_rank_1="metalic_admin.png";  // Image used for Key Master when use_special_rank is set to '1'
+$image_for_special_rank_2="metalic_modo.png"; // Image used for Moderators when use_special_rank is set to '1'
 
 /*END OF CONFIGURABLE VARIABLES */
+?>

910116de3320972a3a1a53c4d042e707875402ae
SamBauers <SamBauers@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-02-13 16:00:26 +0000

    visual-support-forums: tagged version 1.0.1

---
+++ visual-support-forums-for-bbpress/tags/1.0.1/visual-support-forum.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Visual Support Forums
+Plugin URI: http://www.network.net.au/bbpress/plugins/support-forum/visual-support-forum.latest.zip
+Description: This plugin adds some icons before topic names to indicate support/closed status, requires the Support Forums plugin by Aditya Naik.
+Author: Sam Bauers
+Version: 1.0.1
+Author URI: http://www.network.net.au/
+
+Version History:
+1.0		: Initial Release
+1.0.1	: Merged with the Support Forum plugin version 1.2
+*/
+?>

6a372c4b41d56064a31d25205a73fc58bf66b514
louisedade <louisedade@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-05-02 14:44:53 +0000

    avatar-upload: committing version 0.3 with lots of changes

--- avatar-upload/trunk/avatar-upload.php
+++ avatar-upload/trunk/avatar-upload.php
@@ -1,48 +1,15 @@
 <?php
 /*
 Plugin Name: Avatar Upload
-Plugin URI: http://www.classical-webdesigns.co.uk/articles/43_bbpress-plugin-avatar-upload.html
-Version: 0.2
-Description: Allows users to upload an avatar (gif, jpeg/jpg or png) image to bbPress. Admins can configure maximum allowed file size and image dimensions.
+Plugin URI: http://bbpress.org/plugins/topic/46
+Version: 0.3
+Description: Allows users to upload an avatar (gif, jpeg/jpg or png) image to bbPress.
 Author: Louise Dade
 Author URI: http://www.classical-webdesigns.co.uk/
 */
 
 require_once('./bb-load.php'); // load bbPress config 
 bb_auth(); // logged in?
-
-/* --- CONFIGURATION VARIABLES (you can edit these) --- */
-
-// Avatar folder location (default is 'avatars' in the bbPress root, 'BBPATH')
-// You must create the 'avatars' folder before you install this plugin.
-$avatar_dir = BBPATH . "avatars/"; // remember to include trailing slash
-
-// Define maximum values allowed
-$max_width = 150; // pixels
-$max_height = 150; // pixels
-$max_bytes = 51200; // default is 50 KB (51200 bytes)
-
-// Allowed file extensions (*.gif, *.jpeg, *.jpg, *.png)
-$allowed_extns = array("gif", "jpg", "jpeg", "png");
-
-// Content types -- more stringent check on file types.
-// Eg. a file with a '.png' extension MUST have an 'image/png' content-type!
-// You probably want to leave this alone.
-$allowed_types = array('gif'=>"image/gif", 'jpeg'=>"image/jpeg", 'jpg'=>"image/jpeg", 'png'=>"image/png");
-
-/* --- STOP EDITING --- */
-
-/* --- Start bbPress Stuff --- */
-
-// If the script filename is not correct.
-$file = '';
-foreach ( array($_SERVER['PHP_SELF'], $_SERVER['SCRIPT_FILENAME'], $_SERVER['SCRIPT_NAME']) as $name ) {
-	if ( false !== strpos($name, '.php') ) {$file = $name;}
-}
-if (bb_find_filename($file) != 'avatar-upload.php') { // not the correct form!
-	$sendto = bb_get_option('uri');
-	wp_redirect( $sendto );
-}
 
 // Grab user id
 if ( isset($_GET['id']) ) {
@@ -51,33 +18,41 @@ if ( isset($_GET['id']) ) {
 	$user_id = intval( 0 );
 }
 
+// This user may NOT be the user who's avatar is being uploaded,
+// this allows an Admin/Moderator to update another user's avatar
+// (in the event that the user's avatar is objectionable!)
+$current_user = bb_get_user(bb_get_current_user_info('id'));
+
+// User who's profile is being updated
 $user = bb_get_user( $user_id ); // user info
 
+// No user found with that ID
 if ( !$user ) {
 	bb_die(__('User not found.')); // no user found
 }
 
-// Do not let users upload an avatar if user is a bozo OR user id not equal to current user's id
-// (AND user is not allowed to moderate).
-if ( $user->is_bozo || ($user->ID != bb_get_current_user_info( 'id' ) && !bb_current_user_can( 'moderate' )) )
+// Only allow the correct User or an Admin/Moderator to upload
+// but not if they are a bozo!
+if ( ($user->ID != $current_user->ID && !bb_current_user_can( 'moderate' )) || $current_user->is_bozo )
 {
 	bb_die(__('You do not have permission to upload an avatar for this user.'));
 }
-/* --- End bbPress Stuff --- */
 
-
-$max_kbytes = round($max_bytes/1024, 2); // Just a pretty value for output use
+// Get config variables
+$av_opts = avatarupload_config();
+$av_opts['mime_types']['jpeg'] = $av_opts['mime_types']['jpg'];
+$av_opts['max_kbytes'] = round($av_opts['max_bytes']/1024, 2); // Just a pretty value for output use
 
 // Some potential error messages in human readable form
 $errorcodes = array(
 	"- no error (this message will never be shown) -",
-	"The image file is too big, the maximum file size allowed is $max_kbytes KB.",
-	"The image file is too big, the maximum file size allowed is $max_kbytes KB.",
+	"The image file is too big, the maximum file size allowed is {$av_opts['max_kbytes']} KB.",
+	"The image file is too big, the maximum file size allowed is {$av_opts['max_kbytes']} KB.",
 	"The file was only partially uploaded - the connection may have been interrupted.",
 	"The image file does not appear to have been uploaded - did you select an image?",
 	"The file does not appear to be a valid GIF, JPG/JPEG or PNG image type.",
 	"The image file could not be saved to the avatars folder.",
-	"Image dimensions must not be greater than $max_width x $max_height pixels.",
+	"Image dimensions must not be greater than {$av_opts['max_width']} x {$av_opts['max_height']} pixels.",
 	"The avatar filename may only contain upper/lower case letters, numbers, underscores or dashes."
 );
 
@@ -85,7 +60,7 @@ $errorcodes = array(
 
 if (!empty($_FILES['p_browse']))
 {
-	$current_avatar = explode("|", $user->avatar_file); // for comparison later
+	$current_avatar = avatarupload_get_avatar($user_id, 0, 1); // for comparison later
 
 	$img_errs = 0;
 	$error = 0;
@@ -115,20 +90,15 @@ if (!empty($_FILES['p_browse']))
 		$error++;
 	}
 
-	if ($error == 0 && ($img_errs == 1 || $img_errs == 2)  || $img_size > $max_bytes) { 
+	if ($error == 0 && ($img_errs == 1 || $img_errs == 2)  || $img_size > $av_opts['max_bytes']) { 
 		// File size exceeds max_bytes
 		$img_errs = 1;
 		$error++;
 	}
 
-	if ($error == 0 && (!in_array($img_type, $allowed_types) || !in_array($img_ext, $allowed_extns)) ) {
-		// file extension and file type check #1 - simple check to weed out bad filenames
-		$img_errs = 5;
-		$error++;
-	}
-
-	if ($error == 0 &&  $allowed_types[$img_ext] != $img_type) {
-		// file extension and file type check #2 - ok, they are in the list, but to do match?
+	if ($error == 0 && (!in_array($img_type, $av_opts['mime_types'][$img_ext]) || 
+			!in_array($img_ext, $av_opts['file_extns'])) ) {
+		// Check for invalid and/or mismatched mime-type and file extensions
 		$img_errs = 5;
 		$error++;
 	}
@@ -150,13 +120,14 @@ if (!empty($_FILES['p_browse']))
 		$img_w = $dims[0];
 		$img_h = $dims[1];
 
-		if ($img_w > $max_width || $img_h > $max_height) { // File dims greater than max_width/max_height
+		if ($img_w > $av_opts['max_width'] || $img_h > $av_opts['max_height']) {
+			// File dims greater than max_width/max_height
 			$img_errs = 7;
 			$error++;
 		}
 	}
 
-	if ($error == 0 && !move_uploaded_file($img_temp, $avatar_dir . $user_filename))
+	if ($error == 0 && !move_uploaded_file($img_temp, BBPATH . $av_opts['avatar_dir'] . $user_filename))
 	{ // Can save to avatars folder (does it exist?)
 		$img_errs = 6;
 		$error++;
@@ -168,24 +139,14 @@ if (!empty($_FILES['p_browse']))
 		if (!empty($current_avatar[0]) && $user_filename != $current_avatar[0])
 		{	// compare 'new' and 'current' avatar filenames - if different, delete 'current'
 			// this will most likely only happen when the new avatar has a different extension
-			unlink(BBPATH. "avatars/" . $current_avatar[0]);
+			unlink(BBPATH . $av_opts['avatar_dir'] . $current_avatar[0]);
 		}
 
-		$meta_avatar = $user_filename . "|" . $img_w . "|" . $img_h;
+		$meta_avatar = $user_filename . "|" . $img_w . "|" . $img_h . "|avatar-upload";
 		bb_update_usermeta( $user_id, 'avatar_file', $meta_avatar );
 		$success_message = "Your avatar has been uploaded.";
 	}
 }
 
-// this array will be used in the template
-$img_requirements = array(
-	'img_types' => $allowed_extns,
-	'max_width' => $max_width,
-	'max_height' => $max_height,
-	'max_bytes' => $max_bytes,
-	'max_kbytes' => $max_kbytes,
-	'img_ct' => $allowed_types
-);
-
-bb_load_template( 'avatar.php', array('success_message', 'img_requirements') );
+bb_load_template( 'avatar.php', array('success_message', 'av_opts') );
 ?>
--- avatar-upload/trunk/my-templates/avatar.php
+++ avatar-upload/trunk/my-templates/avatar.php
@@ -1,32 +1,32 @@
 <?php bb_get_header(); ?>
 
-<h3 id="breadcrumb"><a href="<?php bb_option('uri'); ?>"><?php bb_option('name'); ?></a> &raquo; <?php _e('Upload Avatar'); ?></h3>
+<h3 class="bbcrumb"><a href="<?php bb_option('uri'); ?>"><?php bb_option('name'); ?></a> &raquo; <?php _e('Upload Avatar'); ?></h3>
 
 <?php if (bb_current_user_can('edit_user', $user->ID)) { ?>
 
 <?php
-	echo (!empty($success_message)) ? $success_message : "";
+	echo (!empty($success_message)) ? '<div class="notice">'.__($success_message).'</div>' : "";
 ?>
 
 <h2 id="userlogin"><?php echo get_user_name( $user->ID ); ?></h2>
 
 <ul>
-<li>The following image formats are allowed: <strong><?php echo implode($img_requirements['img_types'], ", "); ?></strong>.</li>
-<li>Dimensions must be no greater than <strong><?php echo $img_requirements['max_width']; ?> x <?php echo $img_requirements['max_height']; ?> pixels</strong> (your image does not have to be square).</li>
-<li>File size must be no greater than <strong><?php echo $img_requirements['max_kbytes']; ?> <abbr title="kilobytes">KB</abbr></strong></li>
-<li>File names must be <strong>alpha-numeric</strong> and may contain <strong>underscores or dashes</strong> (a-z/A-Z, 0-9, _ or -).</li>
+<li><?php _e('The following image formats are allowed: <strong>' . implode($av_opts['file_extns'], ", ") . '</strong>.'); ?></li>
+<li><?php _e('Dimensions must be no greater than <strong>' .$av_opts['max_width']. ' x ' .$av_opts['max_height']. ' pixels</strong> (your image does not have to be square).'); ?></li>
+<li><?php _e('File size must be no greater than <strong>' . $av_opts['max_kbytes'] . '<abbr title="kilobytes">KB</abbr></strong>'); ?></li>
+<li><?php _e('File names must be <strong>alpha-numeric</strong> and may contain <strong>underscores or dashes</strong> (a-z/A-Z, 0-9, _ or -).'); ?></li>
 </ul>
 
-<form enctype="multipart/form-data" method="POST" action="<?php echo bb_get_option('uri') . 'avatar-upload.php?id=' . $user->ID; ?>">
-<p><label for="p_browse">Locate Image:</label><br />
-<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $img_requirements['max_bytes']; ?>" />
+<form enctype="multipart/form-data" method="POST" action="<?php profile_tab_link($user->ID, 'avatar'); ?>">
+<p><label for="p_browse"><?php _e('Locate Image'); ?>:</label><br />
+<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $av_opts['max_bytes']; ?>" />
 <input type="file" name="p_browse" id="p_browse" size="80" /></p>
 
-<p><input type="submit" name="submit" id="submit" value="Upload Avatar" /></p>
+<p><input type="submit" name="submit" id="submit" value="<?php _e('Upload Avatar'); ?>" /></p>
 </form>
 
-<h3>Your Current Avatar</h3>
-<p><?php echo display_avatar($user->ID, 'new'); ?></p>
+<h3><?php _e('Your Current Avatar'); ?></h3>
+<p><?php echo avatarupload_display($user->ID, 'new'); ?></p>
 
 <?php } ?>
 

fdcb8d25cab21d643a54b1d03c42fa2379a35a16
so1o <so1o@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-07-16 01:53:42 +0000

    Version 4.0   * Added Restriction by User Role   * Renamed Functions to be prefixed by private_forums instead of forum_access   * Changed where the options are stored.   * Added Upgrade Function   * Options can now be set by role administrator

--- private-forums/trunk/private-forums.php
+++ private-forums/trunk/private-forums.php
@@ -4,146 +4,181 @@ Plugin Name: Private Forums
 Plugin URI: http://www.adityanaik.com/projects/plugins/bb-private-forums/
 Description: Regulate Access to forums in bbPress
 Author: Aditya Naik
-Version: 3.0.1
+Version: 4.0
 Author URI: http://www.adityanaik.com/
 
 Version History:
-1.0 	: Initial Release
-1.1 	: bug fix for empty private forums
-		: Added failsafe for installation.
-2.0 	: Added choice to hide private forums or show them with private prefix
-		: Added selectable prefix text
-		: Removed redundant forum_access_update_option
+1.0		: Initial Release
+1.1		: bug fix for empty private forums
+			: Added failsafe for installation.
+2.0		: Added choice to hide private forums or show them with private prefix
+			: Added selectable prefix text
+			: Removed redundant forum_access_update_option
 2.1		: Created Common Submit for all options
 3.0		: Fixed the submenu generation
 			: Fixed Forum Filter
-3.1 	:	Fixed <?
+3.1		: Fixed <?
+4.0		: Added Restriction by User Role
+			: Renamed Functions to be prefixed by private_forums instead of forum_access
+			: Changed where the options are stored.
+			: Added Upgrade Function
+			: Options can now be set by role administrator
 */
 
-add_action( 'bb_admin_menu_generator', 'forum_access_add_admin_page' );
-add_action( 'bb_init', 'forum_access_check_private_forum');
-add_action( 'init', 'forum_access_check_private_forum');
+private_forums_upgrade();
+add_action( 'bb_admin_menu_generator', 'private_forums_add_admin_page' );
+add_action( 'bb_init', 'private_forums_initialize_filters');
+add_action( 'init', 'private_forums_initialize_filters');
 
-function forum_access_add_admin_page() {
-	bb_admin_add_submenu(__('Private Forums'), 'use_keys', 'forum_access_admin_page');
+function private_forums_add_admin_page() {
+	bb_admin_add_submenu(__('Private Forums'), 'administrate', 'private_forums_admin_page');
 
 }
 
-function forum_access_get_option($option) {
+function private_forums_custom_get_options($option) {
+	global $private_forums_options;
+	if(!isset($private_forums_options)) 
+		$private_forums_options = bb_get_option('private_forums_options');
+	return $private_forums_options[$option];
+}
+
+function private_forums_upgrade() {
+	$private_forums_options = bb_get_option('private_forums_options');
+	if(!isset($private_forums_options['version']) || $private_forums_options['version'] < 40) 
+		private_forums_upgrade_4_0();		
+}
+
+function private_forums_upgrade_4_0() {
+	$private_forums_options = array();
+	
+	$forum_access_private_forums = bb_get_option('forum_access_private_forums');
+	$private_forums_options['private_forums'] = ($forum_access_private_forums) ? $forum_access_private_forums : array() ;
+	$private_forums_options['failure_msg'] = bb_get_option('forum_access_failure_msg');
+	$private_forums_options['hide_show_flag'] = bb_get_option('forum_access_private_forums_option');
+	$private_forums_options['user_role'] = "MEMBER";
+	$private_forums_options['private_text'] = bb_get_option('forum_access_private_text');
+	$private_forums_options['version'] = 40;
+	
+	bb_update_option('private_forums_options', $private_forums_options);
+}
+
+function private_forums_get_option($option) {
 	return (bb_get_option($option)) ? bb_get_option($option) : array() ;
 }
 
-function forum_access_admin_page() {
+function private_forums_admin_page() {
 	$forums = get_forums();
-	$priv_forums = forum_access_get_option('forum_access_private_forums');
+	$private_forums = private_forums_custom_get_options('private_forums');
 	?>
 		<h2>Private Forums</h2>
-		<form method="post">
+		<form method="post" name="private_forum_form" id="private_forum_form">
+			<p>
+				<label for="user_role">Forums Restricted to Role</label>
+				<select name="user_role" id="user_role">
+					<option value="MEMBER" <?php echo (private_forums_custom_get_options('user_role') == 'MEMBER') ? 'selected' :'' ;?>>Registered Members</option>
+					<option value="MODERATOR" <?php echo (private_forums_custom_get_options('user_role') == 'MODERATOR') ? 'selected' :'' ;?>>Moderators</option>
+					<option value="ADMINSTRATOR" <?php echo (private_forums_custom_get_options('user_role') == 'ADMINSTRATOR') ? 'selected' :'' ;?>>Adminstrators</option>
+				</select>
+			</p>
 			<?php
 			foreach($forums as $forum) {
 				?>
-					<p><input type="checkbox" name="priv_forum[<?php echo $forum->forum_id; ?>]" <?php echo 'value="' . $forum->forum_id . '" ' . ((array_key_exists($forum->forum_id,($priv_forums) ? $priv_forums : array())) ? ' checked ' : '') ; ?> > <?php echo $forum->forum_name; ?> </p>
+					<p>
+						<input type="checkbox" id="private_forums_<?php echo $forum->forum_id; ?>" 
+							name = "private_forums[<?php echo $forum->forum_id; ?>]" 
+							value = "Y"
+							<?php echo (array_key_exists($forum->forum_id,$private_forums) ? ' checked ' : '') ; ?> > 
+						<label for="private_forums_<?php echo $forum->forum_id; ?>"><?php echo $forum->forum_name; ?></label>
+					</p>
 				<?php
 			}
 			?>
-			<p><input type="hidden" name="action" value="priv_forum_update">
-			<input type="submit" name="submit" value="Submit"></p>
+			<p class="submit"><input type="submit" name="submit" value="Submit"></p>
 		<h2>Privacy Options</h2>
 		<?php
 			$checked = "checked='checked'";
-			if ( 'SHOW_PRIVATE' == bb_get_option('forum_access_private_forums_option')) {
+			if ( 'SHOW_PRIVATE' == private_forums_custom_get_options('hide_show_flag')) {
 				$sp = $checked;
 		 	} else {
 		 		$h = $checked;
 		 	}
 		?>
 			<p>
-				<input type="radio" name="priv_forum_option" value="HIDE" <?php echo $h; ?>/> Hide Private Forums and Posts
+				<input type="radio" id="hide_show_flag_1" name="hide_show_flag" value="HIDE" <?php echo $h; ?>/> <label for="hide_show_flag_1">Hide Private Forums and Topics</label>
 			</p>
 			<p>
-				<input type="radio" name="priv_forum_option" value="SHOW_PRIVATE" <?php echo $sp; ?>/>
-				Mark Private Forums and Posts with
-				<input type="text" onfocus="document.forms[1].priv_forum_option[1].checked=true"
-				name="priv_forum_private_text"
-				value="<?php $text = bb_get_option('forum_access_private_text');
-				if (empty($text) && 'SHOW_PRIVATE' == bb_get_option('forum_access_private_forums_option')) {
+				<input type="radio" id="hide_show_flag_2" name="hide_show_flag" value="SHOW_PRIVATE" <?php echo $sp; ?>/>
+				<label for="hide_show_flag_2">Mark Private Forums and Topics with</label>
+				<input type="text" onfocus="document.private_forum_form.hide_show_flag[1].checked=true"
+				name="private_text"
+				value="<?php $text = private_forums_custom_get_options('private_text');
+				if (empty($text) && 'SHOW_PRIVATE' == private_forums_custom_get_options('hide_show_flag')) {
 					echo  'private';
 				} else {
-					echo bb_get_option('forum_access_private_text');
+					echo private_forums_custom_get_options('private_text');
 				} ?>"/>
 			</p>
-			<p><input type="submit" name="submit" value="Submit"></p>
+			<p class="submit"><input type="submit" name="submit" value="Submit"></p>
 		<h2>Error Options</h2>
 
 			<p>When a user tries to access a private forum without having access
 			he will be redirected to a error page with the following text.</p>
 			<p><strong>Usage:</strong><br/>
-				<ol>
-					<li>Use '&lt;br/&gt;' for line breaks.</li>
+				<ul>
 					<li>Use '%s' for the context of the error. If the error occurs when the user tries to access a private forum, '%s' will be replaces by 'forum'.</li>
 					<li>Word 'login' will be replaced by a link to the login page.</li>
-				</ol>
+				</ul>
 			</p>
 
 
-			<textarea name="failure_msg" id="failure_msg" style="width: 98%;" rows="3" cols="50"><?php echo bb_get_option('forum_access_failure_msg'); ?></textarea>
+			<textarea name="failure_msg" id="failure_msg" style="width: 98%;" rows="3" cols="50"><?php echo private_forums_custom_get_options('failure_msg'); ?></textarea>
 
-			<p><input type="submit" name="submit" value="Submit"></p>
+			<p class="submit"><input type="submit" name="submit" value="Submit"></p>
 		</form>
 		<?php
 }
 
-function forum_access_check_private_forum() {
-	global $bb,$bb_current_user, $forum_access_failure_msg;
+function private_forums_process_post() {
+	if(isset($_POST['submit'])) {
+		global $private_forums_options;
+		$private_forums_options = bb_get_option('private_forums_options');
+		$private_forums_options['private_forums'] = (isset($_POST['private_forums'])) ? $_POST['private_forums'] : array();
+		$private_forums_options['failure_msg'] = $_POST['failure_msg'];
+		$private_forums_options['hide_show_flag'] = $_POST['hide_show_flag'];
+		if ('SHOW_PRIVATE'== $private_forums_options['hide_show_flag']) {
+			$private_forums_options['private_text'] = empty($_POST['private_text']) ? 'private' : $_POST['private_text'];
+		}
+		$private_forums_options['user_role'] = $_POST['user_role'];
+		bb_update_option('private_forums_options',$private_forums_options);
+	}
+}
 
-	add_action( 'bb_admin-header.php','forum_access_process_post');
+function private_forums_initialize_filters() {
+	global $bb,$bb_current_user;
+
+	add_action( 'bb_admin-header.php','private_forums_process_post');
 	$login_page = $bb->path . 'bb-login.php';
 
 	if ($_SERVER['PHP_SELF'] != $login_page) {
-		$priv_forums = forum_access_get_option('forum_access_private_forums');
-		if ( !bb_current_user() && !empty($priv_forums)) {
-			add_action( 'bb_forum.php_pre_db', 'forum_access_forum_init' );
-			add_action( 'bb_topic.php_pre_db', 'forum_access_topic_init' );
-			add_action( 'bb_tag-single.php', 'forum_access_tag_single_init' );
+		$private_forums = private_forums_custom_get_options('private_forums');
+		if ( !private_forums_check_user_access() && !empty($private_forums)) {
+			add_action( 'bb_forum.php_pre_db', 'private_forums_check_private_forum' );
+			add_action( 'bb_topic.php_pre_db', 'private_forums_check_private_topic' );
+			add_action( 'bb_tag-single.php', 'private_forums_check_private_tag' );
 
-			if (bb_get_option('forum_access_private_forums_option') == 'SHOW_PRIVATE') {
-				add_filter( 'get_forum_name', 'forum_access_add_private_name_to_forums', 10,2);
-				add_filter( 'get_topic_title', 'forum_access_add_private_name_to_topics', 10,2);
+			if (private_forums_custom_get_options('hide_show_flag') == 'SHOW_PRIVATE') {
+				add_filter( 'get_forum_name', 'private_forums_add_private_name_to_forums', 10,2);
+				add_filter( 'get_topic_title', 'private_forums_add_private_name_to_topics', 10,2);
 			} else {
-				add_action( 'bb_head', 'forum_access_head_init' );
-				add_filter( 'get_forums','forum_access_filter_forums');
-			}
-
-			$forum_access_failure_msg = forum_access_format_error(bb_get_option('forum_access_failure_msg'));
-
-		}
-	}
-}
-
-function forum_access_process_post() {
-	if(isset($_POST['submit'])) {
-		if ('priv_forum_update' == $_POST['action']) {
-
-			//Private Forums Update
-			$priv_forums = (isset($_POST['priv_forum'])) ? $_POST['priv_forum'] : array();
-			bb_update_option('forum_access_private_forums',$priv_forums);
-
-			//Error Message Update
-			$err_msg = $_POST['failure_msg'];
-			bb_update_option('forum_access_failure_msg',$err_msg);
-
-			//Privacy Option Update
-			bb_update_option('forum_access_private_forums_option',$_POST['priv_forum_option']);
-			if ('SHOW_PRIVATE'== bb_get_option('forum_access_private_forums_option')) {
-				$text = empty($_POST['priv_forum_private_text']) ? 'private' : $_POST['priv_forum_private_text'];
-				bb_update_option('forum_access_private_text',$text);
+				add_action( 'bb_head', 'private_forums_filter_topics_in_head' );
+				add_filter( 'get_forums','private_forums_filter_forums');
 			}
 
 		}
 	}
 }
 
-function forum_access_format_error($err_msg){
+function private_forums_format_error($err_msg){
 	global $bb;
 	if(empty($err_msg)) $err_msg = "This %s requires access to private forum. \r\nPlease login to access it.";
 	$err_msg = trim($err_msg);
@@ -157,47 +192,46 @@ function forum_access_format_error($err_msg){
 }
 
 
-function forum_access_head_init() {
-	global $forum_access_failure_msg, $forums, $topics, $super_stickies, $stickies, $topic, $topic_id;
-	$topics = forum_access_filter_topics($topics);
-	$stickies = forum_access_filter_topics($stickies);
-	$super_stickies = forum_access_filter_topics($super_stickies);
+function private_forums_filter_topics_in_head() {
+	global $forums, $topics, $super_stickies, $stickies, $topic, $topic_id;
+	$topics = private_forums_filter_topics($topics);
+	$stickies = private_forums_filter_topics($stickies);
+	$super_stickies = private_forums_filter_topics($super_stickies);
 }
 
-function forum_access_forum_init($forum_id) {
-	global $forum_access_failure_msg;
+function private_forums_check_private_forum($forum_id) {
 
-	$priv_forums = forum_access_get_option('forum_access_private_forums');
-	if(array_key_exists($forum_id,$priv_forums)) {
-		bb_die(__(sprintf($forum_access_failure_msg, 'forum')));
+	$private_forums = private_forums_custom_get_options('private_forums');
+	if(array_key_exists($forum_id,$private_forums)) {
+		bb_die(__(sprintf(private_forums_format_error(private_forums_custom_get_options('failure_msg')), 'forum')));
 	}
 }
 
-function forum_access_topic_init($topic_id) {
-	global $topic, $forum_access_failure_msg;
+function private_forums_check_private_topic($topic_id) {
+	global $topic;
 
-	$priv_forums = forum_access_get_option('forum_access_private_forums');
-	if(array_key_exists($topic->forum_id,$priv_forums)) {
-		bb_die(__(sprintf($forum_access_failure_msg, 'topic')));
+	$private_forums = private_forums_custom_get_options('private_forums');
+	if(array_key_exists($topic->forum_id,$private_forums)) {
+		bb_die(__(sprintf(private_forums_format_error(private_forums_custom_get_options('failure_msg')), 'topic')));
 	}
 }
 
-function forum_access_tag_single_init($tag_id) {
-	global $topics, $forum_access_failure_msg;
+function private_forums_check_private_tag($tag_id) {
+	global $topics;
 
-	$topics = forum_access_filter_topics($topics);
+	$topics = private_forums_filter_topics($topics);
 	if (empty($topics)) {
-		bb_die(__(sprintf($forum_access_failure_msg, 'tag')));
+		bb_die(__(sprintf(private_forums_format_error(private_forums_custom_get_options('failure_msg')), 'tag')));
 	}
 }
 
-function forum_access_filter_forums($forums) {
+function private_forums_filter_forums($forums) {
 
 	$new_forums = array();
 	if ($forums) {
-		$priv_forums = forum_access_get_option('forum_access_private_forums');
+		$private_forums = private_forums_custom_get_options('private_forums');
 		foreach($forums as $forum) {
-			if(!array_key_exists($forum->forum_id,$priv_forums)) {
+			if(!array_key_exists($forum->forum_id,$private_forums)) {
 				$new_forums[] = $forum;
 			} else {
 				//$new_forums[] = $forum;
@@ -210,33 +244,13 @@ function forum_access_filter_forums($forums) {
 
 }
 
-function forum_access_add_private_name_to_forums($text, $id) {
-	$priv_forums = forum_access_get_option('forum_access_private_forums');
-	if(array_key_exists($id,$priv_forums)) {
-		return '[' . bb_get_option('forum_access_private_text') . '] ' . $text;
-	} else {
-		return $text;
-	}
-}
-
-function forum_access_add_private_name_to_topics($text, $id) {
-	$topic = get_topic( $id );
-	$id = $topic->forum_id;
-	$priv_forums = forum_access_get_option('forum_access_private_forums');
-	if(array_key_exists($id,$priv_forums)) {
-		return '[' . bb_get_option('forum_access_private_text') . '] ' . $text;
-	} else {
-		return $text;
-	}
-}
-
-function forum_access_filter_topics($topics) {
+function private_forums_filter_topics($topics) {
 
 	$new_topics = array();
 	if ($topics) {
-		$priv_forums = forum_access_get_option('forum_access_private_forums');
+		$private_forums = private_forums_custom_get_options('private_forums');
 		foreach($topics as $topic) {
-			if(!array_key_exists($topic->forum_id,$priv_forums)) {
+			if(!array_key_exists($topic->forum_id,$private_forums)) {
 				$new_topics[] = $topic;
 			}
 		}
@@ -244,6 +258,44 @@ function forum_access_filter_topics($topics) {
 	}
 
 	return $topics;
+}
+
+function private_forums_add_private_name_to_forums($text, $id) {
+	$private_forums = private_forums_custom_get_options('private_forums');
+	if(array_key_exists($id,$private_forums)) {
+		return '[' . private_forums_custom_get_options('private_text') . '] ' . $text;
+	} else {
+		return $text;
+	}
+}
+
+function private_forums_add_private_name_to_topics($text, $id) {
+	$topic = get_topic( $id );
+	$id = $topic->forum_id;
+	$private_forums = private_forums_custom_get_options('private_forums');
+	if(array_key_exists($id,$private_forums)) {
+		return '[' . private_forums_custom_get_options('private_text') . '] ' . $text;
+	} else {
+		return $text;
+	}
+}
+
+function private_forums_check_user_access() {
+	if(bb_current_user()) {
+		switch(private_forums_custom_get_options('user_role')) {
+			case 'MODERATOR':
+				return bb_current_user_can('moderate');
+				break;
+			case 'ADMINSTRATOR':
+				return bb_current_user_can('administrate');
+				break;
+			default:
+				return bb_current_user_can('participate');
+				break;
+		}
+	} else {
+		return false;
+	}
 }
 
 ?>
--- private-forums/trunk/readme.txt
+++ private-forums/trunk/readme.txt
@@ -1,5 +1,6 @@
 === Private Forums ===
 Contributors: so1o
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=so1oonnet%40gmail%2ecom&item_name=Aditya%20Naik%20for%20bbPress%20Plugin%20Development&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
 Tags: private, forums, hide
 Requires at least: 0.74
 Tested up to: 0.74
@@ -9,18 +10,21 @@ Regulate Access to forums in bbPress
 
 == Description ==
 
-This plugin filters private forums from view when the user is not logged in. 
-To access the private forums the user will have to login.
+This plugin filters private forums from view when the user does not have appropriate 
+role. The roles can currently be set to Member, Moderator or Administrator.
 
-The plugin enables to user to set forum or forums as private from the 
-administration menu. The admin can also select the text to be shown in case 
-access is denied.
+The plugin enables the administrator to set forum or forums as private from the 
+administration menu. The Administrator can set the role to which the selected forums are
+restricted to.
 
 The administrator can choose how the forum handles the private forums. the 
 forums can either be completely hidden or shown with a text prefix like 
-&quot;private&quot;. the prefix is customizable through the options.
+&quot;[private]&quot;. the prefix is customizable through the options.
 
-The admin menu can accessed by keymaster from 
+The admin can also select the text to be shown in case access is denied. The error message 
+to be shown is parsed and the word 'login' is replaced by the link to login page.
+
+The admin menu can accessed by Administrators from 
 <br/><strong>Admin > Site Management > Private Forums</strong>
 
 == Installation ==
@@ -38,16 +42,33 @@ the administrator control the behavior of the forum to private forums.
 2. The error message shown when the user tries to access private resource is also 
 customizable using the Error Options.
 
+3. The admin menu is accessed through the Site Management tab
 == Version History ==
 
 * 1.0 : 
-  <br/>Initial Release
+  * Initial Release
 * 1.1 :
-  <br/>bug fix for empty private forums
-  <br/>Added failsafe for installation.
+  * bug fix for empty private forums
+  * Added failsafe for installation.
 * 2.0 : 
-  <br/>Added choice to hide private forums or show them with private prefix
-  <br/>Added selectable prefix text
-  <br/>Removed redundant forum_access_update_option
+  * Added choice to hide private forums or show them with private prefix
+  * Added selectable prefix text
+  * Removed redundant forum\_access\_update\_option
 * 2.1 : 
-  <br/>Created Common Submit for all options
+  * Created Common Submit for all options
+* 3.0 : 
+  * Fixed the submenu generation
+  * Fixed Forum Filter
+* 3.1 :
+  * Fixed &lt;?
+* 4.0 :
+  * Added Restriction by User Role
+  * Renamed Functions to be prefixed by private\_forums instead of forum\_access
+  * Changed where the options are stored.
+  * Added Upgrade Function
+  * Options can now be set by role administrator
+
+== Frequently Asked Questions == 
+
+None
+

d504fde54aa7f95541f2e72fa5e26c8d0c250e80
fel64 <fel64@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-07-17 19:41:36 +0000

    Added items remotely
    D:\xampp\htdocs\wordpress\wp-content\plugins\bbsync.php
    D:\xampp\htdocs\wordpress\wp-content\plugins\bbsyncstyles.css
    D:\xampp\htdocs\wordpress\wp-content\plugins\bbreply.php
    D:\xampp\htdocs\wordpress\wp-content\plugins\readme.txt

---
+++ bbsync/trunk/bbreply.php
@@ -0,0 +1,11 @@
+<li class="bbreply <?php echo $alt; ?>">
+	<div class="replybody">
+		<div class="poster">
+			<?php bbreplyavatar(); ?>
+			<em>~ <?php bbreplytime(); ?></em> <strong><?php bbreplier(); ?></strong>
+		</div>
+		<div class="replytext">
+			<?php bbreplytext(); ?>
+		</div>
+	</div>
+</li>

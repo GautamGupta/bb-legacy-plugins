93de8ba3c6014f59abb0fcc15348b731c66ab8a1
box87 <box87@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-08-01 16:37:46 +0000

    Removed unnecessary single quote on line 11

--- wp-css/trunk/wp-css.php
+++ wp-css/trunk/wp-css.php
@@ -5,10 +5,10 @@ Plugin URI: http://box87.com/misc/wp-css
 Description: Adds a css file to bbPress to make the administration page the same colors as WordPress.
 Author: Michael Meyer
 Author URI: http://box87.com
-Version: 1.0
+Version: 1.0.1
 */
 
-function wp_css_admin_head() { '?>
+function wp_css_admin_head() { ?>
 <link rel="stylesheet" href="<?php echo bb_get_plugin_uri(); ?>wp-css/wp-css.css.php" type="text/css" />
 <?php }
 add_action( "bb_admin_head", "wp_css_admin_head" );

843a76eaf82056b2a4fa6dfe59e8d73bcda4f15d
thomasklaiber <thomasklaiber@330c2647-7620-0410-81c2-8fc9ee8a0def>
2007-10-23 20:34:14 +0000

    tag 1.2

--- admin-add-user/trunk/admin-add-user.php
+++ admin-add-user/trunk/admin-add-user.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: Admin add user
- * Plugin Description: Allows Admins to add user through the Administration Panel.
+ * Plugin Description: Allows Keymasters to add user through the administration panel.
  * Author: Thomas Klaiber
- * Author URI: http://www.la-school.com
- * Plugin URI: http://bbpress.org/plugins/topic/29
- * Version: 1.1
+ * Author URI: http://thomasklaiber.com/
+ * Plugin URI: http://thomasklaiber.com/bbpress/admin-add-user/
+ * Version: 1.2
  */
  
 function admin_add_user_adminpage() {
@@ -49,6 +49,19 @@ function admin_add_user_adminpage() {
 	
 		$user_id = $bbdb->insert_id;
 		bb_update_usermeta( $user_id, $bb_table_prefix . 'capabilities', array('member' => true) );
+		
+		if ($_POST['user_send_email']) :
+			// why doesn't this work?
+			// bb_send_pass( $user_id, $password );
+			
+			$message = __("Your username is: %1\$s \nYour password is: %2\$s \nYou can now log in: %3\$s \n\nEnjoy!");
+
+			bb_mail(
+				bb_get_user_email( $user_id ),
+				bb_get_option('name') . ': ' . __('Password'),
+				sprintf( $message, "$user_login", "$password", bb_get_option('uri') )
+			);
+		endif;
 	
 		do_action('bb_admin_new_user', $user_id, $password);
 	
@@ -58,6 +71,7 @@ function admin_add_user_adminpage() {
 		$_POST['user_login'] = "";
 		$_POST['user_email'] = "";
 		$_POST['user_url'] = "";
+		$_POST['user_send_email'] = "";
 	endif;
 
 	if ($admin_add_user_error) : ?>
@@ -108,6 +122,11 @@ function admin_add_user_adminpage() {
 		<td><input name="pass1" type="password" />
 		<br />
 		<input name="pass2" type="password" /></td>
+	</tr>
+	
+	<tr>
+		<th scope="row"><? _e('Send Email') ?>:</th>
+		<td><label><input name="user_send_email" type="checkbox" value="1" /> <? _e('Notify about registration.') ?></label></td>
 	</tr>
 </table>
 <p class="submit">

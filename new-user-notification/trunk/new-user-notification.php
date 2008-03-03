342366fb00b4e8b02499841b7b78215317db92cb
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-03-03 16:14:00 +0000

    first public release - adds email notification to admin upon new user registration

---
+++ new-user-notification/trunk/new-user-notification.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name:  New User Notification Email
+Plugin URI: http://bbpress.org/plugins/
+Description: Notifies the admin by email that a new user has registered.
+Author: _ck_
+Author URI: http://bbShowcase.org
+Version: 0.01
+
+License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+Donate: http://amazon.com/paypage/P2FBORKDEFQIVM
+*/
+
+add_action('bb_new_user', 'new_user_notification');
+
+function new_user_notification($user_id=0) {
+	if (!$user_id) {$user_id=bb_get_current_user_info( 'id' );}
+	$user=bb_get_user($user_id); 
+		
+	$message  = sprintf(__('New user registration on %s:'), bb_get_option('name')) . "\r\n\r\n";
+	$message .= sprintf(__('Username: %s'), stripslashes($user->user_login)) . "\r\n\r\n";
+	$message .= sprintf(__('E-mail: %s'), stripslashes($user->user_email)) . "\r\n\r\n";	
+		
+	$message .= sprintf(__('Agent: %s'), substr(stripslashes($_SERVER["HTTP_USER_AGENT"]),0,80)) . "\r\n\r\n";	
+	$message .= sprintf(__('IP: %s'), $_SERVER['REMOTE_ADDR']) . "\r\n\r\n";	
+	
+	// $host = `host $_SERVER['REMOTE_ADDR']`;  	$host=end(explode(' ',$host));    	$host=substr($host,0,strlen($host)-2);	// safer rDNS process with timeout
+   	// $chk=split("\(",$host);  if($chk[1]) {$message.=$_SERVER['REMOTE_ADDR']." (".$chk[1].")". "\r\n\r\n";} else {$message.=$host. "\r\n\r\n";}
+	
+	$message .= sprintf(__('Profile: %s'), get_user_profile_link($user_id)) . "\r\n\r\n";
+		
+@bb_mail(bb_get_option('admin_email') , sprintf(__('[%s] New User Registration'), bb_get_option('name')), $message, '' );
+}
+
+?>
---
+++ new-user-notification/trunk/readme.txt
@@ -0,0 +1,38 @@
+=== New User Notification Email ===
+Tags: register, registration, new user, notification, email, _ck_
+Contributors: _ck_
+Requires at least: 0.8.2
+Tested up to: 0.8 branch
+Stable tag: trunk
+Donate link: http://amazon.com/paypage/P2FBORKDEFQIVM
+
+Notifies the admin by email that a new user has registered.
+
+== Description ==
+
+Notifies the admin by email that a new user has registered.
+
+== Installation ==
+
+* Add the `new-user-notification.php` file to bbPress' `my-plugins/` directory and activate. Check your email when new users register.
+
+== Frequently Asked Questions ==
+
+= I'm not receiving the emails? =
+
+* If you use Hotmail or AOL, your server must have rDNS and SPF setup. Otherwise try gmail instead.
+
+== License ==
+
+* CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+== Donate ==
+
+* http://amazon.com/paypage/P2FBORKDEFQIVM
+
+== History ==
+
+= Version 0.01 (2008-03-01) =
+
+* first public release
+

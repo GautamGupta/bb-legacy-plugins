083934a6e4be6f8ff297d7cf6d7e992a7478011c
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-04-12 13:19:59 +0000

    0.1.0 first public beta release. Allows any user to edit a Wiki-like post in any topic for a collaborative entry or FAQ.

---
+++ wiki-post/trunk/readme.txt
@@ -0,0 +1,45 @@
+=== Wiki Post ===
+Tags:  wiki, wiki post, group post, shared post, collective, collaborative, _ck_
+Contributors: _ck_
+Requires at least: 0.9
+Tested up to: trunk
+Stable tag: trunk
+Donate link: http://amazon.com/paypage/P2FBORKDEFQIVM
+
+== Description ==
+
+Allows any user to edit a Wiki-like post in any topic for a collaborative entry or FAQ.
+
+This is a beta release without an admin menu (coming soon).
+For now you must edit the wiki-post.php directly to change default options.
+
+== Installation ==
+
+* Edit `wiki-post.php` and change any default settings as desired near the top (until admin menu is made).
+* Add the "wiki-post.php" file to bbPress "my-plugins/" directory and activate. 
+* Optionally edit the "Wiki Post" user that has been created for you, ie. add avatar icon or change email for gravatar use
+* If you edit `wiki-post.php` after install, you must reset the plugin via `your-forum.com?reset_wiki_post` (until there is an admin menu)
+
+== Frequently Asked Questions ==
+
+* If you edit `wiki-post.php` after install, you must reset the plugin via `your-forum.com?reset_wiki_post` (until there is an admin menu)
+
+== License ==
+
+* CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+== Donate ==
+
+* http://amazon.com/paypage/P2FBORKDEFQIVM
+
+== History ==
+
+* 0.0.1 	wiki-post is born 
+* 0.1.0	first public beta release
+	
+== To Do ==
+
+* history of edits
+* rollback of edits
+* optional different background color
+* admin menu
---
+++ wiki-post/trunk/wiki-post.php
@@ -0,0 +1,137 @@
+<?php
+/*
+Plugin Name: Wiki Post
+Description:  Allows any user to edit a Wiki-like post in any topic for a collaborative entry or FAQ.
+Plugin URI:  http://bbpress.org/plugins/topic/101
+Author: _ck_
+Author URI: http://bbShowcase.org
+Version: 0.1.0
+
+License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+Instructions:   tinker with settings below, install, activate
+*/
+
+function wiki_post_initialize() {
+	global $wiki_post;	
+	if (!isset($wiki_post)) {$wiki_post = bb_get_option('wiki_post');}
+	if (!$wiki_post) {
+	
+	// you can edit these
+	
+	$wiki_post['start_question']="Add a Wiki Post to this topic ?";
+	$wiki_post['start_level']="participate";	// participate/moderate/administrate
+	$wiki_post['edit_level']="participate";	// participate/moderate/administrate
+	$wiki_post['post_instructions']="<hr /><em>This is a <strong>Wiki Post</strong> that anyone can edit to collaboratively summarize this topic or make a FAQ</em>";
+	$wiki_post['post_position']="2";		// you can make the wiki post the 1st or 2nd post (2 is recommended)
+	$wiki_post['automatic_insert']=false;	// not implimented yet - automatic wiki creation for every topic
+	$wiki_post['name']="Wiki Post";		// formal wiki name
+	$wiki_post['title']="[W]";			// text, html or image to show on topic titles if Wiki is present	
+	
+	// stop editing
+	
+	$wiki_post['user']=wiki_post_create_user();	// if you don't make a user with the above 'name' one will be made for you	
+	bb_update_option('wiki_post',$wiki_post);	
+	}
+	
+	if (isset($_GET['add_wiki_post']) && intval($_GET['add_wiki_post'])) { 					
+		if ($post_id=wiki_post_create_post()) {wp_redirect( get_post_link( $post_id ));}
+		else {wp_redirect(remove_query_arg(array('add_wiki_post')));}
+	}
+	
+	if (isset($_GET['reset_wiki_post']) && bb_current_user_can('administrate')) {
+		bb_delete_option('wiki_post');
+		wp_redirect(remove_query_arg(array('reset_wiki_post')));
+	}		
+					
+	if (is_topic()) {					
+		add_action('topicmeta','wiki_post_create_link',200);
+		add_filter('post_text','wiki_post_instructions',5);
+	} else {
+		add_filter('topic_title', 'wiki_post_title',200);
+	}	
+	add_filter('bb_current_user_can', 'wiki_post_allow_edit',200,3);	
+} 
+add_action('bb_init','wiki_post_initialize');
+
+function wiki_post_allow_edit($retvalue, $capability, $args) {
+	global $wiki_post, $bb_post;		 			
+	if ($args[0]=="edit_post" && $bb_post->post_id==$args[1] && $bb_post->poster_id==$wiki_post['user'] && bb_current_user_can($wiki_post['edit_level']))  
+	{return true;}	
+return $retvalue;
+}
+
+function wiki_post_create_link($x) {
+	global $wiki_post;		 	
+	$topic_id=get_topic_id(); $topic = get_topic($topic_id);  
+	if ($topic_id && $topic && !isset($topic->wiki_post) && bb_current_user_can($wiki_post['start_level'])) {
+		echo "<li><a href='".add_query_arg( 'add_wiki_post',$topic_id)."'>".$wiki_post['start_question']."</a></li>";
+	}
+}
+
+function wiki_post_create_user() {
+	global $wiki_post, $bbdb;
+	$user_id=$bbdb->get_var("SELECT ID FROM $bbdb->users WHERE user_login = '".$wiki_post['name']."' LIMIT 1");
+	if (!$user_id) {
+		require_once( BB_PATH . BB_INC . 'registration-functions.php');
+		if ( $user_id = bb_new_user( $wiki_post['name'], bb_get_option('admin_email'), "" ) ) {
+			bb_update_usermeta( $user_id, $bbdb->prefix . 'capabilities', array('throttle'=>true, 'member' => true) );
+			bb_update_usermeta( $user_id, $bbdb->prefix . 'title', $wiki_post['name']);
+		}	
+	}
+return $user_id;	
+}
+
+function wiki_post_create_post($topic_id=0) {
+	global $wiki_post, $bbdb;
+	bb_repermalink();
+	$topic_id=get_topic_id($topic_id); $topic = get_topic($topic_id);
+	if ($topic_id && $topic && !isset($topic->wiki_post) && bb_current_user_can($wiki_post['start_level'])) {
+
+		$time=@$bbdb->get_var("SELECT post_time FROM $bbdb->posts WHERE topic_id=$topic_id AND post_status=0 ORDER BY post_time ASC LIMIT 1 OFFSET ".($wiki_post['post_position']-2));
+		$time=gmdate('Y-m-d H:i:s',(strtotime($time." GMT")+1));					 
+
+		$defaults = array(
+			'post_id' => false,
+			'topic_id' => $topic_id,
+			'post_text' => '',
+			'post_time' => $time,
+			'poster_id' => $wiki_post['user'], 
+			'poster_ip' => $_SERVER['REMOTE_ADDR'],
+			'post_status' => 0, 
+			'post_position' => $wiki_post['post_position'],
+			'throttle'=> false
+		);
+
+		$post_id=bb_insert_post($defaults);
+		if ($post_id && $topic_id) {$topic->wiki_post['post_id']=$post_id; bb_update_topicmeta( $topic_id, 'wiki_post', $topic->wiki_post);}
+		
+	wiki_post_fix_last_poster($topic_id);
+	return $post_id;
+	}
+return 0;	
+}
+
+function wiki_post_title( $title ) {
+	global $wiki_post, $topic;
+	if ($wiki_post['title'] && isset($topic->wiki_post) && !is_topic())  {return $wiki_post['title'].' '.$title;}		
+	return $title;
+} 
+
+function wiki_post_fix_last_poster($topic_id) {
+	global $wiki_post, $bbdb;
+	
+	// bbpress wants to make wiki_post the last poster, so undo that		
+	$old_post = $bbdb->get_row( "SELECT post_id, poster_id, post_time FROM $bbdb->posts WHERE poster_id!= ".$wiki_post['user']." AND topic_id = $topic_id AND post_status = 0 ORDER BY post_time DESC LIMIT 1");
+	$old_name = $bbdb->get_var("SELECT user_login FROM $bbdb->users WHERE ID = $old_post->poster_id");
+	$bbdb->update( $bbdb->topics, array( 'topic_time' => $old_post->post_time, 'topic_last_poster' => $old_post->poster_id, 'topic_last_poster_name' => $old_name, 'topic_last_post_id' => $old_post->post_id ), compact( 'topic_id' ) );
+}
+
+function wiki_post_instructions($text) {
+	if (!is_bb_feed()) {
+		global $wiki_post, $bb_post;
+		if ($wiki_post['user']==$bb_post->poster_id) {$text.="<div class='wiki_post'>".$wiki_post['post_instructions']."</div>";}
+	}
+return $text;
+}
+?>

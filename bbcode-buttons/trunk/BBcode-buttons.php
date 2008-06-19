0e22ecda2236ffbc22e37b407d3d13076999c8b2
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-06-19 10:43:11 +0000

    0.0.1 early beta release based upon a request

---
+++ bbcode-buttons/trunk/BBcode-buttons.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: BBcode Buttons Toolbar
+Plugin URI: http://bbpress.org/plugins/topic/114
+Description: Automatically adds an easy access button toolbar above the post textarea to allow quick tags in BBcode. This is an enhanced replacement for the Comment Quicktags plugin. No template editing required.
+Author: _ck_
+Author URI: http://bbShowcase.org
+Version: 0.0.1
+
+License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+Donate: http://amazon.com/paypage/P2FBORKDEFQIVM
+*/
+
+function bbcode_buttons() { 
+global $bb_current_user;
+if ($bb_current_user->ID && (isset($_GET['new']) || in_array(bb_get_location(),array('topic-page','tag-page','forum-page')))) {
+$tags = bb_allowed_tags();
+echo  "<scr"."ipt type='text/javascript' defer='defer'>
+	function BBcodeButtons_init() {
+	BBcodeButtons.push(new BBcodeButton('ed_bold','B','[b]','[/b]','b','font-weight:bold;','bold'));
+	BBcodeButtons.push(new BBcodeButton('ed_italic','I','[i]','[/i]','i','padding-right:7px;font-style:italic;','italics'));
+	BBcodeButtons.push(new BBcodeButton('ed_under','U','[u]','[/u]','u','text-decoration:underline;','underline'));
+	BBcodeButtons.push(new BBcodeButton('ed_strike','S','[s]','[/s]','s','text-decoration:line-through;','strike through'));
+	BBcodeButtons.push(new BBcodeButton('ed_link','URL','','[/url]','a','text-decoration:underline;','make a link')); // special case
+	BBcodeButtons.push(new BBcodeButton('ed_block','&#147;quote&#148;','[quote]','[/quote]','q','padding:0 1px 1px 1px;','quote'));";
+if (isset($tags['img'])) {echo "BBcodeButtons.push(new BBcodeButton('ed_img','IMG','[img]','[/img]','m',-1)); // special case";}
+echo  "BBcodeButtons.push(new BBcodeButton('ed_ul','UL','[list]','[/list]','u','','unordered list'));
+	BBcodeButtons.push(new BBcodeButton('ed_ol','OL','[list=1]','[/list]','o','','ordered list'));
+	BBcodeButtons.push(new BBcodeButton('ed_li','LI','[li]','[/li]','l','','list item'));";	
+if (isset($tags['center'])) {echo "BBcodeButtons.push(new BBcodeButton('ed_center','center','[center]','[/center]','c','','center'));";}	
+echo  "BBcodeButtons.push(new BBcodeButton('ed_code','CODE','[code]','[/code]','p','line-height:160%;font-size:80%;letter-spacing:1px;font-family:anadale,serif;','unformatted / code'));
+	BBcodeButtons.push(new BBcodeButton('ed_close','close','','','c',' ','auto-close any tags you left open'));
+	}</scr"."ipt>
+	<scr"."ipt src='" .bb_get_option('uri').str_replace(array(".php",BBPATH),array(".js",""),__FILE__)."' type='text/javascript' defer='defer'></scr"."ipt>";
+}	
+} add_action('bb_foot', 'bbcode_buttons');	
+
+?>
---
+++ bbcode-buttons/trunk/readme.txt
@@ -0,0 +1,56 @@
+=== BBcode Buttons  ===
+Tags: bbcode, _ck_
+Contributors: _ck_
+Requires at least: 0.8.2
+Tested up to: trunk
+Stable tag: trunk
+Donate link: http://amazon.com/paypage/P2FBORKDEFQIVM
+
+Automatically adds an easy access button toolbar above the post textarea to allow quick tags in BBcode. This is an enhanced replacement for the Comment Quicktags plugin. No template editing required.
+
+== Description ==
+
+Automatically adds an easy access button toolbar above the post textarea to allow quick tags in BBcode. This is an enhanced replacement for the Comment Quicktags plugin. No template editing required.
+
+Requires the BBcode-Lite plugin (or some other kind of BBcode support).
+
+Some buttons will only appear if the tag is supported  (ie. img, center)
+
+Smilie support coming eventually.
+
+* example:
+http://bbshowcase.org/forums/topic/new-bbpress-plugin-bbcode-lite
+
+== Installation ==
+
+* Add entire folder `bbcode-buttons` to bbPress' `my-plugins/` directory and activate.
+
+== Frequently Asked Questions ==
+
+= Why is this better than Comment Quicktags? =
+
+* The Comment Quicktags plugin does something extreme awkward - it captures the entire PHP page output and then parses it to insert the toolbar. 
+That can be incompatible with some PHP configurations. BBcode Buttons instead uses javascript to insert itself. BBcode is also more familiar to forum users than bbPress's default html tags.
+
+= The toolbar is working but the codes don't do anything? =
+
+* BBcode Buttons requires the BBcode-Lite plugin installed (or some other kind of BBcode support).
+
+== License ==
+
+* CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+== Donate ==
+
+* http://amazon.com/paypage/P2FBORKDEFQIVM
+
+== History ==
+
+= Version 0.0.1 (2008-06-19) =
+
+* early beta release based upon a request
+
+== To Do ==
+
+* lots, this is an early beta
+

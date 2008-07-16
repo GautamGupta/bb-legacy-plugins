666e612988a7fd9cea553d9cef02a3126bbc2668
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-07-16 05:05:14 +0000

    0.0.2 minor tweaks

--- terms-of-service/trunk/terms-of-service.php
+++ terms-of-service/trunk/terms-of-service.php
@@ -3,7 +3,7 @@
 Plugin Name: Terms of Service
 Plugin URI:  http://bbpress.org/plugins/topic/117
 Description:  Adds a "Terms of Service" (aka TOS) agreement to your registration page (and can optionally be linked to directly).
-Version: 0.0.1
+Version: 0.0.2
 Author: _ck_
 Author URI: http://bbshowcase.org
 
@@ -17,20 +17,8 @@ if (bb_get_location()=="register-page") {	// determines if we're actually on reg
 	add_action('bb_send_headers', 'terms_of_service_check');	// check before headers finish sending
 } 
 
-add_action( 'bb_init', 'terms_of_service_get');	// check for direct link
-
-function terms_of_service_get() {
-	if (isset($_GET['terms_of_service']) || isset($_GET['terms-of-service'])) {	// check for direct link
-		bb_get_header();	?>
-		<h3 class="bbcrumb"><a href="<?php bb_option('uri'); ?>"><?php bb_option('name'); ?></a> &raquo; <?php _e('Terms of Service'); ?></h3>
-		<div class="indent">
-		<h2 id="register"><?php _e('Terms of Service'); ?></h2>		
-		<div id="terms_of_service" style="padding:0.5em 1em 1em 1em; margin:0em 3em; background: #eee; color: #000;">	
-		<?php 	@readfile(dirname(__FILE__).'/terms-of-service.html');
-		echo '</div></div>';
-		bb_get_footer(); 
-		exit();	
-	}
+if (isset($_GET['terms_of_service']) || isset($_GET['terms-of-service'])) {	// check for direct link
+	add_action( 'bb_init', 'terms_of_service_get');	
 }
 
 function terms_of_service() {	// show the form
@@ -46,5 +34,17 @@ function terms_of_service_check() {	// examine the answer
 	if ($_POST && (!isset($_POST['terms_of_service']) || $_POST['terms_of_service']!="agree")) {
 		bb_die(__("You must agree to the terms of service to use the forum").",<br /><a href='register.php'>".__("please go back and try again")."</a>.");
 	}
+}
+
+function terms_of_service_get() {	
+	bb_get_header();	?>
+	<h3 class="bbcrumb"><a href="<?php bb_option('uri'); ?>"><?php bb_option('name'); ?></a> &raquo; <?php _e('Terms of Service'); ?></h3>
+	<div class="indent">
+	<h2 id="register"><?php _e('Terms of Service'); ?></h2>		
+	<div id="terms_of_service" style="padding:0.5em 1em 1em 1em; margin:0em 3em; background: #eee; color: #000;">	
+	<?php 	@readfile(dirname(__FILE__).'/terms-of-service.html');
+	echo '</div></div>';
+	bb_get_footer(); 
+	exit();	
 }
 ?>

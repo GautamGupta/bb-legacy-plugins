8a0be175fd434f317adb74c69179d7bc0fe2e326
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-08-06 14:14:31 +0000

    0.0.4 bunch of new features including online status in posts, last online time in profile, bot tracking

--- mini-track/trunk/mini-track.php
+++ mini-track/trunk/mini-track.php
@@ -5,7 +5,7 @@ Plugin URI: http://bbpress.org/plugins/topic/130
 Description: A simple way to count and track both members and non-members as they move around your forum.
 Author: _ck_
 Author URI: http://bbShowcase.org
-Version: 0.0.2
+Version: 0.0.4
 
 License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
 
@@ -23,7 +23,11 @@ function mini_track($display=0) {
 global $mini_track_done, $bb_current_user; 
 if (isset($mini_track_done)) {return;} $mini_track_done=true; // only run once if manually called
 $mini_track=bb_get_option('mini_track');
-$users=0; $members=0; $onpage=0; $names=""; $index="";
+$users=0; $members=0; $bots=0; $onpage=0; $names=""; $index="";
+$bots="worm|bot|spider|crawler|Googlebot|mediapartners|MSNBOT|YahooSeeker|Overture|VerticalCrawler|FastSearch|modspider|froogle|"
+."Bloglines|ZyBorg|InfoSeek|inktomi|looksmart|Scooter|Slurp|AskJeeves\/Teoma|teoma_agent|teomaagent|MARTINI|Gigabot|Netcraft|SurveyBot|ia_archiver|"
+."lycos|scooter|fast\-webcrawler|slurp\@inktomi|turnitinbot|technorati|Findexa|NextLinks|findlinks|Gais|gaisbo|zyborg|surveybot|"
+."bloglines|BlogSearch|PubSub|pubsub|Syndic8|userland|gigabot|become\.com";
 $index=ip2long($_SERVER["REMOTE_ADDR"]);	// this has some limitations and bugs - disable if you use the next two lines 
 /*
 // more advanced indexing technique on the next two lines - disabled by default for speed
@@ -31,14 +35,25 @@ $indexlist=array('REMOTE_ADDR','HTTP_USER_AGENT','HTTP_X_FORWARDED_FOR', 'HTTP_X
 foreach ($indexlist as $check) {if (isset($_SERVER[$check])) {$index.=$_SERVER[$check];}} $index=md5($index);
 */
 $url=addslashes(urlencode($_SERVER["REQUEST_URI"]));	// this also has some issues with dynamic url cruft but is acceptable
+if ($bb_current_user->ID && is_array($mini_track) && !array_key_exists($index,$mini_track)) {@bb_update_usermeta($bb_current_user->ID,'mini_track',date('r'));} // store first seen date
+$mini_track[$index]->time=time(); 
 $mini_track[$index]->location=$url;
 $mini_track[$index]->member=$bb_current_user->ID;
-$mini_track[$index]->name=$bb_current_user->data->user_login;
-$mini_track[$index]->time=time()-1217548800;	// the subtraction is to keep the number small since it's serialized and not stored integer
-$cutoff=(time()-1217548800)-30*60; 	// seconds to consider user "online" :: 30 minute default, reduce if desired
+if ($bb_current_user->ID) {$mini_track[$index]->name=$bb_current_user->data->user_login;}
+elseif (eregi($user_track['bots'],$_SERVER['HTTP_USER_AGENT'])) {$mini_track[$index]->bot=1;}
+
+$cutoff=time()-30*60; // seconds to consider user "online" :: 30 minute default, reduce if desired
 $bb_uri=bb_get_option('uri'); $profile=$bb_uri."profile.php?id=";
-foreach ($mini_track as $key=>$value) { if ($value->time<$cutoff) {unset($mini_track[$key]);}
-else {$users++; if ($value->member) {$members++; $names.="<a href='$profile$value->member'>$value->name</a>, ";} if ($value->location==$url) {$onpage++;}}}
+foreach ($mini_track as $key=>$value) { 
+if ($value->time<$cutoff) {
+if ($mini_track[$key]->member) {@bb_update_usermeta($mini_track[$key]->member,'mini_track',date('r',$mini_track[$key]->time));} // store last seen date
+unset($mini_track[$key]);}
+else {$users++; 
+	if ($value->member) {$members++; $names.="<a href='$profile$value->member'>$value->name</a>, ";} 
+	if (isset($value->bot)) {$bots++;} 
+	if ($value->location==$url) {$onpage++;}
+	}
+}
 if ($display) {
 if (bb_current_user_can('administrate')) {$start="<a href='?mini_track_display'>"; $end="</a>";} else {$start=""; $end="";}
 echo "<div class='mini_track'>There are <strong>$users</strong> total $start"."users online".$end.".";
@@ -52,16 +67,56 @@ echo " <strong>$onpage</strong> of them are on this page.</div>";
 function mini_track_display() {
 if (!bb_current_user_can('administrate')) {return;}
 $bb_uri=bb_get_option('uri'); $profile=$bb_uri."profile.php?id=";
+if (isset($_GET['mini_track_reset'])) {@bb_update_option('mini_track','');}
 echo '<html><head><meta http-equiv="refresh" content="30;url='.$bb_uri.'?mini_track_display" /></head><pre>';
-mini_track(1); echo "<br>";
+echo "<div style='float:right;'>[<a href='$bb_url?mini_track_reset'><small>reset</small></a>]</div>";
+mini_track(1); 
+echo "<br clear=both>";
 $mini_track=bb_get_option('mini_track');
 foreach ($mini_track as $key=>$value) {
 $url=urldecode($value->location);
-echo (($value->member) ? "<a href='$profile$value->member'>$value->name</a>" : "anonymous")." - ".ceil(((time()-1217548800)-$value->time+1)/60)." minutes ago - <a href='$url'>$url</a><br>";
+if ($value->member) echo "<a href='$profile$value->member'>$value->name</a>";
+elseif (isset($value->bot)) {echo "bot";} else {echo "guest";}
+echo " - ".ceil(((time())-$value->time+1)/60)." minutes ago - <a href='$url'>$url</a><br>";
 }
 exit();
 }
 
+function mini_track_online($user_id=0) {
+global $mini_track_online, $bb_post, $user; 	
+	if (!isset($mini_track_online)){	
+		$mini_track=bb_get_option('mini_track');
+		foreach ($mini_track as $key=>$value) {if ($mini_track[$key]->member) {$mini_track_online[$mini_track[$key]->member]=true;}}
+	}
+if (!$user_id) {if (isset($bb_post)) {$user_id=$bb_post->poster_id;} elseif (isset($user)) {$user_id=$user->ID;}}
+return array_key_exists($user_id,$mini_track_online);
+}
+
+function mini_track_online_filter($titlelink) {
+	if (mini_track_online()) {echo "<div class='mini_track_online'>".__("online")."</div>";} 
+	else {echo "<div class='mini_track_offline'>".__("offline")."</div>";} 
+return $titlelink;
+}
+
+function mini_track_profile_key($keys) {	// inserts post_count into profile without hacking
+global $self; 
+if (empty($self)==true && isset($_GET['tab'])==false && bb_get_location()=="profile-page") {
+	(array) $keys=array_merge(array_slice((array) $keys, 0 , 1), array('mini_track' => array(0, __('Last Online'))), array_slice((array) $keys,  1));    
+}
+return (array) $keys;
+}
+
+function mini_track_css() {
+echo '<style type="text/css">
+.mini_track_online {font-size:90%; color:green;}
+.mini_track_offline {font-size:90%; color:#bbb;}
+</style>'; 
+}
+
 add_action('bb_foot','mini_track');
-if (isset($_GET['mini_track_display'])) {add_action('bb_init','mini_track_display');}
+add_action('bb_head','mini_track_css');
+add_filter( 'get_profile_info_keys','mini_track_profile_key');
+add_filter( 'post_author_title', 'mini_track_online_filter',100);
+
+if (isset($_GET['mini_track_display']) || isset($_GET['mini_track_reset'])) {add_action('bb_init','mini_track_display');}
 ?>

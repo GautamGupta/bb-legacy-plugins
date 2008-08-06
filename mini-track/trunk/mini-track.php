84a83bd12e1fc5c31d735813c9a99268ac145a7f
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-08-06 16:01:52 +0000

    0.0.6 automatic insertion by default and many adjustable options at top

--- mini-track/trunk/mini-track.php
+++ mini-track/trunk/mini-track.php
@@ -5,48 +5,89 @@ Plugin URI: http://bbpress.org/plugins/topic/130
 Description: A simple way to count and track both members and non-members as they move around your forum.
 Author: _ck_
 Author URI: http://bbShowcase.org
-Version: 0.0.5
+Version: 0.0.6
 
 License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
 
 Donate: http://amazon.com/paypage/P2FBORKDEFQIVM
 
-To use this, put   <?php mini_track(1); ?>  in your template where you want the output to display. 
+Update: This plugin now inserts itself into the footer automatically, no template edits required unless you want custom placement.
+To use this MANUALLY, put   <?php mini_track(1); ?>  in your template where you want the output to display. 
 If you only want it on the front page you can do it like this: <?php if (is_front() ) {mini_track(1);} ?>
 If you also want a list of the member names, use <?php mini_track(2); ?>
 You can see a list of users and locations by going to  your-forum-url.com/?mini_track_display
 This plugin was written in 15 minutes and demonstrates how easy it is to write plugins for bbPress.
 */
 
-function mini_track($display=0) {  
-// $display=2;		// uncomment this line if you want automatic display in every footer without template edits
-global $mini_track_done, $bb_current_user; 
-if (isset($mini_track_done)) {return;} $mini_track_done=true; // only run once if manually called
-$mini_track=bb_get_option('mini_track');
-$users=0; $members=0; $bots=0; $onpage=0; $names=""; $index="";
-$bots="worm|bot|spider|crawler|Googlebot|mediapartners|MSNBOT|YahooSeeker|Overture|VerticalCrawler|FastSearch|modspider|froogle|"
+// change any option you don't want to false
+
+$mini_track_options['automatic_in_footer'] = true;
+$mini_track_options['show_names_in_footer'] = true;
+$mini_track_options['show_only_on_front_page'] = false;
+$mini_track_options['last_online_in_profile'] = true;
+$mini_track_options['online_status_in_post'] = true;
+$mini_track_options['fast_index'] = false;			// false tracks NAT/proxy users better, true = faster IP only
+$mini_track_options['display_refresh_time'] = 30; 	// seconds
+
+$mini_track_options['style']="
+	.mini_track {font-size:1em; color:black; text-align:center;} 
+	.mini_track strong, mini_track a {color: #006400;}
+	.mini_track_online {font-size:90%; color:green;} 
+	.mini_track_offline {font-size:90%; color:#bbb;}
+";
+
+$mini_track_options['bots']="worm|bot|spider|crawler|Googlebot|mediapartners|MSNBOT|YahooSeeker|Overture|VerticalCrawler|FastSearch|modspider|froogle|"
 ."Bloglines|ZyBorg|InfoSeek|inktomi|looksmart|Scooter|Slurp|AskJeeves\/Teoma|teoma_agent|teomaagent|MARTINI|Gigabot|Netcraft|SurveyBot|ia_archiver|"
 ."lycos|scooter|fast\-webcrawler|slurp\@inktomi|turnitinbot|technorati|Findexa|NextLinks|findlinks|Gais|gaisbo|zyborg|surveybot|"
 ."bloglines|BlogSearch|PubSub|pubsub|Syndic8|userland|gigabot|become\.com";
+
+/* STOP EDITING HERE */
+
+$bb->load_options = true;	// better db performance, but probably won't work here
+
+add_action('bb_foot','mini_track');
+if ($mini_track_options['style']) {add_action('bb_head','mini_track_style');}
+if ($mini_track_options['last_online_in_profile']) {add_filter( 'get_profile_info_keys','mini_track_profile_key');}
+if ($mini_track_options['online_status_in_post']) {add_filter( 'post_author_title', 'mini_track_online_filter',100);}
+if (isset($_GET['mini_track_display']) || isset($_GET['mini_track_reset'])) {add_action('bb_init','mini_track_display');}
+
+function mini_track($display=0) {  
+global $mini_track_options, $mini_track_done, $bb_current_user; 
+
+if (isset($mini_track_done)) {return;} $mini_track_done=true; // only run once if manually called
+if ($mini_track_options['automatic_in_footer']) {$display=1;}
+if ($mini_track_options['show_names_in_footer']) {$display=2;}
+if ($display && $mini_track_options['show_only_on_front_page'] && !is_front()) {$display=0;}
+
+$mini_track=bb_get_option('mini_track');
+$users=0; $members=0; $bots=0; $onpage=0; $names=""; $index="";
+
+if ($mini_track_options['fast_index']) {
 $index=ip2long($_SERVER["REMOTE_ADDR"]);	// this has some limitations and bugs - disable if you use the next two lines 
-/*
-// more advanced indexing technique on the next two lines - disabled by default for speed
+} else {
+// more advanced indexing technique on the next two lines - disable for speed at expense of no NAT/proxy detection
 $indexlist=array('REMOTE_ADDR','HTTP_USER_AGENT','HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_FORWARDED_FOR','HTTP_FORWARDED','HTTP_VIA', 'HTTP_X_COMING_FROM', 'HTTP_COMING_FROM'); 
 foreach ($indexlist as $check) {if (isset($_SERVER[$check])) {$index.=$_SERVER[$check];}} $index=md5($index);
-*/
+}
+
 $url=addslashes(urlencode($_SERVER["REQUEST_URI"]));	// this also has some issues with dynamic url cruft but is acceptable
-if ($bb_current_user->ID && is_array($mini_track) && !array_key_exists($index,$mini_track)) {@bb_update_usermeta($bb_current_user->ID,'mini_track',date('r'));} // store first seen date
+
+// store first seen date when they arrive
+if ($bb_current_user->ID && is_array($mini_track) && !array_key_exists($index,$mini_track)) {@bb_update_usermeta($bb_current_user->ID,'mini_track',date('r'));} 
+
 $mini_track[$index]->time=time(); 
 $mini_track[$index]->location=$url;
 $mini_track[$index]->member=$bb_current_user->ID;
 if ($bb_current_user->ID) {$mini_track[$index]->name=$bb_current_user->data->user_login;}
-elseif (eregi($bots,$_SERVER['HTTP_USER_AGENT'])) {$mini_track[$index]->bot=1;}
+elseif (eregi($mini_track_options['bots'],$_SERVER['HTTP_USER_AGENT'])) {$mini_track[$index]->bot=1;}	// detect/save bots
 
 $cutoff=time()-30*60; // seconds to consider user "online" :: 30 minute default, reduce if desired
 $bb_uri=bb_get_option('uri'); $profile=$bb_uri."profile.php?id=";
 foreach ($mini_track as $key=>$value) { 
 if ($value->time<$cutoff) {
-if ($mini_track[$key]->member) {@bb_update_usermeta($mini_track[$key]->member,'mini_track',date('r',$mini_track[$key]->time));} // store last seen date
+
+// store last seen date when they leave
+if ($mini_track[$key]->member) {@bb_update_usermeta($mini_track[$key]->member,'mini_track',date('r',$mini_track[$key]->time));} 
 unset($mini_track[$key]);}
 else {$users++; 
 	if ($value->member) {$members++; $names.="<a href='$profile$value->member'>$value->name</a>, ";} 
@@ -54,24 +95,25 @@ else {$users++;
 	if ($value->location==$url) {$onpage++;}
 	}
 }
-if ($display) {
+if ($display) {	// to do: internationalize i18n
 if (bb_current_user_can('administrate')) {$start="<a href='?mini_track_display'>"; $end="</a>";} else {$start=""; $end="";}
 echo "<div class='mini_track'>There are <strong>$users</strong> total $start"."users online".$end.".";
 echo " <strong>$members</strong> of them are members";
 if ($members && ($display==2 || $display=="members")) {echo ": ".rtrim($names,", ").".";} else {echo ".";}
-echo " <strong>$onpage</strong> of them are on this page.</div>";
+if ($onpage>1 && !$mini_track_options['show_only_on_front_page']) {echo " <strong>$onpage</strong> of them are on this page.</div>";}
 }
 @bb_update_option('mini_track',$mini_track);	// this serialized string will get nasty for more than a few dozen people online
 }
 
 function mini_track_display() {
 if (!bb_current_user_can('administrate')) {return;}
+global $mini_track_options;
 $bb_uri=bb_get_option('uri'); $profile=$bb_uri."profile.php?id=";
 if (isset($_GET['mini_track_reset'])) {@bb_update_option('mini_track','');}
-echo '<html><head><meta http-equiv="refresh" content="30;url='.$bb_uri.'?mini_track_display" /></head><pre>';
+echo '<html><head><meta http-equiv="refresh" content="'.$mini_track_options['display_refresh_time'].';url='.$bb_uri.'?mini_track_display" /></head><pre>';
 echo "<div style='float:right;'>[<a href='$bb_url?mini_track_reset'><small>reset</small></a>]</div>";
 mini_track(1); 
-echo "<br clear=both>";
+echo "<br clear=both /><br />";
 $mini_track=bb_get_option('mini_track');
 $mini_track=array_reverse($mini_track,true);
 foreach ($mini_track as $key=>$value) {
@@ -110,17 +152,5 @@ if (empty($self)==true && isset($_GET['tab'])==false && bb_get_location()=="prof
 return (array) $keys;
 }
 
-function mini_track_css() {
-echo '<style type="text/css">
-.mini_track_online {font-size:90%; color:green;}
-.mini_track_offline {font-size:90%; color:#bbb;}
-</style>'; 
-}
-
-add_action('bb_foot','mini_track');
-add_action('bb_head','mini_track_css');
-add_filter( 'get_profile_info_keys','mini_track_profile_key');
-add_filter( 'post_author_title', 'mini_track_online_filter',100);
-
-if (isset($_GET['mini_track_display']) || isset($_GET['mini_track_reset'])) {add_action('bb_init','mini_track_display');}
+function mini_track_style() {global $mini_track_options; echo "<style type='text/css'>".$mini_track_options['style']."</style>"; }
 ?>
--- mini-track/trunk/readme.txt
+++ mini-track/trunk/readme.txt
@@ -13,38 +13,37 @@ A simple way to count and track both members and non-members as they move around
 A simple way to count and track both members and non-members as they move around your forum.
 Now shows online status in posts and last online time in profiles and can determine most bots.
 
-To use this, put   `<?php mini_track(1); ?>`  in your template where you want the output to display. 
-(If you also want a list of the member names, change "1" to "2", use `<?php mini_track(2); ?>` )
+This plugin now inserts itself into the footer automatically, no template edits required unless you want custom placement.
 You can see a list of users and locations by going to  `your-forum-url.com/?mini_track_display`
 
 The core of this plugin was written in 15 minutes to demonstrate how easy it is to write plugins for bbPress.
 (It is meant as a temporary substitute until the full "User Track" plugin is released much later this year.)
 
+If you upgrade from an old version you need to use the RESET link in the mini-track display panel.
+
 == Installation ==
 
 * Install `mini-track.php` to  `my-plugins/` and activate
 
-* Edit your `footer.php` template to add  the info like so:
+* Edit options (true/false) at the top of the plugin until an admin menu is made
+
+*  This plugin now inserts itself into the footer automatically, no template edits required unless you want custom placement.
+If you want CUSTOM placement, edit your `footer.php` template (or other template) to add  the info like so:
 `
 	<div id="footer">
-		<?php mini_track(2); ?>
+		<?php mini_track(1); ?>
 		<p><?php printf(__('%1$s is proudly powered by <a href="%2$s">bbPress</a>.'), bb_option('name'), "http://bbpress.org") ?></p>
 	</div>
 
 `
-* If you only want it on the front page you can do it like this: `<?php if (is_front() ) {mini_track(1);} ?>` or you can put it anywhere in `front-page.php` that you'd like.
-
-
+* With non-automatic placement, if you also want a list of the member names, use `<?php mini_track(2); ?>`
+or if you only want it on the front page you can do it like this: `<?php if (is_front() ) {mini_track(1);} ?>` or you can put it anywhere in `front-page.php` that you'd like.
 
 == Frequently Asked Questions ==
 
-= I know there are more people online than Mini Track is showing ? =
+= There are duplicates of users or dates are wrong ? =
 
-* Note the method this plugin uses, indexing by plain IP only, has some limitations. If you get more than one person from one IP, only one person will be tracked. If you run bbPress on an intranet or multiple people often visit from the same proxy like a school, this makes it useless.
-
-If it's a problem for anyone, you could take the md5 of the IP plus the User Agent plus any proxy headers and use that as an index instead. I didn't do that by default because it would be considerably slower and doubles the size of the key (and serialized string).
-
-The md5 technique is included with version 0.0.2 but is disabled by default (commented out). You'll have to enable it if you need it.
+* If you upgrade from an old version you need to use the RESET link in the mini-track display panel.
 
 = Does this slow down my website? =
 
@@ -55,6 +54,14 @@ $bb->load_options = true;
 That will make all options load at once when bbPress starts.
 
 * It is NOT recommended to use this on forums with more than a few dozen visitors at a time (because of the complex string serialization/deserialization)
+
+= I know there are more people online than Mini Track is showing ? =
+
+* Note the (old) method this plugin uses, indexing by plain IP only, has some limitations. If you get more than one person from one IP, only one person will be tracked. If you run bbPress on an intranet or multiple people often visit from the same proxy like a school, this makes it useless.
+
+If it's a problem for anyone, you could take the md5 of the IP plus the User Agent plus any proxy headers and use that as an index instead. I didn't do that by default because it would be considerably slower and doubles the size of the key (and serialized string).
+
+The md5 technique is now used by default. You can disable it if you want and use the old IP method at the expense of missing multiple NAT/proxy visitors.
 
 == License ==
 

f82b39567f79ba6d3d23b15e3f0acedaeb300092
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-08-28 05:13:07 +0000

    1.6.0 fixes for bbPress 1.0 compatibility

--- bb-topic-views/trunk/bb-topic-views.php
+++ bb-topic-views/trunk/bb-topic-views.php
@@ -1,28 +1,39 @@
 <?php
 /*
 Plugin Name: bb-Topic-Views
-Plugin URI: http://blog.wittmania.com/bb-topic-views
+Plugin URI: http://bbpress.org/plugins/topic/bb-topic-views/
 Description: Counts the number of times a topic has been viewed, and allows the administrator to display the count in various places.
-Author: Mike Wittmann
+Author: Mike Wittmann, _ck_
 Author URI: http://blog.wittmania.com/
-Version: 1.5
+Version: 1.6.0
 */
 
 // Set this to zero if you DON'T want the view count automatically appended to the topic title.  Default is 1.
 $append_to_title = 1;
 
+/*  stop editing here */
+
+// only executes if option is set to 1 above
+if ($append_to_title && (is_front() || is_forum() || is_tags())) {add_filter('topic_title', 'view_count_append_to_title', 99);}
+
+add_filter('bb_head', 'update_view_count');
+add_action('bb_init', 'views_session_check');
+
 //Force bbpress to open a session, if it hasn't already, which will help to avoid double-counting views (see below)
-function views_session_check ()
-{
-	if( !isset( $_SESSION ) ) {
-		session_start();
+function views_session_check () {
+	if( !isset( $_SESSION ) && is_topic()) {		// only start session if not already stared and it's a topic page
+		// eaccelerator_set_session_handlers();	// todo: detect and work with memcaches
+		// @session_cache_limiter('public');	// allows back button to work without losing form data - update: bad idea, causes other problems
+		@session_start();
 	}
 }
 
-function view_count_append_to_title ($title)
+if (!function_exists('is_tags')) {function is_tags() {return is_tag();}}	// older bbPress compatibility
+
+function view_count_append_to_title ($title) {
 /*This function appends the view count to the end of the title on the front, forum, and tags pages.  You can comment this out if you want 
 to use the "show_view_count" function to place the view count somewhere else instead.*/
-{
+
 	global $topic;
 	
 	$view_count = get_view_count( $topic->topic_id );
@@ -39,22 +50,29 @@ to use the "show_view_count" function to place the view count somewhere else ins
 	return $title;
 }
 
-function show_view_count ()
+function show_view_count () {
 /*Use this function directly to display the view count somehere other than at the end of the title.  If you are going that route,
 you will probably want to comment out the display_view_count_title function above*/
-{
+
 	global $topic;
 	
 	$view_count = get_view_count ( $topic->topic_id );
 	echo $view_count;
 }
 	
-
-function get_view_count ( $topic_id )
-{
+function get_view_count ( $topic_id ) {
 	global $bbdb;
+	
+	if (bb_get_option('bb_db_version')>1600) {	// bbPress 1.0
+	
+	$view_count = $bbdb->get_var("SELECT meta_value FROM $bbdb->meta WHERE object_type='bb_topic' AND object_id = $topic_id AND meta_key='views' ");
 
+	} else {
+	
 	$view_count = $bbdb->get_var("SELECT meta_value FROM $bbdb->topicmeta WHERE topic_id = $topic_id AND meta_key='views'");
+	
+	}
+		
 		//If it already set, it just returns the value
 
 	if ($view_count<=0) { //If the view count hasn't bee initialized yet, this will initialize the value before it is returned
@@ -64,48 +82,74 @@ function get_view_count ( $topic_id )
 	return $view_count;
 }
 
-function initialize_view_count( $topic_id ) //If the view count for a topic hasn't been set yet
-{ 
+function initialize_view_count( $topic_id ) {	//If the view count for a topic hasn't been set yet
+ 
 	global $bbdb, $topic;
 	
-	$view_count = $topic->topic_posts; //Sets the new record to the number of posts that have been made in a topic
+	$view_count = $topic->topic_posts; 	//Sets the new record to the number of posts that have been made in a topic
 		
 	//Adds the record to the DB so it isn't zero any longer
-	$bbdb->query("INSERT INTO $bbdb->topicmeta ( meta_id, topic_id, meta_key, meta_value) VALUES ( NULL , $topic_id, 'views', $view_count )");
+	
+	if (bb_get_option('bb_db_version')>1600) {	// bbPress 1.0
+	
+	@$bbdb->query("INSERT INTO $bbdb->meta (object_id, object_type, meta_key, meta_value) VALUES ($topic_id, 'bb_topic', 'views', $view_count)");
+	
+	} else {		// bbPress 0.7 - 0.9
+	
+	@$bbdb->query("INSERT INTO $bbdb->topicmeta (topic_id, meta_key, meta_value) VALUES ($topic_id, 'views', $view_count)");
+	
+	} 
 	
 	return $view_count;
 }
 	
-function update_view_count()
-{
-	global $bbdb, $topic;
+function update_view_count() {
+	global $bbdb, $topic, $topic_id;
 	
 	if (is_topic()) {
-		$topic_id = get_topic_id();
-		
-		$row = $bbdb->get_row("SELECT * FROM $bbdb->topicmeta WHERE topic_id = $topic_id AND meta_key='views'");
-		$view_count = $row->meta_value;
-		$meta_id = $row->meta_id;
+		if (empty($topic->views)) {$view_count=0;} else {$view_count=$topic->views;}
 	
 		if ($view_count>=1) {
-			$last_topic_id = $_SESSION['last_topic_id']; //Pulls the session variable for the last topic we viewed
-			if ($topic_id != $last_topic_id) { //Makes sure we aren't viewing a different page of the same topic.
-				//Add 1 to $view_count and update the DB
-				$view_count = ($view_count + 1);
-				$bbdb->query("UPDATE $bbdb->topicmeta SET meta_value = $view_count WHERE meta_id = $meta_id");
-			}
-			}else{
-				initialize_view_count ( $topic_id); // Initializes the value so the next time it is displayed it will not be zero
+			
+			$last_topic_id = $_SESSION['last_topic_id'];	// Pulls the session variable for the last topic we viewed
+			if ($topic_id != $last_topic_id) { 	//Makes sure we aren't viewing a different page of the same topic.
+			
+				// Add 1 to $view_count and update the DB
+				$topic->views++; 
+
+				if (bb_get_option('bb_db_version')>1600) { // bbPress 1.0
+			
+				@$bbdb->query("UPDATE $bbdb->meta SET meta_value=meta_value+1 WHERE object_type='bb_topic' AND object_id=$topic_id AND meta_key='views' LIMIT 1");
+			
+				} else { // 0.7 - 0.9
+			
+				@$bbdb->query("UPDATE $bbdb->topicmeta SET meta_value=meta_value+1 WHERE topic_id=$topic_id AND meta_key='views' LIMIT 1");
+			
+				}
+			
+			}			
+						
+		} else {
+			initialize_view_count ( $topic_id); // Initializes the value so the next time it is displayed it will not be zero
 		}
-	$_SESSION['last_topic_id'] = $topic_id; //Sets the session variable so it is there the next time we view a topics page
+		$_SESSION['last_topic_id'] = $topic_id;	 //Sets the session variable so it is there the next time we view a topics page
 	}
 }
 
-function most_viewed_list ( $list_length = '10', $before_list = '<ul>', $after_list = '</ul>', $before_item = '<li>', $after_item = '</li>')
-{
+function most_viewed_list ( $list_length = '10', $before_list = '<ul>', $after_list = '</ul>', $before_item = '<li>', $after_item = '</li>') {
+	
 	global $bbdb;
 	
+	if (bb_get_option('bb_db_version')>1600) {   // bbPress 1.0
+	
+	$most_viewed = (array) $bbdb->get_results("SELECT object_id as topic_id, meta_value FROM $bbdb->meta WHERE object_type='bb_topic' AND meta_key='views' ORDER BY cast(meta_value as UNSIGNED) DESC");
+	
+	} else {
+
 	$most_viewed = (array) $bbdb->get_results("SELECT topic_id, meta_value FROM $bbdb->topicmeta WHERE meta_key='views' ORDER BY cast(meta_value as UNSIGNED) DESC");
+	
+	}
+		
 	$most_viewed = array_slice($most_viewed, 0, $list_length);
 	
 	echo ($before_list);
@@ -122,11 +166,19 @@ function most_viewed_list ( $list_length = '10', $before_list = '<ul>', $after_l
 	echo ($after_list);
 }
 
-function most_viewed_table ( $list_length = '10')
-{
+function most_viewed_table ( $list_length = '10') {
 	global $bbdb;
 	
+	if (bb_get_option('bb_db_version')>1600) {   // bbPress 1.0
+	
+	$most_viewed = (array) $bbdb->get_results("SELECT object_id as topic_id, meta_value FROM $bbdb->meta WHERE object_type='bb_topic' AND meta_key='views' ORDER BY cast(meta_value as UNSIGNED) DESC");
+	
+	} else {
+
 	$most_viewed = (array) $bbdb->get_results("SELECT topic_id, meta_value FROM $bbdb->topicmeta WHERE meta_key='views' ORDER BY cast(meta_value as UNSIGNED) DESC");
+	
+	}
+		
 	$most_viewed = array_slice($most_viewed, 0, $list_length);
 	
 	?>
@@ -170,20 +222,4 @@ function most_viewed_table ( $list_length = '10')
 	<?php
 }
 
-if (!function_exists('is_tags')) {
-	function is_tags()
-	{
-		return is_tag();
-	}
-}
-
-add_action('bb_init', 'views_session_check');
-add_filter('bb_head', 'update_view_count');
-
-// only executes if option is set to 1 above
-if (is_front() || is_forum() || is_tags()) {
-	if ($append_to_title == 1) {
-		add_filter('topic_title', 'view_count_append_to_title', 99);
-	}
-}
 ?>

ebb1fac3c07568c2e4f1a46e2b9f524c2ac7b6c5
Ipstenu <Ipstenu@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-10-31 19:27:54 +0000

    To handle bbcode

---
+++ spoiler-bar/tags/0.4/spoiler-tags.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Spoiler Tags
+Plugin URI: http://bbpress.org/
+Description: Allows <spoiler></spoiler> tags to be posted in your forums.
+Author: Mika A. Epstein
+Author URI: http://ipstenu.org/
+Version: 0.4
+*/
+
+// You can add more tags here
+function add_spoiler_tags( $tags ) {
+        $tags['spoiler'] = array();
+        return $tags;
+}
+
+function spoiler_embed_callback( $content )
+{
+        return preg_replace_callback("|<spoiler>(.*?)</spoiler>|", 'bb_spoiler', $content);
+}
+
+
+function bb_spoiler( $matches )
+{
+        $text = $matches[0];
+        $text = str_replace("<spoiler>", "", $text);
+        $text = str_replace("</spoiler>", "", $text);
+        return "<span class=\"spoiler\">$text</span>";
+}
+
+function spoiler_embed_bbcode_callback($content)
+{
+      return preg_replace_callback("|\[spoiler\](.*?)\[/spoiler\]|", 'bbcode_spoiler', $content);
+}
+
+function bbcode_spoiler( $matches )
+{
+       $text = $matches[0];
+       $text = str_replace("[spoiler]", "", $text);
+       $text = str_replace("[/spoiler]", "", $text);
+       return "<span class=\"spoiler\">$text</span>";
+}
+
+add_filter('post_text', 'spoiler_embed_bbcode_callback', 1); // This replaces [spoiler] with the span
+add_filter('post_text', 'spoiler_embed_callback', 1); // This replaces <spoiler> with <span> stuff
+add_filter( 'bb_allowed_tags', 'add_spoiler_tags' ); // This adds spoilers to Allowed markup list
+
+
+?>

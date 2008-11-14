4fb8a59c00290691d8ae7f2097619a78624e1a10
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-11-14 19:17:38 +0000

    Shows some simple forum statistics at the bottom of your pages and links to a full summary display.

---
+++ mini-stats/trunk/mini-stats-init.php
@@ -0,0 +1,294 @@
+<?php
+
+add_action('bb_head','mini_stats_graph_header',100); 
+
+bb_get_header(); 
+echo '<h3 class="bbcrumb"><a href="'.bb_get_option('uri').'">'.bb_get_option('name').'</a> &raquo; '.__('Statistics').'</h3>';
+mini_stats_graphs();
+if (empty($_GET[$mini_stats['trigger']]))  {mini_stats_statistics();}
+echo '<br clear="both" />';
+bb_get_footer(); 
+exit;
+
+function mini_stats_graph_header() {global $mini_stats;  echo '<style type="text/css">'.$mini_stats['style_graph'].'</style>';}
+
+function mini_stats_graphs() { 
+global $bbdb, $mini_stats;
+$time=$_GET[$mini_stats['trigger']]; 
+if (empty($time)) {$time=time();} else {$time=strtotime($time); if (empty($time) || $time==-1) {$time=time();}}
+$gmt_offset=bb_get_option("gmt_offset")*3600;
+$today=gmdate("n/j",$time+$gmt_offset);
+$limit=31;
+$monthago=$time+$gmt_offset-($limit*24*3600);
+$maxheight=50;
+
+$label[1]=__("Daily Total Topics");
+$label[2]=__("Daily Total Posts");
+$label[3]=__("Daily Total Registrations");
+
+$day=$time+gmt_offset; // $monthago;
+do {					
+$empty[date('Y-m-d',$day)]->time=$day;
+$empty[date('Y-m-d',$day)]->posts=0;
+$empty[date('Y-m-d',$day)]->views=0;
+$empty[date('Y-m-d',$day)]->users=0;
+$day=$day-24*3600;
+} while ($day>$monthago);
+$empty=array_reverse($empty);
+
+if (bb_current_user_can('administrate')) {
+echo '<div style="float:right; font-size:12px; margin:0 0 -12px 0;">'.(empty($_GET[$mini_stats['trigger']]) ? '' : '[<a href="?'.$mini_stats['trigger'].'">'.__('reset').'</a>] ')
+.'[<a href="?'.$mini_stats['trigger'].'='.date('Y-n-j',$time-31*24*3600).'">'.__('last month').'</a>] [<a href="?'.$mini_stats['trigger'].'='.(date('Y',$time)-1).'-'.date('n-j',$time).'">'.__('last year').'</a>]</div>';
+}
+
+for ($loop=1; $loop<=3; $loop++) {
+
+if ($loop==1) {
+// topics per day
+$query="SELECT count(*) as topics, UNIX_TIMESTAMP(topic_time) as time FROM $bbdb->topics WHERE topic_status=0  AND UNIX_TIMESTAMP(topic_time)>$monthago GROUP BY DATE(topic_time) ORDER BY topic_time ASC LIMIT $limit";
+}
+elseif ($loop==2) {
+// posts per day
+$query="SELECT count(*) as posts, UNIX_TIMESTAMP(post_time) as time FROM $bbdb->posts WHERE post_status=0  AND UNIX_TIMESTAMP(post_time)>$monthago GROUP BY DATE(post_time) ORDER BY post_time ASC LIMIT $limit";
+}
+elseif ($loop==3) {
+// registrations per day
+$query="SELECT count(*) as users, UNIX_TIMESTAMP(user_registered) as time FROM $bbdb->users WHERE user_status=0  AND UNIX_TIMESTAMP(user_registered)>$monthago GROUP BY DATE(user_registered) ORDER BY user_registered ASC LIMIT $limit";
+}
+@$results=$bbdb->get_results($query);
+
+// make missing days have zero results
+unset($fill); foreach ($results as $result) {$fill[date('Y-m-d',$result->time)]=$result;} $results=array_merge($empty,$fill);
+
+    $count=0; unset($values); unset($labels); unset($colors);
+    foreach ($results as $result) {    	    	
+    	        if ($loop==1) {$values[$count]=$result->topics;}    		
+    	elseif ($loop==2) {$values[$count]=$result->posts;}
+    	elseif ($loop==3) {$values[$count]=$result->users;}	
+
+    	$labels[$count]=gmdate("n/j",$result->time+$gmt_offset); 
+    	if ($labels[$count]==$today) {$colors[$count]="#bbb";}     // #7799aa
+    	$count++; if ($count>$limit) {break;}
+    }    
+    $peak=$values; arsort($peak); $peak=array_keys($peak);
+    $colors[$peak[0]]="#cc0000";
+    $colors[$peak[1]]="#0000cc";
+    $colors[$peak[2]]="#00cc00";
+    $multiply=round($maxheight/(0.0001+$values[$peak[0]]),2);
+    $width=round(100/($count+0.0001),2); if ($width<1) {$width=1;} elseif ($width>100) {$width=50;}
+    
+$output= "<br clear='both' /><h3 align='center'>$label[$loop]</h3>\n<table class='mini_stats_graph'>";
+$output.="<tr valign='bottom'>";
+foreach (array_keys($values) as $count)  {
+	$output.="<td width='$width%'>";	
+	// if ($values[$count]>$values[$peak[20]]) {$output.="$values[$count]<div";} else {$output.="<div title='$values[$count]'";}
+	if ($values[$count]) {$output.="$values[$count]";}
+	$output.="<div style='height:".(1+$multiply*$values[$count])."px;"; // 
+	if (isset($colors[$count])) {$output.="background:$colors[$count];";}
+	$output.="'> </div></td>";
+}
+$output.="</tr>";
+
+$output.="<tr class='alt'>";
+foreach (array_keys($values) as $count)  {
+// if ($values[$count]>$values[$peak[20]]) {$output.="<td>$labels[$count]</td>";} else {$output.="<td title='$labels[$count]'> </td>";}
+$output.="<td>$labels[$count]</td>";
+}
+$output.="</tr>";
+
+$output.= "</table><br clear='both'>\n";
+
+echo $output;
+
+}	//  end graph loops
+}
+
+function mini_stats_statistics() {
+global $bbdb;
+
+
+echo  "<br clear='both' /><h3 align='center'>".__('Statistics')."</h3>\n<br clear='both' />";
+
+$topiclimit=10;  // top topics 10,15,20 etc.
+$userlimit=10;  // top users 10,15,20 etc.
+
+// SELECT count(*) as posts, count(distinct topic_id) as topics, count(distinct poster_id) as members FROM `bb_posts` WHERE post_status=0
+// SELECT count(*) as posts, count(distinct t1.topic_id) as topics, count(distinct poster_id) as members FROM `bb_posts` as t1  LEFT JOIN `bb_topics` as t2 ON t1.topic_id=t2.topic_id WHERE post_status=0 AND topic_status=0
+
+$results=$bbdb->get_results("SELECT COUNT(*) as forums, SUM(posts)  as posts, SUM(topics) as topics FROM $bbdb->forums");
+$statistics["total_forums"]=$results[0]->forums;
+$statistics["total_posts"]=$results[0]->posts;
+$statistics["total_topics"]=$results[0]->topics;
+
+$statistics["total_members"]=$bbdb->get_var("SELECT COUNT(*) FROM $bbdb->users");
+$statistics["total_days_old"]=round((time()-strtotime($bbdb->get_var("SELECT topic_start_time FROM $bbdb->topics ORDER BY topic_start_time LIMIT 1"))) / (3600 * 24),2);
+
+if (bb_get_option('bb_db_version')>1600) { // bbPress 1.0 vs 0.9
+$statistics["total_topic_views"]= $bbdb->get_var("SELECT SUM(meta_value) FROM $bbdb->meta LEFT JOIN $bbdb->topics ON $bbdb->meta.object_id = $bbdb->topics.topic_id  WHERE $bbdb->topics.topic_status=0 AND $bbdb->meta.meta_key='views' AND $bbdb->meta.object_type='bb_topic' ");
+$statistics["total_tags"]=$bbdb->get_var("SELECT COUNT(*) FROM $bbdb->terms");
+} else {	 // 0.9
+$statistics["total_topic_views"]= $bbdb->get_var("SELECT SUM(meta_value) FROM $bbdb->topicmeta LEFT JOIN $bbdb->topics ON $bbdb->topicmeta.topic_id = $bbdb->topics.topic_id  WHERE $bbdb->topics.topic_status=0 AND $bbdb->topicmeta.meta_key='views' ");
+$statistics["total_tags"]=$bbdb->get_var("SELECT COUNT(*) FROM $bbdb->tags");
+}
+
+$statistics["average_registrations"]=$statistics["total_members"]/$statistics["total_days_old"];
+$statistics["average_posts"]=$statistics["total_posts"]/$statistics["total_days_old"];
+$statistics["average_topics"]=$statistics["total_topics"]/$statistics["total_days_old"];
+
+$statistics["oldest_members"] = $bbdb->get_results("SELECT ID,user_registered FROM $bbdb->users ORDER BY user_registered ASC LIMIT $userlimit");
+$statistics["newest_members"] = $bbdb->get_results("SELECT ID,user_registered FROM $bbdb->users ORDER BY user_registered DESC LIMIT $userlimit");
+
+$statistics["top_posters"] = $bbdb->get_results("SELECT DISTINCT poster_id, COUNT(poster_id) AS post_count FROM $bbdb->posts WHERE post_status != 1 GROUP BY poster_id ORDER BY post_count DESC LIMIT $userlimit");
+$statistics["top_topic_starters"]=$bbdb->get_results("SELECT DISTINCT topic_poster, topic_poster_name, COUNT(topic_id) AS post_count FROM $bbdb->topics WHERE topic_status=0 GROUP BY topic_poster_name ORDER BY post_count DESC LIMIT $userlimit");
+
+$topics=$bbdb->get_results("SELECT * FROM $bbdb->topics WHERE topic_status=0 ORDER BY topic_posts DESC LIMIT $topiclimit"); 	// topic_id,topic_title,topic_posts
+$statistics["top_topics_by_posts"] = bb_append_meta( $topics, 'topic' );
+	
+if (!empty($statistics["total_topic_views"])) {
+	$statistics["average_topic_views"]=$statistics["total_topic_views"]/$statistics["total_days_old"];
+
+	if (bb_get_option('bb_db_version')>1600) { // bbPress 1.0 vs 0.9
+	$most_views = $bbdb->get_results("SELECT object_id as topic_id FROM $bbdb->meta WHERE meta_key='views' AND object_type='bb_topic' ORDER BY cast(meta_value as UNSIGNED) DESC LIMIT $topiclimit");
+	} else {
+	$most_views = $bbdb->get_results("SELECT topic_id FROM $bbdb->topicmeta WHERE meta_key='views' ORDER BY cast(meta_value as UNSIGNED) DESC LIMIT $topiclimit");
+	}
+	foreach (array_keys($most_views) as $i) {$trans[$most_views[$i]->topic_id] =& $most_views[$i];} $ids = join(',', array_keys($trans));
+	$where = apply_filters('get_latest_topics_where',"WHERE topic_status=0 AND topic_id IN ($ids) ");
+	$topics ="SELECT * FROM $bbdb->topics $where ORDER BY FIELD(topic_id, $ids)"; // topic_id,topic_title
+	$topics = $bbdb->get_results($topics);
+$statistics["top_topics_by_views"] = bb_append_meta( $topics, 'topic' );
+} else {
+	$statistics["top_topics_by_views"] = array(); 
+	$statistics["average_topic_views"]="";
+}
+
+?>
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:left;" id="latest">
+<thead>
+<tr><th>Totals</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php $totals=array("members","posts","topics","topic views","tags","forums","days old");
+foreach ($totals as $total) {
+$item=$statistics["total_".str_replace(" ","_",$total)];
+echo  "<tr".get_alt_class('plugin', $class)."><td> Total ".ucwords($total).":</td>
+	<td class=num>".bb_number_format_i18n($item,((int) $item.""==$item."" ? 0 : 2))."</td></tr>";
+} ?>
+</tbody>
+</table>
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:right;" id="latest">
+<thead>
+<tr><th>Averages</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php $averages=array("registrations","posts","topics","topic views");
+foreach ($averages as $average) {
+$item=$statistics["average_".str_replace(" ","_",$average)];
+echo  "<tr".get_alt_class('plugin', $class)."><td> Average ".ucwords($average)." per day:</td>
+	<td class=num>".bb_number_format_i18n($item,((int) $item.""==$item."" ? 0 : 2))."</td></tr>";
+} ?>
+</tbody>
+</table>
+
+<br clear="both" />
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:left;" id="latest">
+<thead>
+<tr><th>Top Topics (by posts)</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php
+foreach ($statistics["top_topics_by_posts"] as $top_topic) {
+echo  "<tr".get_alt_class('plugin', $class)."><td> <a href='".get_topic_link($top_topic->topic_id)."'>".$top_topic->topic_title."</a></td>
+	<td class=num>".bb_number_format_i18n($top_topic->topic_posts)."</td></tr>";
+} ?>
+</tbody>
+</table>
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:right;" id="latest">
+<thead>
+	<tr><th>Top Topics (by views)</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php
+foreach ($statistics["top_topics_by_views"] as $top_topic) {
+echo  "<tr".get_alt_class('plugin', $class)."><td> <a href='".get_topic_link($top_topic->topic_id)."'>".$top_topic->topic_title."</a></td>
+	<td class=num>".bb_number_format_i18n($top_topic->views)."</td></tr>";
+} ?>
+</tbody>
+</table>
+
+<br clear="both" />
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:left;" id="latest">
+<thead>
+<tr><th>Oldest Members</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php 
+unset($ids); foreach ($statistics["oldest_members"] as $member) {
+		$ids[$member->ID]=$member->ID;} 
+bb_cache_users($ids); 
+foreach ($statistics["oldest_members"] as $member) {
+echo  "<tr".get_alt_class('plugin', $class)."><td> <a href='" . attribute_escape( get_user_profile_link( $member->ID ) ) . "'>" .get_user_name($member->ID)."</a></td>
+	<td class='num'><span class='timetitle' title='". bb_datetime_format_i18n(bb_gmtstrtotime( $member->user_registered ), 'date')."'>".sprintf(__('%s'),bb_since(bb_gmtstrtotime( $member->user_registered )))."</span></td></tr>";
+} ?>
+</tbody>
+</table>
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:right;" id="latest">
+<thead>
+<tr><th>Newest  Members</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php 
+unset($ids); foreach ($statistics["newest_members"] as $member) {
+		$ids[$member->ID]=$member->ID;} 
+bb_cache_users($ids); 
+foreach ($statistics["newest_members"] as $member) {
+echo  "<tr".get_alt_class('plugin', $class)."><td> <a href='" . attribute_escape( get_user_profile_link( $member->ID ) ) . "'>" .get_user_name($member->ID)."</a></td>
+	<td class='num'><span class='timetitle' title='". bb_datetime_format_i18n(bb_gmtstrtotime( $member->user_registered ), 'date')."'>".sprintf(__('%s'),bb_since(bb_gmtstrtotime( $member->user_registered )))."</span></td></tr>";
+} ?>
+</tbody>
+</table>
+
+<br clear="both" />
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:left;" id="latest">
+<thead>
+<tr><th>Top Posters</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php 
+unset($ids); foreach ($statistics["top_posters"] as $top_poster) {
+		$ids[$top_poster->poster_id]=$top_poster->poster_id;} 
+bb_cache_users($ids); 
+foreach ($statistics["top_posters"] as $top_poster) {
+echo  "<tr".get_alt_class('plugin', $class)."><td> <a href='" . attribute_escape( get_user_profile_link( $top_poster->poster_id ) ) . "'>" .get_user_name($top_poster->poster_id)."</a></td>
+	<td class=num>".bb_number_format_i18n($top_poster->post_count)."</td></tr>";
+} ?>
+</tbody>
+</table>
+
+<table style="width:49%;margin:0 0 1em 0;clear:none;float:right;" id="latest">
+<thead>
+<tr><th>Top Topic Starters</th><th>#</th></tr>
+</thead>
+<tbody>
+<?php 
+unset($ids); foreach ($statistics["top_topic_starters"] as $top_topic_starter) {
+		$ids[$top_topic_starter->topic_poster]=$top_topic_starter->topic_poster;} 
+bb_cache_users($ids); 
+foreach ($statistics["top_topic_starters"] as $top_topic_starter) {
+echo  "<tr".get_alt_class('plugin', $class)."><td>  <a href='" . attribute_escape( get_user_profile_link( $top_topic_starter->topic_poster ) ) . "'>" .$top_topic_starter->topic_poster_name."</a></td>
+	<td class=num>".bb_number_format_i18n($top_topic_starter->post_count)."</td></tr>";
+} ?>
+</tbody>
+</table>
+
+<?php
+}
+
+?>
---
+++ mini-stats/trunk/mini-stats.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Plugin Name: Mini Stats
+Plugin URI: http://bbpress.org/plugins/topic/mini-stats
+Description: Some simple forum statistics.
+Author: _ck_
+Author URI: http://bbShowcase.org
+Version: 0.0.1
+
+License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+Donate: http://amazon.com/paypage/P2FBORKDEFQIVM
+*/
+
+// place a link anywhere in your templates you want to link to statistics with the trigger, ie.  <a href="?mini-stats">statistics</a>
+
+$mini_stats['statistics_in_footer']=true;			// true = automatic, no edits,  false =  manual placement via mini_stats();
+$mini_stats['statistics_only_on_front_page']=false;	// every page or only front-page
+$mini_stats['show_new_members']=true;			// show names of newest registered members
+$mini_stats['new_members']=3;		// how many new members to show if enabled;
+
+$mini_stats['trigger']="mini-stats";		// URL line option to show stats,  ie.  ?mini-stats
+$mini_stats['level']="read"; 			// read/participate/moderate/administrate  (access level, read = anyone)
+
+$mini_stats['icons']=bb_get_option('uri').trim(str_replace(array(trim(BBPATH,"/\\"),"\\"),array("","/"),dirname(__FILE__)),' /\\').'/icons/';  // web path to icons
+
+$mini_stats['style']="
+.mini_stats {text-align:center; line-height:160%;} 		
+.mini_stats_num, .mini_stats strong, mini_track a {color: #000080;}
+.mini_stats_num {font-family: verdana, arial, sans-serif; font-weight:bold; font-size: 90%;}
+.mini_stats_wrap {white-space: nowrap; zoom:1;}	
+.mini_stats img {margin:0px 2px -1px 2px; border:0; text-decoration:none;}
+.mini_stats_users {padding-left: 20px;  background: url(".$mini_stats['icons']."users.png) 1px 0px no-repeat;}
+.mini_stats_member {padding-left: 20px;  background: url(".$mini_stats['icons']."member.png) 1px 0px no-repeat;}
+.mini_stats_stats {padding-left: 20px;  background: url(".$mini_stats['icons']."stats.png) 1px 0px no-repeat;}	
+.mini_stats_users .mini_stats_num, .mini_stats_star .mini_stats_num {margin-left: -1px;}
+";
+
+$mini_stats['style_graph']="
+.mini_stats_graph {border:1px solid #aabbcc; float:left; font-size:11px; letter-spacing:-1px; word-spacing:-1px; border: 0; border-collapse: collapse; width:100%; margin:1em 0;}
+.mini_stats_graph TD {white-space:nowrap; overflow:hidden; text-align:center; border:0; border-collapse: collapse;}
+.mini_stats_graph TD div {font-size:1px;background:#aabbcc; margin: 0 1px; border-right: 1px solid #000000; border-top: 1px solid #dedede; border-left: 1px solid #dedede;}
+.mini_stats_graph TD span {width:100%;white-space:nowrap;}
+.mini_stats_graph2 {margin:1em 5px; width:48%; word-spacing:0;}
+.mini_stats_graph2 TD {text-align:left; vertical-align: middle; padding: 0 2px; height:20px;}
+.mini_stats_graph2 TD div {float: left; height:8px; font-size:1px; line-height:1px;  margin: 2px 5px 0 0; border-bottom: 1px solid #000000;}
+.mini_stats_graph .alt TD {border-top:1px solid #444; letter-spacing:-2px;}
+h3 {font-size:1.3em; position:relative; bottom: -10px; margin-top:-10px;}
+#latest th {text-align:center;}
+";
+
+/*  stop editing here  */
+
+if (isset($_GET[$mini_stats['trigger']])) {add_action('bb_init','mini_stats_init');}
+
+// statistics hooks
+add_action('bb_head','mini_stats_header',100); 
+if ($mini_stats['statistics_in_footer']) {add_action('bb_foot','mini_stats',200);}
+add_action( 'bb_new_post','mini_stats_update');
+add_action( 'bb_delete_post','mini_stats_update');
+add_action('register_user','mini_stats_update');
+add_action('user_register','mini_stats_update');
+// add_action('profile_edited', 'mini_stats_update');	// unfortunately no clean way to hook cap changes if set inactive
+
+function mini_stats_header() {global $mini_stats;  echo '<style type="text/css">'.$mini_stats['style'].'</style>';}
+
+function mini_stats_init() {global $mini_stats; if ($mini_stats['level']=='read' || bb_current_user_can($mini_stats['level'])) {@require_once("mini-stats-init.php");}}
+
+function mini_stats($display=0) {
+global $mini_stats, $mini_stats_done, $mini_track_statistics_done; 
+if (!empty($mini_stats_done) || !empty($mini_track_statistics_done)) {return;} $mini_stats_done=true;   // only run once if manually called
+if ($mini_stats['statistics_only_on_front_page'] && !is_front()) {return;}
+if (empty($display)) {
+	if ($mini_stats['statistics_in_footer']) {$display=1;}
+	if ($mini_stats['show_new_members']) {$display=2;}
+	if ($display && $mini_stats['statistics_only_on_front_page'] && !is_front()) {$display=0;}
+}
+if ($display) {
+$results=bb_get_option('mini_stats'); if (empty($results)) {$results=mini_stats_update();}
+$months=ceil((time()-strtotime($results->days)) / (3600 * 24 * 30));
+$output="<div class='mini_stats'>";
+$output.=" <a href='".bb_get_option('uri')."?".$mini_stats['trigger']."'><span class='mini_stats_stats'>";
+$output.=" <span class='mini_stats_num'>".bb_number_format_i18n($results->posts)."</span></a> ".__('posts in'); 
+$output.=" <span class='mini_stats_num'>".bb_number_format_i18n($results->topics)."</span> ".__('topics over'); 
+$output.=" <span class='mini_stats_num'>$months</span> ".__('months by'); 
+$output.=" <span class='mini_stats_users mini_stats_num'>".bb_number_format_i18n($results->active)."</span> ".__('of')." <span class='mini_stats_num'>".bb_number_format_i18n($results->members)."</span> ".__('members.'); 
+$output.=" </span>";
+if ($display>1) {
+$output.="<span class='mini_stats_wrap mini_stats_member'>";
+if (!empty($results->latest)) {$output.=__('Latest:'); $uri=bb_get_option('uri')."profile.php?id="; foreach ($results->latest as $key=>$value) {$output.=" <a href='$uri$key'>$value</a>, ";}}
+$output=trim($output,", ")."</span>";
+}
+$output.="</div> ";
+echo $output;
+}
+}
+
+function mini_stats_update($x="") {
+global $bbdb, $mini_stats;
+$query="SELECT count(*) as posts, count(distinct topic_id) as topics, count(distinct poster_id) as active, min(post_time) as days FROM $bbdb->posts WHERE post_status=0";
+$results=$bbdb->get_results($query);
+$usertable=$bbdb->users; $usermeta=$bbdb->usermeta;
+$base="FROM $usertable as t1 LEFT JOIN $usermeta as t2 on t1.ID=t2.user_id WHERE user_status=0 AND (meta_key='".$bbdb->prefix.'capabilities'."' AND NOT (meta_value LIKE '%inactive%' OR meta_value LIKE '%blocked%'))";
+$query="SELECT user_login,ID $base ORDER BY user_registered DESC LIMIT ".$mini_stats['new_members'];
+$results2=$bbdb->get_results($query);
+$results2=array_reverse($results2);
+foreach ($results2 as $key=>$value) {$results[0]->latest[$value->ID]=$value->user_login;}
+$query="SELECT count(*) as members $base";
+$results2=$bbdb->get_results($query);
+$results[0]->members=$results2[0]->members;
+bb_update_option('mini_stats',$results[0]);
+return $results[0];
+}
+
+?>

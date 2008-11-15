e60f549218b7598c19998865862b7c3418b3b498
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-11-15 17:01:47 +0000

    Displays a list of related topics based on tags (and eventually keywords and manual selection). No template edits required.

---
+++ related-topics/trunk/readme.txt
@@ -0,0 +1,69 @@
+=== Related Topics  ===
+Tags: related, topics, tags, keywords, search, _ck_
+Contributors: _ck_
+Requires at least: 0.9
+Tested up to: trunk
+Stable tag: trunk
+Donate link: http://amazon.com/paypage/P2FBORKDEFQIVM
+
+Displays a list of related topics based on tags (and eventually keywords and manual selection). No template edits required.
+
+== Description ==
+
+Displays a list of related topics based on tags (and eventually keywords and manual selection). No template edits required.
+
+This early release is a proof-of-concept and only uses tags to find matches for now. 
+
+== Installation ==
+
+* Install the `related-topics.php`  file  into  `my-plugins/` and activate.
+
+* Change defaults if desired by editing options at the top of the plugin until an admin menu is made. 
+
+*  This plugin inserts itself into the topicmeta at the top of each topic automatically, no template edits required unless you want custom placement.
+
+* ONLY if you want CUSTOM placement, edit your `topic.php` template (or other template) to add  the info like so:
+`	
+<?php do_action('related_topics'); ?>
+	or
+<?php related_topics(); ?>
+`
+The first method is prefered because if you deactivate the plugin, you won't get errors. 
+But the second method will allow you to specify options like topic id and alternate before/after tags, ie. `related_topics(97,'<ul>','</ul>');`
+
+* it is highly recommend you put this line in your `bb-config.php`  which will help with database performance on active forums by loading all  bbPress options at once instead of piecemeal
+`
+$bb->load_options = true;
+`
+
+== Frequently Asked Questions ==
+
+= How does this determine related topics? =
+
+* It scores topics higher based on multiple tag matches and younger topic age.
+
+== License ==
+
+* CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+== Donate ==
+
+* http://amazon.com/paypage/P2FBORKDEFQIVM
+
+== History ==
+
+= Version 0.0.1 (2008-11-15) =
+
+* first public release for proof-of-concept feedback
+
+== To Do ==
+
+* reduce tag queries to single query for performance (0.9 vs 1.0 difficulty)
+
+* also check topic titles for keyword matches
+
+* eventually check posts for keyword matches (this feature may take some time due to technical limitations)
+
+* cache results (current adds a few queries per topic view)
+
+* manual add / manual exclude related topics to list
---
+++ related-topics/trunk/related-topics.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: Related Topics
+Plugin URI: http://bbpress.org/plugins/topic/mini-stats
+Description: Displays a list of related topics based on tags (and eventually keywords and manual selection)
+Author: _ck_
+Author URI: http://bbShowcase.org
+Version: 0.0.1
+
+License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+
+Donate: http://amazon.com/paypage/P2FBORKDEFQIVM
+*/
+
+$related_topics['automatic_in_topicmeta']=true;  // true/false, use false for manual placement
+$related_topics['topicmeta_position']=250; 	 // position in topicmeta items, high number = lower
+$related_topics['minimum_tag_match']=1;	 // how many tags must match to be related
+$related_topics['maximum_count']=5;		 // how many related items at most
+$related_topics['label']=__("Related Topics:");
+
+/*  stop editing here  */
+
+add_action('related_topic','related_topics');
+if ($related_topics['automatic_in_topicmeta']) {add_action('topicmeta','related_topics',$related_topics['topicmeta_position']);}
+
+function related_topics($topic_id=0,$before="<li>",$after="</li>") {
+global $related_topics, $topic;
+//   if (!bb_current_user_can('administrate')) {return;}	// debug
+if (empty($topic_id)) {$topic_id=$topic->topic_id;}
+$tags=get_topic_tags($topic_id);
+if (empty($tags)) {return;}
+
+foreach ($tags as $tag) {
+	$tag_topics=bb_get_tagged_topic_ids($tag->tag_id);
+	foreach ($tag_topics as $related_topic) {@$tag_count[$related_topic]++;}
+}
+
+unset($tag_count[$topic_id]);	// remove self
+if (!empty($tag_count)) {
+	ksort($tag_count,SORT_NUMERIC);	// newest topics first by high id
+	arsort($tag_count,SORT_NUMERIC); 	 // highest tag match count first
+	if (reset($tag_count)<$related_topics['minimum_tag_match']) {return;}
+
+	foreach ($tag_count as $related_topic=>$score) {
+		if ($score<$related_topics['minimum_tag_match']) {break;}
+		if (@++$count>$related_topics['maximum_count']) {break;}
+		$final[]=$related_topic;		
+	}
+
+	$output=$before.$related_topics['label']."<ol style='margin-left:1.5em;'>"; 
+	$links=related_topics_get_links($final);
+	foreach ($final as $related_topic) {$output.="<li>$links[$related_topic]</li>";} 	//  ($tag_count[$related_topic] - $related_topic)  // debug
+	$output.="</ol>".$after;
+	echo $output;
+} // empty tag_count
+
+}
+
+function related_topics_get_links($list) {
+global $bbdb;
+	$topics=$bbdb->get_results("SELECT topic_id,topic_slug,topic_title FROM $bbdb->topics WHERE topic_id IN (".implode(',',$list).")");
+	
+	$rewrite = bb_get_option( 'mod_rewrite' );
+	if ( $rewrite ) {if ( $rewrite === 'slugs' ) {$column = 'topic_slug';} else {$column = 'topic_id';}}
+	foreach ($topics as $topic) {
+		if ( $rewrite ) {
+			$link = bb_get_option('uri') . "topic/" . $topic->$column;
+		} else {					
+			$link=add_query_arg('id',$topic->topic_id, bb_get_option('uri').'topic.php');
+		}	
+		$links[$topic->topic_id]="<a href='$link'>$topic->topic_title</a>";
+	}
+return $links;	
+}
+
+?>

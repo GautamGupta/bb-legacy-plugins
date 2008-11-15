1191c50f39cc618611350d5923ceb27fe34d9f23
Nightgunner5 <Nightgunner5@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-11-15 21:54:51 +0000

    Typo

--- nospamuser/tags/0.5/bb-nospamuser.php
+++ nospamuser/tags/0.5/bb-nospamuser.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: bb-NoSpamUser
-Version: 0.5
+Version: 0.6
 Plugin URI: http://danielgilbert.de/nospamuser/
 Description: Prevents known Spam Users from registering on your forum.
 Author: Daniel Gilbert
@@ -24,7 +24,7 @@ function nospamuser_check($type, $data) {
 
 function nospamuser_check_email($r, $email) {
 	if ($r) {
-		$response = nopsamuser_check('email', $email);
+		$response = nospamuser_check('email', $email);
 		if ($response) bb_die('Your email address has been marked as spam by StopForumSpam.com.  Contact the forum administrator if you are not a spambot.');
 		return $r;
 	} else return $r;
@@ -32,7 +32,7 @@ function nospamuser_check_email($r, $email) {
 
 function nospamuser_check_username($username) {
 	if ($username) {
-		$response = nopsamuser_check('username', $username);
+		$response = nospamuser_check('username', $username);
 		if ($response) bb_die('Your username has been marked as spam by StopForumSpam.com.  Contact the forum administrator if you are not a spambot.');
 		return $username;
 	} else return $username;

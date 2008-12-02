52583c1eb90c5d6fe0ac56ca6624f342b577ed9f
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-12-02 10:19:32 +0000

    bad php string parsing on img tag check

--- bbcode-buttons/trunk/BBcode-buttons.php
+++ bbcode-buttons/trunk/BBcode-buttons.php
@@ -5,7 +5,7 @@ Plugin URI: http://bbpress.org/plugins/topic/114
 Description: Automatically adds an easy access button toolbar above the post textarea to allow quick tags in BBcode. This is an enhanced replacement for the Comment Quicktags plugin. No template editing required.
 Author: _ck_
 Author URI: http://bbShowcase.org
-Version: 0.0.5
+Version: 0.0.6
 
 License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
 
@@ -22,9 +22,9 @@ echo  "<scr"."ipt type='text/javascript' defer='defer'>
 	BBcodeButtons.push(new BBcodeButton('ed_italic','I','[i]','[/i]','i','padding-right:7px;font-style:italic;','italics'));
 	BBcodeButtons.push(new BBcodeButton('ed_under','U','[u]','[/u]','u','text-decoration:underline;','underline'));
 	BBcodeButtons.push(new BBcodeButton('ed_strike','S','[s]','[/s]','s','text-decoration:line-through;','strike through'));
-	BBcodeButtons.push(new BBcodeButton('ed_link','URL','','[/url]','a','text-decoration:underline;','make a link')); // special case
+	BBcodeButtons.push(new BBcodeButton('ed_link','URL','','[/url]','a','text-decoration:underline;','make a link')); 
 	BBcodeButtons.push(new BBcodeButton('ed_block','&#147;quote&#148;','[quote]','[/quote]','q','padding:0 1px 1px 1px;','quote'));";
-if (isset($tags['img'])) {echo "BBcodeButtons.push(new BBcodeButton('ed_img','IMG','[img]','[/img]','m',-1)); // special case";}
+if (isset($tags['img'])) {echo "BBcodeButtons.push(new BBcodeButton('ed_img','IMG','[img]','[/img]','m',-1));";}
 echo  "BBcodeButtons.push(new BBcodeButton('ed_ul','UL','[list]','[/list]','u','','unordered list'));
 	BBcodeButtons.push(new BBcodeButton('ed_ol','OL','[list=1]','[/list]','o','','ordered list'));
 	BBcodeButtons.push(new BBcodeButton('ed_li','LI','[*]','[/*]','l','','list item'));";	

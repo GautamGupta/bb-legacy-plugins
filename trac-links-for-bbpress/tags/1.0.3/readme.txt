9a1ef217e692093ddae86a476f6e534b77eea9c0
sambauers <sambauers@330c2647-7620-0410-81c2-8fc9ee8a0def>
2008-12-12 13:40:22 +0000

    trac-links-for-bbpress: tag version 1.0.3

---
+++ trac-links-for-bbpress/tags/1.0.3/readme.txt
@@ -0,0 +1,55 @@
+=== Trac links for bbPress ===
+Contributors: SamBauers
+Tags: Trac, development
+Requires at least: 0.9
+Tested up to: 1.0-alpha-2
+Stable tag: 1.0.3
+
+Allows the use of Trac shortcodes like #1234 for tickets and [1234] for
+changesets within post text.
+
+== Description ==
+
+This plugin allows forum users to insert quick links to Trac tickets and
+changesets inside bbPress posts.
+
+For example #123 will be automatically changed into a link to a ticket in Trac,
+[321] will change into a link to a changset.
+
+Multiple Trac installs can be specified and distinguished by appending an
+"intertrac" code in the numbers. e.g. [WP576] or #BB902
+
+== Installation ==
+
+1. If you don't have a /my-plugins/ directory in your bbpress installaltion, 
+   create it on the same level as config.php.
+
+2. Upload the folder into your /my-plugins/ directory
+
+3. Activate the plugin in your "Plugins" administration screen.
+
+Configuration can be carried out by editing the small config section at the top
+of the plugin file.
+
+== License ==
+
+Trac links for bbPress version 1.0.3<br />
+Copyright (C) 2008 Sam Bauers (http://unlettered.org/)
+
+Trac links for bbPress comes with ABSOLUTELY NO WARRANTY
+This is free software, and you are welcome to redistribute
+it under certain conditions.
+
+See accompanying license.txt file for details.
+
+== Version History ==
+
+* 1.0   :
+  <br />Initial Release
+* 1.0.1 :
+  <br />Stop numbered HTML entities from being turned into links
+* 1.0.2 :
+  <br />Stop capturing of character before the hash
+  <br />Stop transformation inside code blocks
+* 1.0.3 :
+  <br />Catch multiline code blocks of any lettter-case
---
+++ trac-links-for-bbpress/tags/1.0.3/trac-links-for-bbpress.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Trac links for bbPress
+Plugin URI: http://bbpress.org/plugins/topic/trac-links-for-bbpress/
+Description: Allows the use of Trac shortcodes like #1234 for tickets and [1234] for changesets within post text.
+Author: Sam Bauers
+Author URI: http://unlettered.org/
+Version: 1.0.3
+*/
+
+
+
+// Add a single Trac site here - this will be the primary Trac
+// which is addressable by basic codes like #123 or [4321]
+$tl_trac = 'http://trac.bbpress.org/';
+
+// Use these if you have multiple trac installs to reference, the
+// key needs to be prepended to the code like: #BB123 or [WP4321]
+$tl_intertrac = array(
+	'BB' => 'http://trac.bbpress.org/',
+	'BP' => 'http://backpress.automattic.com/',
+	'WP' => 'http://trac.wordpress.org/'
+);
+
+
+
+$tl_code_blocks = array();
+
+function tl_store_code_blocks( $text )
+{
+	global $tl_code_blocks;
+	$tl_code_blocks = array();
+
+	$tl_text_blocks = array();
+
+	$all_blocks = preg_split( '@(<code>.*</code>)@imsU', $text, -1, PREG_SPLIT_NO_EMPTY + PREG_SPLIT_DELIM_CAPTURE );
+
+	foreach ( $all_blocks as $block ) {
+		if ( strpos( $block, '<code>' ) === 0 ) {
+			$tl_code_blocks[] = $block;
+		} else {
+			$tl_text_blocks[] = $block;
+		}
+	}
+
+	return join( '%%tl_code_block%%', $tl_text_blocks );
+}
+
+function tl_restore_code_blocks( $text )
+{
+	if ( strpos( $text, '%%tl_code_block%%' ) === false ) {
+		return $text;
+	}
+
+	global $tl_code_blocks;
+
+	foreach ( $tl_code_blocks as $block ) {
+		$text = preg_replace( '@%%tl_code_block%%@', $block, $text, 1 );
+	}
+
+	$tl_code_blocks = array();
+
+	return $text;
+}
+
+function tl_make_tracable( $text )
+{
+	global $tl_trac;
+	global $tl_intertrac;
+	if ( !$tl_trac ) {
+		return $text;
+	}
+
+	// Check if anything in the text matches the basic form of the patterns at all
+	if ( !preg_match('@(?:#[a-z0-9]+)|(?:\[[a-z0-9]+\])@i', $text ) ) {
+		return $text;
+	}
+
+	// Strip out code blocks and save them
+	$text = tl_store_code_blocks( $text );
+$_text = $text;
+	$tracs = array( $tl_trac );
+	if ( isset( $tl_intertrac ) && is_array( $tl_intertrac ) ) {
+		$tracs = array_merge( $tracs, $tl_intertrac );
+	}
+
+	$_tracs = array();
+	$i = 0;
+	foreach ( $tracs as $trac_id => $trac_url ) {
+		if ( $trac_url ) {
+			if ( !$parsed_trac_url = parse_url( $trac_url ) ) {
+				continue;
+			}
+			if ( $trac_id === 0 ) {
+				$trac_id = '';
+			}
+			$trac_url = rtrim( $trac_url, '/' ) . '/';
+			$_tracs[$i][0][0] = '([^&]?)#' . preg_quote( $trac_id ) . '([0-9]+)';
+			$_tracs[$i][0][1] = '$1';
+			$_tracs[$i][0][2] = $trac_url . 'ticket/$2';
+			$_tracs[$i][0][3] = '#' . $trac_id . '$2';
+			$_tracs[$i][1][0] = '\[' . preg_quote( $trac_id ) . '([0-9]+)\]';
+			$_tracs[$i][1][1] = '';
+			$_tracs[$i][1][2] = $trac_url . 'changeset/$1';
+			$_tracs[$i][1][3] = '[' . $trac_id . '$1]';
+			$i++;
+		}
+	}
+
+	if ( !count( $_tracs ) ) {
+		return $text;
+	}
+
+	foreach ( $_tracs as $pairs ) {
+		foreach ( $pairs as $pair ) {
+			$text = preg_replace( '@' . $pair[0] . '@', $pair[1] . '<a href="' . $pair[2] . '">' . $pair[3] . '</a>', $text );
+		}
+	}
+
+	// Put the code blocks back in
+	$text = tl_restore_code_blocks( $text );
+
+	return $text;
+}
+add_filter( 'post_text', 'tl_make_tracable', 1 );
+?>

c8d6cfe4b61884a4d83fd88e068f0fdf03763d96
Ben L. <nightgunner5@llamaslayers.net>
2009-01-03 23:11:10 +0000

    Initial Commit

---
+++ bbpress-moderation-suite/tags/0.1-alpha1/core.php
@@ -0,0 +1,149 @@
+<?php
+/*
+Plugin Name: bbPress Moderation Suite
+Description: A set of tools to help moderate your forums.
+Plugin URI: http://llamaslayers.net/daily-llama/tag/bbpress-moderation-suite
+Author: Nightgunner5
+Author URI: http://llamaslayers.net/
+Version: 0.1-alpha1
+*/
+
+function bbmodsuite_init() {
+	global $bbmod_plugins, $active_bbmod_plugins;
+	$bbmod_plugins = array(
+		'report' => array(
+			'name' => 'Report',
+			'description' => '<p>Allows users to report posts for consideration by the moderation team.</p>',
+			'filename' => 'report.php'
+		)
+	);
+	$active_bbmod_plugins = (array) bb_get_option('bbpress_moderation_suite_helpers');
+	foreach ($active_bbmod_plugins as $plugin) {
+		require_once $bbmod_plugins[$plugin]['filename'];
+	}
+}
+add_action('bb_init', 'bbmodsuite_init');
+
+function bbmodsuite_admin_add() {
+	global $bb_submenu;
+	$plugins = array($bb_submenu['plugins.php'][5]);
+	$plugins[] = array(__('bbPress Moderation Suite'), 'use_keys', 'bbpress_moderation_suite');
+	$bb_submenu['plugins.php'] = array_merge($plugins, array_slice($bb_submenu['plugins.php'], 1));
+}
+add_action('bb_admin_menu_generator', 'bbmodsuite_admin_add');
+
+function bbmodsuite_admin_parse() {
+	global $bbmod_plugins, $active_bbmod_plugins;
+	$plugin = $_GET['mod_helper'];
+	$action = $_GET['action'];
+	if ($plugin && $action && bb_verify_nonce($_GET['_wpnonce'], $action . '-plugin_' . $plugin)) {
+		switch ($action) {
+			case 'activate':
+				if (in_array($plugin, $active_bbmod_plugins) ||
+					!isset($bbmod_plugins[$plugin])) break;
+				$active_bbmod_plugins[] = $plugin;
+				bb_update_option('bbpress_moderation_suite_helpers', $active_bbmod_plugins);
+				require_once $bbmod_plugins[$plugin]['filename'];
+				call_user_func('bbmodsuite_' . $plugin . '_install');
+				bb_admin_notice(sprintf(__('Plugin "%s" <strong>activated</strong>'), $bbmod_plugins[$plugin]['name']));
+				break;
+			case 'deactivate':
+				if (!in_array($plugin, $active_bbmod_plugins) ||
+					!isset($bbmod_plugins[$plugin])) break;
+				$active_bbmod_plugins = array_flip($active_bbmod_plugins);
+				unset($active_bbmod_plugins[$plugin]);
+				$active_bbmod_plugins = array_flip($active_bbmod_plugins);
+				bb_update_option('bbpress_moderation_suite_helpers', $active_bbmod_plugins);
+				bb_admin_notice(sprintf(__('Plugin "%s" <strong>deactivated</strong>'), $bbmod_plugins[$plugin]['name']));
+			case 'uninstall':
+				if (!in_array($plugin, $active_bbmod_plugins) ||
+					!isset($bbmod_plugins[$plugin])) break;
+				$active_bbmod_plugins = array_flip($active_bbmod_plugins);
+				unset($active_bbmod_plugins[$plugin]);
+				$active_bbmod_plugins = array_flip($active_bbmod_plugins);
+				bb_update_option('bbpress_moderation_suite_helpers', $active_bbmod_plugins);
+				call_user_func('bbmodsuite_' . $plugin . '_uninstall');
+				bb_admin_notice(sprintf(__('Plugin "%s" <strong>deactivated</strong> and <strong>uninstalled</strong>'), $bbmod_plugins[$plugin]['name']));
+		}
+	}
+}
+add_action('bbpress_moderation_suite_pre_head', 'bbmodsuite_admin_parse');
+
+function bbpress_moderation_suite() {
+	global $bbmod_plugins, $active_bbmod_plugins;
+?>
+<h2><?php _e('bbPress Moderation Suite'); ?></h2>
+
+<table class="widefat">
+	<thead>
+		<tr>
+			<th><?php _e('Moderation Assistants'); ?></th>
+			<th><?php _e('Description'); ?></th>
+			<th class="action"><?php _e('Actions'); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+
+<?php
+	foreach ( $bbmod_plugins as $plugin => $plugin_data ) :
+		$class = '';
+		$action = 'activate';
+		$action_class = 'edit';
+		$action_text = __('Activate');
+		if ( in_array($plugin, $active_bbmod_plugins) ) {
+			$class =  'active';
+			$action = 'deactivate';
+			$action_class = 'delete';
+			$action_text = __('Deactivate');
+		}
+		$href = attribute_escape(
+			bb_nonce_url(
+				bb_get_uri(
+					'bb-admin/admin-base.php',
+					array(
+						'mod_helper' => urlencode($plugin),
+						'action' => $action,
+						'plugin' => 'bbpress_moderation_suite'
+					),
+					BB_URI_CONTEXT_A_HREF + BB_URI_CONTEXT_BB_ADMIN
+				),
+				$action . '-plugin_' . $plugin
+			)
+		);
+?>
+
+		<tr<?php alt_class( 'normal_plugin', $class ); ?>>
+			<td><?php echo $plugin_data['name']; ?></td>
+			<td>
+				<?php echo $plugin_data['description']; ?>
+			</td>
+			<td class="action">
+				<a class="<?php echo $action_class; ?>" href="<?php echo $href; ?>"><?php echo $action_text; ?></a>
+<?php if (in_array($plugin, $active_bbmod_plugins)) { ?>
+				<a class="delete" href="<?php echo attribute_escape(
+	bb_nonce_url(
+		bb_get_uri(
+			'bb-admin/admin-base.php',
+			array(
+				'mod_helper' => urlencode($plugin),
+				'action' => 'uninstall',
+				'plugin' => 'bbpress_moderation_suite'
+			),
+			BB_URI_CONTEXT_A_HREF + BB_URI_CONTEXT_BB_ADMIN
+		),
+		'uninstall-plugin_' . $plugin
+	)
+); ?>"><?php _e('Uninstall'); ?></a>
+<?php } ?>
+			</td>
+		</tr>
+
+<?php
+	endforeach;
+?>
+
+	</tbody>
+</table>
+<?php
+}
+?>
---
+++ bbpress-moderation-suite/trunk/readme.txt
@@ -0,0 +1,54 @@
+=== bbPress Moderation Suite ===
+Contributors: Nightgunner5
+Tags: moderation
+Requires at least: 0.9
+Tested up to: trunk
+Stable tag: 0.1-alpha1
+
+A set of tools to help moderate your forums.
+
+== Description ==
+
+bbPress Moderation Suite is a set of tools to help moderate your forums.  There are multiple parts, each able to function separately from the others.  You can activate or deactivate each part separately.  It even includes an uninstaller so if you don't want to use a part anymore, you can remove all of its database usage!
+
+= Report =
+
+`Report` allows users on your forum to report posts to the moderators on your forum. Believe me, if your moderators go on your forums regularly, they'll see the reports very quickly.
+
+= Mod Log =
+
+Being programmed.
+
+= Ban Plus =
+
+Being programmed.
+
+= Warning =
+
+Being programmed.
+
+= Probation =
+
+Being programmed.
+
+== Installation ==
+
+1. If you do not have a `my-plugins` folder, make one now in the same place as the folder `bb-admin`.
+1. Upload the entire `bbpress-moderation-suite` folder into your `my-plugins` folder.
+1. Activate the plugin through the 'Plugins' menu in bbPress
+1. Activate the parts of bbPress Moderation Suite that you would like to use and leave the others alone.
+
+== Frequently Asked Questions ==
+
+= The Report plugin isn't putting a `report` link anywhere =
+
+Your theme is using old functions.  Find `<?php post_ip_link(); ?> <?php post_edit_link(); ?> <?php post_delete_link(); ?>` or the like in your current theme's `post.php` file and change it to `<?php bb_post_admin(); ?>`.  If there's an if statement before it, delete that too.
+
+== Screenshots ==
+
+1. Can you spot what happened because there was a report?
+
+== Changelog ==
+
+0.1-alpha1
+* First public release

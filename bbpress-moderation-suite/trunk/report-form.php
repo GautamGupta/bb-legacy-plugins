9057d89150e1c5261f110d5aae0dee2bdcefa183
Nightgunner5 <Nightgunner5@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-01-03 23:11:10 +0000

    Initial Commit

---
+++ bbpress-moderation-suite/trunk/report-form.php
@@ -0,0 +1,25 @@
+<ol id="thread"><li>
+<?php
+bb_post_template();
+?>
+</li></ol>
+<h2>Report this post</h2>
+<p>
+	<label for="report_reason"><?php _e('What is your reason for reporting this post?'); ?>
+		<select name="report_reason" id="report_reason" tabindex="1">
+<?php foreach (bbmodsuite_report_reasons() as $id => $reason) { ?>
+			<option value="<?php echo $id; ?>"><?php echo $reason; ?></option>
+<?php } ?>
+			<option value="0" selected="selected"><?php _e('Other') ?></option>
+		</select>
+	</label>
+</p>
+<p>
+	<label for="report_content"><?php _e('Please give more information: (Plain text only, no HTML allowed.)'); ?>
+		<textarea name="report_content" cols="50" rows="8" id="report_content" tabindex="2"></textarea>
+	</label>
+</p>
+<p class="submit">
+  <input type="submit" id="postformsub" name="Submit" value="<?php echo attribute_escape( __('Send Report &raquo;') ); ?>" tabindex="3" />
+</p>
+

64d71019c1686847c0877f08a4a4553e2dd4b9ef
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-01-04 21:51:14 +0000

    0.0.4 cache non-indexed query, compatibility with bbPress 1.0a5+

--- forum-last-poster/trunk/forum-last-poster.php
+++ forum-last-poster/trunk/forum-last-poster.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Forum Last Poster
 Description:  Adds `forum_last_poster()`, `forum_time()`, `forum_last_post_link()` and other functions to bbPress to mimic the topic tables' FRESHNESS column. Requires simple template edits.
-Plugin URI:  http://bbpress.org/plugins/topic/123
+Plugin URI:  http://bbpress.org/plugins/topic/forum-last-poster
 Author: _ck_
 Author URI: http://bbShowcase.org
-Version: 0.0.3
+Version: 0.0.4
 */
 
 function forum_last_poster($id=0) {topic_last_poster(forum_last_topic_id($id));}
@@ -20,18 +20,31 @@ function forum_last_post_link($id=0) {topic_last_post_link(forum_last_topic_id($
 
 function get_forum_last_post_link($id=0) {return get_topic_last_post_link(forum_last_topic_id($id));}
 
-function forum_last_topic_id($id = 0) {
-global $forums_last_topic_id, $forum, $bbdb, $bb_topic_cache; 
-if (!$id) {$id=$forum->forum_id;}
-if (!isset($forums_last_topic_id)) {	// unfortunately the join method does not handle deleted topics correctly and returns no results for that forum :-(
-// $last_topics = $bbdb->get_results("SELECT t1.forum_id as forum_id, t1.topic_id as topic_id FROM $bbdb->topics AS t1 LEFT JOIN $bbdb->topics AS t2 ON t1.forum_id=t2.forum_id AND t1.topic_time < t2.topic_time WHERE t1.topic_status=0 AND t2.forum_id IS NULL;");
-$last_topics = $bbdb->get_results("SELECT forum_id, topic_id FROM $bbdb->topics WHERE topic_last_post_id IN (SELECT MAX(topic_last_post_id) FROM $bbdb->topics WHERE topic_status=0 GROUP BY forum_id)"); 
-foreach ($last_topics as $forum_last_topic_id) {
-	if ($temp=$forum_last_topic_id->forum_id) {
-		$forums_last_topic_id[$temp]=$forum_last_topic_id->topic_id;
-		if (!isset($bb_topic_cache[$forum_last_topic_id->topic_id])) {$add_cache[]->topic_id=$forum_last_topic_id->topic_id;}
-	}
-} if (!empty($add_cache)) {bb_cache_post_topics($add_cache);}	// cache topics not already in cache
-} return $forums_last_topic_id[$id];
+add_action( 'bb_new_post', 'forum_last_poster_update');
+add_action( 'bb_delete_post','forum_last_poster_update');
+
+function forum_last_poster_update($post_id=0) {
+	global $bbdb;
+	$query="SELECT forum_id, topic_id FROM $bbdb->topics 
+		WHERE topic_last_post_id IN (SELECT MAX(topic_last_post_id) FROM $bbdb->topics WHERE topic_status=0 GROUP BY forum_id)";
+	$last_topics = $bbdb->get_results($query); 
+	foreach ($last_topics as $last_topic) {$forum_last_poster[$last_topic->forum_id]=$last_topic->topic_id;}
+	bb_update_option('forum_last_poster',$forum_last_poster);
+	return $forum_last_poster;
 }
+
+function forum_last_topic_id($id = 0) {
+global $forums_last_topic_id, $forum, $bb_topic_cache, $wp_object_cache; 
+if (!$id) {$id=$forum->forum_id;}
+if (isset($forums_last_topic_id)) {return $forums_last_topic_id[$id];}
+$last_topics=bb_get_option('forum_last_poster');
+if (empty($last_topics)) {$last_topics=forum_last_poster_update();}
+foreach ($last_topics as $temp=>$id) {
+	$forums_last_topic_id[$temp]=$id;
+	if (!isset($bb_topic_cache[$id]) && !isset($wp_object_cache->cache['bb_topic'][$id])) {$add_cache[]->topic_id=$id;}
+} 
+if (!empty($add_cache)) {bb_cache_post_topics($add_cache);}	// cache topics not already in cache
+return $forums_last_topic_id[$id];
+}
+
 ?>

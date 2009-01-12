cc3d2087dfd50334c4a0b6841ffae8d5648aafbd
Ben L. <nightgunner5@llamaslayers.net>
2009-01-12 19:47:42 +0000

    I forgot to version bump...

--- bbpress-moderation-suite/tags/0.1-alpha3/core.php
+++ bbpress-moderation-suite/tags/0.1-alpha3/core.php
@@ -5,7 +5,7 @@ Description: A set of tools to help moderate your forums.
 Plugin URI: http://llamaslayers.net/daily-llama/tag/bbpress-moderation-suite
 Author: Nightgunner5
 Author URI: http://llamaslayers.net/
-Version: 0.1-alpha2
+Version: 0.1-alpha3
 */
 
 function bbmodsuite_init() {

20617a9351645b054bf5e219b1bcdbc73abb472c
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-03-13 07:19:28 +0000

    tag filter fine tuning

--- hidden-forums/trunk/hidden-forums-tag-filter.php
+++ hidden-forums/trunk/hidden-forums-tag-filter.php
@@ -13,22 +13,30 @@ add_filter( 'sort_tag_heat_map', 'hidden_forums_filter_tags',1);
 function hidden_forums_filter_tags(&$tags) {
 global $bbdb,$hidden_forums_list; 
 if (empty($hidden_forums_list)) {return;}
+$min=min($tags)-1; 	// attempt at query speedup
 
 if (defined('BACKPRESS_PATH')) { 	// bbpress 1.0
 	$query="SELECT name,count FROM $bbdb->terms AS t 
 		INNER JOIN $bbdb->term_taxonomy AS tt ON t.term_id = tt.term_id 
 		INNER JOIN $bbdb->term_relationships AS tr ON tt.term_taxonomy_id = tr.term_taxonomy_id
 		INNER JOIN $bbdb->topics as tp ON object_id=topic_id
-		WHERE tt.taxonomy='bb_topic_tag'  AND tt.count > 0 AND forum_id IN($hidden_forums_list)";
+		WHERE tt.taxonomy='bb_topic_tag'  AND tt.count >= $min AND forum_id IN($hidden_forums_list)";
 } else {  	// bbpress 0.9
 	$query="SELECT tag as name,t.tag_count as count FROM $bbdb->tags as t
 		INNER JOIN $bbdb->tagged as tt ON t.tag_id=tt.tag_id
 		INNER JOIN $bbdb->topics as tp ON tp.topic_id=tt.topic_id
-		WHERE t.tag_count> 0 AND forum_id IN($hidden_forums_list)";
+		WHERE t.tag_count >= $min AND forum_id IN($hidden_forums_list)";
 }
 
-$results=$bbdb->get_results($query); if (empty($results) || !is_array($results)) {return;}
-foreach ($results as $result) {$tags[$result->name]-=$result->count; if ($tags[$result->name]<1) {unset($tags[$result->name]);}}
+$results=$bbdb->get_results($query); 
+if (empty($results) || !is_array($results)) {return;} 
+foreach ($results as $result) {
+	if (isset($tags[$result->name])) {
+		$tags[$result->name]-=$result->count; 
+		if ($tags[$result->name]<1) {unset($tags[$result->name]);}
+	}
+}
+unset($results);	// force cleanup for large array
 }
 
 

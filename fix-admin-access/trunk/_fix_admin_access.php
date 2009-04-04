9732792bc86c55d9a330438bff266b8285a09c56
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-04-04 10:38:46 +0000

    make it more intelligent about custom table prefixes

--- fix-admin-access/trunk/_fix_admin_access.php
+++ fix-admin-access/trunk/_fix_admin_access.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Fix Admin Access
 Description:  restores keymaster access to control panel on bbPress and/or WordPress integration. This is an auto-load plugin and must have a leading underscore "_". Put into your plugin directory, access bbPress and/or WordPress once and then delete it from the directory.
-Plugin URI:  http://bbpress.org/plugins/
+Plugin URI:  http://bbpress.org/plugins/topic/86
 Author: _ck_
 Author URI: http://bbshowcase.org
-Version: 0.01
+Version: 0.0.2
 
 License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
 
@@ -16,12 +16,14 @@ Warning: This plugin assumes your keymaster is user id #1. It is rare but possib
 */
 
 function fix_admin_access() {
-global $bbdb,$wpdb; if (isset($bbdb)) {$db=$bbdb;} else {$db=$wpdb;}
+global $bb,$bbdb,$wpdb; if (isset($bbdb)) {$db=$bbdb;} else {$db=$wpdb;}
+if (empty($bbdb->prefix)) {$prefix="bb_";} else {$prefix=$bbdb->prefix;}
+if (empty($bb->wp_table_prefix)) {$wp_prefix="wp_";} else {$wp_prefix=$bb->wp_table_prefix;}
 $meta=array(
-'bb_capabilities' => 'a:1:{s:9:"keymaster";b:1;}',
-'wp_capabilities' => 'a:1:{s:13:"administrator";b:1;}',
-'wp_user_level' =>  '10'
-);
+$prefix.'capabilities' => 'a:1:{s:9:"keymaster";b:1;}',
+$wp_prefix.'capabilities' => 'a:1:{s:13:"administrator";b:1;}',
+$wp_prefix.'user_level' =>  '10'
+); 
 if ($db) {
 foreach ($meta as $key => $value) :
 	if ($db->get_var("SELECT meta_value FROM $db->usermeta WHERE meta_key = '$key' AND user_id = '1' LIMIT 1"))
@@ -31,5 +33,6 @@ endforeach;
 } 
 }
 add_action('bb_init','fix_admin_access',200);
+add_action('init','fix_admin_access',200);
 
 ?>

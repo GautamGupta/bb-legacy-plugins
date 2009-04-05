150b03921ac417b143afe3b41135bf3fb6bb4edf
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-04-05 04:49:05 +0000

    allow easy edit to change user id number

--- fix-admin-access/trunk/_fix_admin_access.php
+++ fix-admin-access/trunk/_fix_admin_access.php
@@ -5,17 +5,19 @@ Description:  restores keymaster access to control panel on bbPress and/or WordP
 Plugin URI:  http://bbpress.org/plugins/topic/86
 Author: _ck_
 Author URI: http://bbshowcase.org
-Version: 0.0.2
+Version: 0.0.3
 
-License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+Instructions:   This is an auto-load plugin and must have a leading underscore "_". 
+Put into your plugin directory, access bbPress and/or WordPress once and then delete it from the directory.
 
-Instructions:   This is an auto-load plugin and must have a leading underscore "_". Put into your plugin directory, access bbPress and/or WordPress once and then delete it from the directory.
-
-Warning: This plugin assumes your keymaster is user id #1. It is rare but possible you are no longer user #1 if you have deleted your original keymaster. In which case this won't work.
-
+Warning: This plugin assumes your keymaster is user id #1. 
+It is rare but possible you are no longer user #1 if you have deleted your original keymaster. In which case this won't work.
 */
 
 function fix_admin_access() {
+
+$id=1;	 	//  assume keymaster was user #1, you can change this to another number if you are positive it's different
+
 global $bb,$bbdb,$wpdb; if (isset($bbdb)) {$db=$bbdb;} else {$db=$wpdb;}
 if (empty($bbdb->prefix)) {$prefix="bb_";} else {$prefix=$bbdb->prefix;}
 if (empty($bb->wp_table_prefix)) {$wp_prefix="wp_";} else {$wp_prefix=$bb->wp_table_prefix;}
@@ -26,9 +28,9 @@ $wp_prefix.'user_level' =>  '10'
 ); 
 if ($db) {
 foreach ($meta as $key => $value) :
-	if ($db->get_var("SELECT meta_value FROM $db->usermeta WHERE meta_key = '$key' AND user_id = '1' LIMIT 1"))
-	            {$db->query("UPDATE $db->usermeta SET meta_value = '$value' WHERE user_id = '1' AND meta_key = '$key' LIMIT 1");}
-     	   else {$db->query("INSERT INTO $db->usermeta  (user_id, meta_key, meta_value)  VALUES ('1', '$key', '$value')");}
+	if ($db->get_var("SELECT meta_value FROM $db->usermeta WHERE meta_key = '$key' AND user_id = '$id' LIMIT 1"))
+	            {$db->query("UPDATE $db->usermeta SET meta_value = '$value' WHERE user_id = '$id' AND meta_key = '$key' LIMIT 1");}
+     	   else {$db->query("INSERT INTO $db->usermeta  (user_id, meta_key, meta_value)  VALUES ('$id', '$key', '$value')");}
 endforeach;
 } 
 }

25fa65902581cac7e8462d1371214df6644c6dfa
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-04-09 10:05:45 +0000

    0.0.2 try curl first with fsockopen fallback to handle safe mode, non-curl

--- check-for-updates/trunk/check-for-updates.php
+++ check-for-updates/trunk/check-for-updates.php
@@ -5,7 +5,7 @@ Description: Shows which bbPress plugins may be out of date.
 Plugin URI:  http://bbpress.org/plugins/topic/check-for-updates
 Author: _ck_
 Author URI: http://bbShowcase.org
-Version: 0.0.1
+Version: 0.0.2
 */ 
 if ((defined('BB_IS_ADMIN') && BB_IS_ADMIN) || !(strpos($_SERVER['REQUEST_URI'],"/bb-admin/")===false)) { // "stub" only load functions if in admin 	
 	function check_for_updates_admin_page() {global $bb_submenu; $bb_submenu['plugins.php'][] = array(__('Check for Updates'), 'administrate', 'check_for_updates');}

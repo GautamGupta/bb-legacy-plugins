607dbe7649bbe58658d3977813dbf63f187ebd33
Atsutane <Atsutane@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-04-27 22:18:28 +0000

    Add initial release.

---
+++ wordpress-latest-post/trunk/readme.txt
@@ -0,0 +1,39 @@
+=== Wordpress Latest Post ===
+Donate link: http://www.atsutane.net/
+Tags: bbpress, wordpress, latest, post, bbpress, topic, plugin, sidebar, page
+Contributors: Atsutane
+Requires at least: 0.9.4
+Tested up to: 1.0
+Stable Tag: trunk
+
+This plugin will generates Wordpress Latest Post list inside your bbpress forum.
+
+== Description ==
+
+This plugin will generates Wordpress Latest Post list inside your bbpress forum.
+
+If you like my work, Please give a link back.
+
+== Installation ==
+
+Simply download the Zip-Archive and extract all folder into your `my-plugins` directory. Then go into your BbPress administration page, click on Plugins and activate it. Open `wp2bbpress.php` with your favorite text editor to config the plugin.
+
+I’ve created one template tags you can use in your pages:
+
+1. `get_wp_postdata('title')` : Show Bbpress latest discussion on wp static page.
+
+Examples of use:
+
+`<?php if (function_exists('get_wp_postdata')) { get_wp_postdata('title'); } ?>`
+
+More Documentation <a href="http://www.atsutane.net/2009/04/wordpress-inside-bbpress-part-1.html">Here</a>.
+
+== Frequently Asked Questions ==
+
+-
+
+== Version History ==
+
+Version 0.1 (2009-04-28):
+
+* Initial Release.
---
+++ wordpress-latest-post/trunk/wp2bbpress.php
@@ -0,0 +1,67 @@
+<?php
+/*
+
+Plugin Name: Wordpress Latest Post
+Plugin URI: http://www.atsutane.net
+Description: Wordpress Latest Post On Bbpress
+Author: Atsutane Shirane
+Author URI: http://www.atsutane.net
+Version: 0.1
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Copyright 2009 Atsutane Shirane, Atsutane.net
+
+*/
+
+### Configuration
+$blog_address = 'http://localhost'; // Example: http://www.google.com
+$wp_table_prefix = 'wp_';
+
+### Function Get Wordpress Data
+function get_wp_postdata($title = '', $limit = 10, $before_title = '<h2>', $after_title = '</h2>', $before = '<ul>', $after = '</ul>') {
+	global $bbdb, $blog_address, $wp_table_prefix;
+	$wp_url = get_wp_permalink();
+	$wppost = $bbdb->get_results("SELECT * FROM ".$wp_table_prefix."posts WHERE post_type = 'post' AND post_status = 'publish' ORDER BY ID DESC LIMIT ".$limit);
+	echo $before_title.$title.$after_title.$before;
+	if ($wppost) {
+		foreach ($wppost as $wp) {
+			$wp_title = $wp->post_title;
+			$wp_date = strtotime($wp->post_date);
+			$wp_slug = $wp->post_name;
+			$wp_id = $wp->ID;
+			$wp_showurl = $wp_url;
+			$wp_showurl = str_replace("%year%", date('Y', $wp_date), $wp_showurl);
+			$wp_showurl = str_replace("%monthnum%", date('m', $wp_date), $wp_showurl);
+			$wp_showurl = str_replace("%day%", date('d', $wp_date), $wp_showurl);
+			$wp_showurl = str_replace("%hour%", date('H', $wp_date), $wp_showurl);
+			$wp_showurl = str_replace("%minute%", date('i', $wp_date), $wp_showurl);
+			$wp_showurl = str_replace("%second%", date('s', $wp_date), $wp_showurl);
+			$wp_showurl = str_replace("%postname%", $wp_slug, $wp_showurl);
+			$wp_showurl = str_replace("%post_id%", $wp_id, $wp_showurl);
+			$wp_posturl = $blog_address . $wp_showurl;
+			echo '<li><a href="'.$wp_posturl.'" title="'.$wp_title.'">'.$wp_title.'</a></li>';			
+		}
+	}
+	echo $after;
+}
+
+### Function Get Permalink Data
+function get_wp_permalink() {
+	global $bbdb, $wp_table_prefix;
+	$wpurl = $bbdb->get_row("SELECT * FROM ".$wp_table_prefix."options WHERE option_name = 'permalink_structure' LIMIT 1");
+	if ($wpurl) {
+			$wp_perma = $wpurl->option_value;
+	}
+	return $wp_perma;
+}
+
+?>

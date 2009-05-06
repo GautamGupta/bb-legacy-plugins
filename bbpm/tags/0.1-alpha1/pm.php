68adcd3973a6634b5df72c24e19ee2413a839a43
Ben L. <nightgunner5@llamaslayers.net>
2009-05-06 23:08:12 +0000

    Initial commit

---
+++ bbpm/tags/0.1-alpha1/pm.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/bb-load.php';
+
+bb_auth( 'logged_in' ); // Is the user logged in?
+
+global $bbpm;
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' && empty( $_POST['reply_to'] ) ) {
+	bb_check_admin_referer( 'bbpm-new' );
+
+	if ( !bb_current_user_can( 'write_posts' ) || ( function_exists( 'bb_current_user_is_bozo' ) && bb_current_user_is_bozo() ) ) 
+		bb_die( __( 'You are not allowed to write private messages.  Are you logged in?', 'bbpm' ) );
+	if ( !trim( $_POST['message'] ) )
+		bb_die( __( 'You need to actually submit some content!' ) );
+	if ( !trim( $_POST['title'] ) )
+		bb_die( __( 'Please enter a private message title.', 'bbpm' ) );
+	$to = new BP_User( trim( $_POST['to'] ) );
+	if ( !$to = $to->ID )
+		bb_die( __( 'You need to choose a valid person to send the message to.', 'bbpm' ) );
+	$redirect_to = $bbpm->send_message( $to, trim( stripslashes( $_POST['title'] ) ), $_POST['message'] );
+	if ( !$redirect_to )
+		bb_die( __( 'Either your outbox or the recipient\'s inbox is full.', 'bbpm' ) );
+	else
+		wp_redirect( $redirect_to );
+	exit;
+} elseif ( $_SERVER['REQUEST_METHOD'] == 'POST' && !empty( $_POST['reply_to'] ) ) {
+	$reply_to  = (int)$_POST['reply_to'];
+	
+	bb_check_admin_referer( 'bbpm-reply-' . $reply_to );
+
+	if ( !bb_current_user_can( 'write_posts' ) || ( function_exists( 'bb_current_user_is_bozo' ) && bb_current_user_is_bozo() ) ) 
+		bb_die( __( 'You are not allowed to write private messages.  Are you logged in?', 'bbpm' ) );
+	if ( !trim( $_POST['message'] ) )
+		bb_die( __( 'You need to actually submit some content!' ) );
+
+	$_reply_to = new bbPM_Message( $reply_to );
+
+	if ( !$_reply_to->exists )
+		bb_die( __( 'There was an error sending your message.', 'bbpm' ) );
+
+	$redirect_to = $bbpm->send_message( $_reply_to->from->ID == bb_get_current_user_info( 'ID' ) ? $_reply_to->to->ID : $_reply_to->from->ID, ( $_reply_to->reply ? '' : 'Re: ' ) . $_reply_to->title, $_POST['message'], $reply_to );
+	if ( !$redirect_to )
+		bb_die( __( 'Either your outbox or the recipient\'s inbox is full.', 'bbpm' ) );
+	else
+		wp_redirect( $redirect_to );
+	exit;
+}
+
+if ( isset( $_GET['delete'] ) && bb_verify_nonce( $_GET['_wpnonce'], 'bbpm-delete-' . $_GET['delete'] ) ) {
+	$bbpm->delete_message( $_GET['delete'] );
+	wp_redirect( bb_get_uri( 'pm' ) );
+}
+
+?>
---
+++ bbpm/tags/0.1-alpha1/readme.txt
@@ -0,0 +1,36 @@
+=== Bavatars ===
+Contributors: Nightgunner5
+Tags: private messaging, messages, pm
+Requires at least: 0.9
+Tested up to: trunk
+Stable tag: 0.1-alpha1
+
+bbPM - The new way to private message.
+
+== Description ==
+
+Users can send private messages to each other. There is threading of replies and individual messages can be deleted.
+
+== Installation ==
+
+1. Upload the entire `bbpm` plugin to the `my-plugins` directory of your bbPress installation. If you don't have one, create it!
+1. Activate the plugin in your administration panel.
+
+== Frequently Asked Questions ==
+
+= I use bbPress 0.9 and I get a 404 error when I click on Private Messages =
+You will need to add the following code to your .htaccess file. If your bbPress installation is not at /forums, change the code accordingly.
+
+`
+RewriteRule ^pm/?(.*)$ /forums/my-plugins/bbpm/index.php?$1 [L]
+`
+
+== Screenshots ==
+
+1. Members can send messages to each other as simply as typing in a username, title, and message.
+2. Replies are threaded, and the reply form is loaded using ajax.
+
+== Changelog ==
+
+* 0.1-alpha1
+	* First public release

e6209ba5129ca986f0046bdedf404215486c58e8
Nightgunner5 <Nightgunner5@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-06-15 12:43:28 +0000

    Release 0.1-alpha5

---
+++ bbpm/tags/0.1-alpha5/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/bb-load.php';
+
+if ( strpos( $_SERVER['REQUEST_URI'], bb_get_option( 'path' ) . 'pm' ) === false )
+	$_SERVER['REQUEST_URI'] = bb_get_option( 'path' ) . 'pm/' . substr( $_SERVER['REQUEST_URI'], strpos( $_SERVER['REQUEST_URI'], '?' ) + 1 );
+
+require_once dirname( __FILE__ ) . '/privatemessages.php';
+
+?>

f6e5ddf47032384f928108fef0d6175aad769ab8
bobrik <bobrik@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-07-06 12:31:06 +0000

    oops. now must be really 0.6.0

--- wordpress-bbpress-syncronization/trunk/bbwp-sync.php
+++ wordpress-bbpress-syncronization/trunk/bbwp-sync.php
@@ -4,7 +4,7 @@ Plugin Name: bbPress-WordPress syncronization
 Plugin URI: http://bobrik.name
 Description: Sync your WordPress comments to bbPress forum and back.
 Author: Ivan Babrou <ibobrik@gmail.com>
-Version: 0.5.2
+Version: 0.6.0
 Author URI: http://bobrik.name
 
 Copyright 2008 Ivan Babroŭ (email : ibobrik@gmail.com)

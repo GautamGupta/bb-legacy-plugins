c2add34d3f1b31bdf1469306c7d30b211cbd8b52
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-07-19 07:19:45 +0000

    fix view to cache all output properly

--- leaderboard/trunk/view.php
+++ leaderboard/trunk/view.php
@@ -1,32 +1,36 @@
-<h3 class="bbcrumb"><a href="<?php bb_option('uri'); ?>"><?php bb_option('name'); ?></a> &raquo; Leaderboard</h3>
+<?php	
+
+global $leaders,$page,$total;  $days=isset($_REQUEST['days']) ? intval($_REQUEST['days']) : 0;
+$output='
+<h3 class="bbcrumb"><a href="'.bb_get_option('uri').'">'.bb_get_option('name').'</a> &raquo; Leaderboard</h3>
 
 <div class="indent">
 
 <h2>Leaderboard</h2>
 
-<form class="days" method="get" action="<?php echo preg_replace( '|\/page\/[0-9]+?|', '/', $_SERVER["REQUEST_URI"]); ?>">
+<form class="days" method="get" action="'.preg_replace( '|\/page\/[0-9]+|', '/', $_SERVER["REQUEST_URI"]).'">
 
 	<ul class="leaderboard">
-	<li>View most active contributors: </li><?php $days=isset($_REQUEST['days']) ? intval($_REQUEST['days']) : 0; ?>	
-	<?php // echo "<li>in forum ".leaderboard_forum_list()."<br /></li>"; ?>
-	<li><input id="leadersall" name="days" value="0" onclick="this.form.submit()" type="radio" <?php if (!$days) {echo 'checked="checked"';} ?>> <label for="leadersall">Of All-Time</label></li>
-	<li><input id="leaders30" name="days" value="30" onclick="this.form.submit()" type="radio" <?php if ($days==30) {echo 'checked="checked"';} ?>> <label for="leaders30">From The Past Month</label></li>
-	<li><input id="leaders7" name="days" value="7" onclick="this.form.submit()" type="radio" <?php if ($days==7) {echo 'checked="checked"';} ?>> <label for="leaders7">From The Past Week</label></li>
-	<li><input id="leaders1"name="days" value="1" onclick="this.form.submit()" type="radio" <?php if ($days==1) {echo 'checked="checked"';} ?>> <label for="leaders1">From Today</label></li>
+	<li>View most active contributors: </li>	
+';
+// $output.='<li>in forum '.leaderboard_forum_list().'<br /></li>';
+$output.='
+	<li><input id="leadersall" name="days" value="0" onclick="this.form.submit()" type="radio" '.(!$days ? 'checked="checked"' : '').' <label for="leadersall">Of All-Time</label></li>
+	<li><input id="leaders30" name="days" value="30" onclick="this.form.submit()" type="radio" '.($days==30 ? 'checked="checked"' : '').' <label for="leaders30">From The Past Month</label></li>
+	<li><input id="leaders7" name="days" value="7" onclick="this.form.submit()" type="radio"  '.($days==7 ? 'checked="checked"' : '').' <label for="leaders7">From The Past Week</label></li>
+	<li><input id="leaders1"name="days" value="1" onclick="this.form.submit()" type="radio" '.($days==1 ? 'checked="checked"' : '').' <label for="leaders1">From Today</label></li>
 	</ul>	
 </form>
-
-<?php	
-	global $leaders,$page,$total; 
+';	
 	if (empty($leaders)) {
-		$output="	
+		$output.="	
 			<br />
 			<p>Sorry, there are no users for this selection, please try another.</p>
 			<br />
 		";
 	
 	} else { 
-		$output="
+		$output.="
 			<table id='latest' class='leaderboard'>
 			<tr>		
 			<th width='1'>".__('Rank')."</th>
@@ -56,17 +60,13 @@
 	$output.="</table>";
 } // end empty $leaders
 
-$output.='<div class="nav">'.get_page_number_links( $page, $total ).'</div>';
+$output.='<div class="nav">'.get_page_number_links( $page, $total ).'</div></div>';
 
 // $output.=" <small>updated ".bb_since($filemtime)." ago</small>";
 	
 echo $output;
 leaderboard_cache($template,$days,$forums,$output,$page);	
-?>
 
-</div>
-
-<?php
 function leaderboard_forum_list( $args = '' ) {
 global $forum_id, $forum;
 	$old_global = $forum;			

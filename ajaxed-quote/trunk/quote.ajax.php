86ddcc7b99ab20341a37faef7a828f661960a8a4
Detective <Detective@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-07-23 12:41:11 +0000

    new version 1.1 for bbPress 1.0.1

--- ajaxed-quote/trunk/quote.ajax.php
+++ ajaxed-quote/trunk/quote.ajax.php
@@ -1,37 +1,21 @@
 <?php
 
+if ( !isset( $_GET['quoted'] ) )
+	die('');
+
 require('../../bb-load.php');
 
-/// from php.net/htmlspecialchars
-function jschars($str)
-{
-    $str = mb_ereg_replace("\\\\", "\\\\", $str);
-    $str = mb_ereg_replace("\"", "\\\"", $str);
-    $str = mb_ereg_replace("'", "\\'", $str);
-    $str = mb_ereg_replace("\r\n", "\\n", $str);
-    $str = mb_ereg_replace("\r", "\\n", $str);
-    $str = mb_ereg_replace("\n", "\\n", $str);
-    $str = mb_ereg_replace("\t", "\\t", $str);
-    $str = mb_ereg_replace("<", "\\x3C", $str); // for inclusion in HTML
-    $str = mb_ereg_replace(">", "\\x3E", $str);
-    return $str;
-}
-
-if (bb_is_user_logged_in() && isset($_POST['quoted_id'])){
-	$quoted_id = (int) $_POST['quoted_id'];
+if ( bb_current_user_can('write_posts') ){
+	$quoted_id = (int) $_GET['quoted'];
 	bb_check_admin_referer('quote-' . $quoted_id);
-	if ($quoted_id <= 0)
-		bb_die("What are you doing??");
+	if ( $quoted_id <= 0 )
+		die('');
 	
-	$quoted_post = bb_get_quoted_post($quoted_id);
-	if ($quoted_post === false || empty($quoted_post))
-		bb_die("What are you doing??");
-		
-	$quoted_post = jschars($quoted_post) . "\\n";
+	$quoted_post = bb_get_quoted_post( $quoted_id );
+	if ( $quoted_post === false || empty( $quoted_post ) )
+		die('');
 	
-	die("document.getElementById('post_content').value += '$quoted_post';");
+	die($quoted_post);
 
 } else 
-	bb_die("What are you doing??");
-
-?>
+	die('');

bc8b786aa7dad14cd0722137e85ca8fc1ff6e40e
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-08-07 13:01:41 +0000

    mysql query optimization (100 times faster?!)

--- forum-last-poster/trunk/forum-last-poster.php
+++ forum-last-poster/trunk/forum-last-poster.php
@@ -27,8 +27,9 @@ function forum_last_poster_update($post_id=0) {
 	global $bbdb; $WHERE=""; $forum_last_poster=bb_get_option('forum_last_poster'); 
 	if (empty($forum_last_poster) || !is_array($forum_last_poster)) {$forum_last_poster=array();}
 	elseif (!empty($post_id)) {$post=bb_get_post($post_id); $WHERE=" AND forum_id=$post->forum_id ";}
-	$query="SELECT forum_id, topic_id FROM $bbdb->topics 
-		WHERE topic_last_post_id IN (SELECT MAX(topic_last_post_id) FROM $bbdb->topics WHERE topic_status=0 $WHERE GROUP BY forum_id)";
+	$query="SELECT s1.forum_id, s1.topic_id FROM $bbdb->topics AS s1
+		JOIN (SELECT MAX(topic_last_post_id) AS topic_last_post_id FROM $bbdb->topics WHERE topic_status=0 $WHERE GROUP BY forum_id) AS s2 
+		ON s1.topic_last_post_id = s2.topic_last_post_id";
 	$last_topics = $bbdb->get_results($query); 
 	foreach ($last_topics as $last_topic) {$forum_last_poster[$last_topic->forum_id]=$last_topic->topic_id;}
 	bb_update_option('forum_last_poster',$forum_last_poster);

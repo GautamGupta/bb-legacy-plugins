29a00c0f85f619153710857a6d1586dfb08e0185
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-08-18 19:38:11 +0000

    fix for 0.9.0.7 and 1.0.3 admin menu API security lockdown

--- all-settings/trunk/all-settings.php
+++ all-settings/trunk/all-settings.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: All Settings
-Plugin URI: http://bbpress.org/plugins/topic/133
+Plugin URI: http://bbpress.org/plugins/topic/all-settings
 Description:  Allows all settings, including hidden options to be edited in the admin control panel. Works exactly like the (little-known) WordPress feature. 
 Author: _ck_
 Author URI: http://bbShowcase.org
@@ -9,13 +9,13 @@ Version: 0.0.2
 
 License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
 
-Donate: http://amazon.com/paypage/P2FBORKDEFQIVM
+Donate: http://bbshowcase.org/donate/
 
 Instructions:   install, activate, look under Settings in admin menu for All Settings
 */
 
 if ((defined('BB_IS_ADMIN') && BB_IS_ADMIN) || !(strpos($_SERVER['REQUEST_URI'],"/bb-admin/")===false)) { // "stub" only load functions if in admin 	
-	function all_settings_admin_page() {global $bb_submenu; $bb_submenu['options-general.php'][] = array(__('All Settings'), 'use_keys', 'all_settings');}
+	function all_settings_admin_page() {bb_admin_add_submenu(__('All Settings'), 'use_keys', 'all_settings','options-general.php');}
 	add_action( 'bb_admin_menu_generator', 'all_settings_admin_page',200);	// try to be last menu feature
 	if (isset($_GET['plugin']) && $_GET['plugin']=="all_settings") {require_once("all-settings-admin.php");} // load entire core only when needed
 }

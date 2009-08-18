02494d0000f5ca63d389ddbb95ab4ee31bdc9c0c
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-08-18 19:40:11 +0000

    fix for 0.9.0.7 and 1.0.3 admin menu API security lockdown

--- mass-delete-users/trunk/mass-delete-users.php
+++ mass-delete-users/trunk/mass-delete-users.php
@@ -5,11 +5,11 @@ Plugin URI: http://bbpress.org/plugins/topic/mass-delete-users
 Description:  Allows administrators to physically delete multiple users at once from the bbPress / WordPress database based on a variety of search attributes.
 Author: _ck_
 Author URI: http://bbShowcase.org
-Version: 0.0.4
+Version: 0.0.5
 */
 
 if ((defined('BB_IS_ADMIN') && BB_IS_ADMIN) || !(strpos($_SERVER['REQUEST_URI'],"/bb-admin/")===false)) { // "stub" only load functions if in admin 	
-	function mass_delete_users_admin_page() {global $bb_submenu; $bb_submenu['users.php'][] = array(__('Mass Delete'), 'administrate', 'mass_delete_users');}
+	function mass_delete_users_admin_page() {bb_admin_add_submenu(__('Mass Delete'), 'administrate', 'mass_delete_users','users.php');}
 	add_action( 'bb_admin_menu_generator', 'mass_delete_users_admin_page',200);	// try to be last menu feature
 	if (isset($_GET['plugin']) && $_GET['plugin']=="mass_delete_users") {require_once("mass-delete-users-admin.php");}
 }

29ba109bbc7963bc76fefa9586f114bf6bd18ff4
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-08-18 19:38:33 +0000

    fix for 0.9.0.7 and 1.0.3 admin menu API security lockdown

--- mass-edit/trunk/mass-edit.php
+++ mass-edit/trunk/mass-edit.php
@@ -15,7 +15,7 @@ Instructions:   install, activate, look under Content in admin menu for Mass Edi
 */
 
 if ((defined('BB_IS_ADMIN') && BB_IS_ADMIN) || !(strpos($_SERVER['REQUEST_URI'],"/bb-admin/")===false)) { // "stub" only load functions if in admin 	
-	function mass_edit_admin_page() {global $bb_submenu; $bb_submenu['content.php'][] = array(__('Mass Edit'), 'administrate', 'mass_edit');}
+	function mass_edit_admin_page() {bb_admin_add_submenu( __('Mass Edit'), 'administrate', 'mass_edit','content.php');}
 	add_action( 'bb_admin_menu_generator', 'mass_edit_admin_page',200);	// try to be last menu feature
 	if (isset($_GET['plugin']) && $_GET['plugin']=="mass_edit") {require_once("mass-edit-admin.php");} // load entire core only when needed
 }

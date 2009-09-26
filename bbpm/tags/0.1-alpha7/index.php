382a7f9ab5af3f2db69ebb4b45b74d6b25886f1f
Ben L. <nightgunner5@llamaslayers.net>
2009-09-26 16:08:41 +0000

    Release 0.1-alpha7. It's taken long enough and most of the bugs are ironed out.

---
+++ bbpm/tags/0.1-alpha7/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package bbPM
+ * @version 0.1-alpha7
+ * @author Nightgunner5
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License, Version 3 or higher
+ */
+
+/**
+ * Load bbPress core
+ */
+require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/bb-load.php';
+
+if ( strpos( $_SERVER['REQUEST_URI'], bb_get_option( 'path' ) . 'pm' ) === false )
+	$_SERVER['REQUEST_URI'] = bb_get_option( 'path' ) . 'pm/' . $_SERVER['QUERY_STRING'];
+
+if ( !$template = bb_get_template( 'privatemessages.php', false ) ) {
+	$template = dirname( __FILE__ ) . '/privatemessages.php';
+}
+/**
+ * Load up the PM template
+ *
+ * @uses privatemessages.php (the default)
+ */
+require_once $template;
+
+?>

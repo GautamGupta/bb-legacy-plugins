8745fb494ebdc13830ae925a932110bfa250aa94
filosofo <filosofo@330c2647-7620-0410-81c2-8fc9ee8a0def>
2009-10-20 20:32:23 +0000

    more tweaks

--- login-free-posting/trunk/login-free-posting.php
+++ login-free-posting/trunk/login-free-posting.php
@@ -2,7 +2,7 @@
  /*
 Plugin Name: Login-Free Posting
 Plugin URI: 
-Description: Allows bbPress users to post without logging in, much like WordPress comments.
+Description: [Under Development] Allows bbPress users to post without logging in, much like WordPress comments.
 Version: 0.1-alpha
 Author: Austin Matzko
 Author URI: http://ilfilosofo.com/

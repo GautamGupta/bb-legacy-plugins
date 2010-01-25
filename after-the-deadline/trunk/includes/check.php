4199ee487da755681518c38bcc06ced7e8d972c5
Gautam Gupta <Gautam Gupta@330c2647-7620-0410-81c2-8fc9ee8a0def>
2010-01-25 14:03:08 +0000

    I like less code

--- after-the-deadline/trunk/includes/check.php
+++ after-the-deadline/trunk/includes/check.php
@@ -7,38 +7,37 @@
 
 /* Browsing Function */
 function atd_http( $url, $method = 'POST', $data = array() ){
-   if( class_exists( 'WP_Http' ) ){
-      $request = new WP_Http;
-      return wp_remote_retrieve_body( $request->request( $url, array( 'method' => $method, 'body' => $data, 'user-agent' => 'AtD/bbPress' ) ) );
+   if( class_exists( 'WP_Http' ) ){ //not necessarily as we avoid loading bb-load
+      return wp_remote_retrieve_body( wp_remote_request( $url, array( 'method' => $method, 'body' => $data, 'user-agent' => 'AtD/bbPress v' . ATD_VER ) ) );
    } elseif ( function_exists( 'curl_init' ) ) { // Use cURL
-         $ch = curl_init();
-         curl_setopt( $ch, CURLOPT_URL, $url );
-         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-         curl_setopt( $ch, CURLOPT_USERAGENT, 'AtD/bbPress' );
-         if( strtoupper( $method ) == 'POST' ){
-            curl_setopt( $ch, CURLOPT_POST, true );
-            curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $data ) );
-	 }
-         curl_setopt( $ch, CURLOPT_HEADER, false );
-         $source = curl_exec( $ch );
-         curl_close( $ch );
-         return $source;
+      $ch = curl_init();
+      curl_setopt( $ch, CURLOPT_URL, $url );
+      curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+      curl_setopt( $ch, CURLOPT_USERAGENT, 'AtD/bbPress' );
+      if( strtoupper( $method ) == 'POST' ){
+         curl_setopt( $ch, CURLOPT_POST, true );
+         curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $data ) );
+      }
+      curl_setopt( $ch, CURLOPT_HEADER, false );
+      $source = curl_exec( $ch );
+      curl_close( $ch );
+      return $source;
    } elseif ( function_exists( 'file_get_contents' ) ) { // use file_get_contents()
       if( strtoupper( $method ) == 'POST' && function_exists( 'stream_context_create' ) ){
          $opts = array('http' =>
-               array(
-                  'method' => 'POST',
-                  'user_agent' => 'AtD/bbPress',
-                  'content' => http_build_query( $data )
-               )
-            );
+            array(
+               'method' => 'POST',
+               'user_agent' => 'AtD/bbPress',
+               'content' => http_build_query( $data )
+            )
+         );
          $context = stream_context_create($opts);
          return file_get_contents( $url, false, $context );
       }else{
          return file_get_contents( $url );
       }
    } else {
-	 return false;
+      return false;
    }
 }
 

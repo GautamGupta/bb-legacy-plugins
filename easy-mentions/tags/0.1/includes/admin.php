889e81e6b3e0fdd4b7f64bd5c36277d93065d194
Gautam Gupta <gautam@gaut.am>
2010-01-28 17:38:31 +0000

    Everything seems fine, so tag 0.1

---
+++ easy-mentions/tags/0.1/includes/admin.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @package Easy Mentions
+ * @subpackage Admin Section
+ * @author Gautam Gupta (www.gaut.am)
+ * @link http://gaut.am/bbpress/plugins/easy-mentions/
+ */
+
+/**
+ * Check for Updates
+ * 
+ * @return string|bool Returns version if update is available, else false
+ * @uses WP_Http
+ */
+function em_update_check(){
+	$latest_ver = trim( wp_remote_retrieve_body( wp_remote_request( 'http://gaut.am/uploads/plugins/updater.php?pid=7&chk=ver&soft=bb&current=' . EM_VER ) ) );
+	if( $latest_ver && version_compare( $latest_ver, EM_VER, '>' ) )
+		return strval( $latest_ver );
+	
+	return false;
+}
+
+/**
+ * Makes a settings page for the plugin
+ * 
+ * @uses bb_option_form_element() to generate the page
+ */
+function em_options(){
+	global $em_plugopts;
+	
+	if ( $_POST['em_opts_submit'] == 1 ) { /* Settings have been recieved, now save them! */
+		bb_check_admin_referer( 'em-save-chk' ); /* Security Check */
+		/* Checks on options, also saving them */
+		$em_plugopts['link-to'] = ( strval( $_POST['link-to'] ) == 'website') ? 'website' : 'profile';
+		$em_plugopts['reply-link'] = ( intval( $_POST['reply-link'] ) == 1) ? 1 : 0;
+		bb_update_option( EM_OPTIONS, $em_plugopts );
+		bb_admin_notice( __( 'The options were successfully saved!', 'easy-mentions' ) );
+	}
+	
+	/* Check for Updates */
+	$ver = em_update_check();
+	if( $ver ) /* If available, then notify */
+		bb_admin_notice( sprintf( __( 'New version (%1$s) of Easy Mentions is available! Please download the latest version from <a href="%2$s">here</a>.', 'easy-mentions' ), $ver, 'http://bbpress.org/plugins/topic/easy-mentions/' ) );
+	
+	/* Options in an array to be printed */
+	$options = array(
+		'link-to' => array(
+			'title' => __( 'Link the user to profile or website?', 'easy-mentions' ),
+			'type' => 'radio',
+			'value' => $em_plugopts['link-to'] ? $em_plugopts['link-to'] : 'profile',
+			'options' => array(
+				'profile' => __( 'Profile', 'easy-mentions' ),
+				'website' => __( 'Website', 'easy-mentions' ),
+			),
+			'note' => __( 'If you selected the website option and the user\'s website does not exist, then the user will be linked to his or her profile page.', 'easy-mentions' )
+		),
+		'reply-link' => array(
+			'title' => __( 'Add a reply link below each post?', 'easy-mentions' ),
+			'type' => 'checkbox',
+			'value' => $em_plugopts['reply-link'] ? $em_plugopts['reply-link'] : '',
+			'options' => array(
+				'1' => __( 'Yes', 'easy-mentions' ),
+			),
+			'note' => sprintf( __( 'Before checking this option, please verify that there is a post form below the topic on each page. (<a href="%s">Help</a>)', 'easy-mentions' ), 'http://bbpress.org/plugins/topic/easy-mentions/faq/' )
+		)
+	);
+	?>
+	
+	<h2><?php _e( 'Easy Mention Options', 'easy-mentions' ); ?></h2>
+	<?php do_action( 'bb_admin_notices' ); ?>
+	<form method="post" class="settings options">
+		<fieldset>
+			<?php
+			foreach ( $options as $option => $args ) {
+				bb_option_form_element( $option, $args );
+			}
+			?>
+		</fieldset>
+		<fieldset class="submit">
+			<?php bb_nonce_field( 'em-save-chk' ); ?>
+			<input type="hidden" name="em_opts_submit" value="1"></input>
+			<input class="submit" type="submit" name="submit" value="Save Changes" />
+		</fieldset>
+		<p><?php printf( __( 'Happy with the plugin? Why not <a href="%1$s">buy the author a cup of coffee or two</a> or get him something from his <a href="%2$s">wishlist</a>?', 'easy-mentions' ), 'http://gaut.am/donate/EM/', 'http://gaut.am/wishlist/' ); ?></p>
+	</form>
+<?php
+}
+
+/**
+ * Adds a menu link to the setting's page in the Settings section
+ *
+ * @uses bb_admin_add_submenu()
+ */
+function em_menu_link() {
+	bb_admin_add_submenu( __( 'Easy Mentions', 'easy-mentions' ), 'administrate', 'em_options', 'options-general.php' );
+}
+
+add_action('bb_admin_menu_generator', 'em_menu_link', 8, 0); /* Adds a menu link to setting's page */
---
+++ easy-mentions/tags/0.1/includes/public.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @package Easy Mentions
+ * @subpackage Public Section
+ * @author Gautam Gupta (www.gaut.am)
+ * @link http://gaut.am/bbpress/plugins/easy-mentions/
+ */
+
+/**
+ * Links the users in the posts
+ *
+ * @see bp_activity_at_name_filter() BuddyPress Function
+ * 
+ * @param $content The content to be parsed
+ */
+function em_do_linking( $content ){
+	global $em_plugopts;
+	
+        preg_match_all( '/[@]+([A-Za-z0-9-_]+)/', $content, $usernames );
+
+        if ( !$usernames = array_unique( $usernames[1] ) ) /* Make sure there's only one instance of each username */
+                return $content;
+
+        foreach( (array)$usernames as $username ) {
+                if ( !$user = bb_get_user( $username, array( 'by' => 'login' ) ) ){ //check #1, by username
+			if ( !$user = bb_get_user( $username, array( 'by' => 'nicename' ) ) ) //check #2, by nicename
+				continue;
+                }
+
+                /* Increase the number of new @ mentions for the user - maybe later */
+                /*$new_mention_count = (int)bb_get_usermeta( $user_id, 'em_mention_count' );
+                bb_update_usermeta( $user_id, 'em_mention_count', $new_mention_count + 1 );*/
+		
+		if( 'website' == $em_plugopts['link-to'] ){
+			if( !$link = $user->user_url )
+				$link = get_user_profile_link( $user->ID );
+		}else{
+			$link = get_user_profile_link( $user->ID );
+		}
+		
+		if ( $link )
+			$content = str_replace( "@$username", "@<a href='" . $link . "'>$username</a>", $content ); //should we add rel='nofollow'?
+        }
+
+        return $content;
+}
+
+/**
+ * Enqueue the Reply Javascript
+ *
+ * @uses wp_enqueue_script()
+ */
+function em_js(){
+	global $em_plugopts;
+	//wp_enqueue_script( 'easy-mentions', EM_PLUGPATH . 'js/reply-uncompressed.js', array( 'jquery' ), EM_VER, true );
+	if( $em_plugopts['reply-link'] == 1 && bb_is_topic() && topic_is_open() && ( bb_is_user_logged_in() || ( function_exists( 'bb_is_login_required' ) && !bb_is_login_required() ) ) ) /* Check if script is needed */
+		wp_enqueue_script( 'easy-mentions', EM_PLUGPATH . 'js/reply.js', array( 'jquery' ), EM_VER, true );
+}
+
+/**
+ * Add reply link below each post
+ *
+ * @param $post_links Array of the links
+ * @param $args Array of args
+ */
+function em_reply_link( $post_links, $args ) {
+	global $em_plugopts;
+	
+	if( $em_plugopts['reply-link'] == 1 && bb_is_topic() && topic_is_open() && ( bb_is_user_logged_in() || ( function_exists( 'bb_is_login_required' ) && !bb_is_login_required() ) ) ) /* Check if link is needed */
+		$post_links[] = $args['before_each'].'<a class="reply_link" id="reply-' . $bb_post->post_id . '" style="cursor:pointer;">' . __( 'Reply', 'easy-mentions' ) . '</a>'.$args['after_each'];
+	
+        return $post_links;
+}
+
+add_action( 'post_text', 'em_do_linking', -999, 1 ); /* Do Linking */
+add_action( 'wp_print_scripts', 'em_js', 5, 0 ); /* Add JS for reply */
+add_filter( 'bb_post_admin', 'em_reply_link', 11, 2 ); /* Add reply link */

5a4cc8f5fa8c77b54984e0d46f41a7a1b920599f
Gautam Gupta <gautam@gaut.am>
2010-01-29 13:04:53 +0000

    Better Javascript

--- easy-mentions/trunk/js/reply-uncompressed.js
+++ easy-mentions/trunk/js/reply-uncompressed.js
@@ -7,14 +7,18 @@
  * @see reply.js for compressed version
  */
 
+/**
+ * That file is compressed by http://closure-compiler.appspot.com/home
+ * Method - Go there, paste this javascript there and compress with simple method (Advanced causes errors!)
+ */
+
 jQuery(document).ready(function() {
     jQuery(".reply_link").click(function () {
         var original = jQuery("#post_content").val();
         if( original != ''){
             original = original + '\n\n';
         }
-        var text = original+'<em>@<a href="'+jQuery(this).parent().children('a').get(0)+'">'+jQuery(this).parent().parent().parent().children('.threadauthor').children('p').children('strong').text()+'</a></em>\n\n';
-        jQuery("#post_content").val(text).focus();
+        jQuery("#post_content").val(original+'<em>@<a href="'+jQuery(this).siblings('a:first').get(0)+'">'+jQuery(this).parents('.threadpost').siblings().children('p').children('strong').text()+'</a></em>\n\n').focus();
         jQuery(document).scrollTop(jQuery('#postform').offset().top)
     });
 });

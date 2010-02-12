9988904272b4854064d2bc136cac32dce413da20
Gautam Gupta <Gautam Gupta@330c2647-7620-0410-81c2-8fc9ee8a0def>
2010-02-12 08:55:38 +0000

    Tagging 1.5

---
+++ after-the-deadline/tags/1.5/includes/admin.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @package After the Deadline
+ * @subpackage Admin Section
+ * @author Gautam Gupta (www.gaut.am)
+ * @link http://gaut.am/bbpress/plugins/after-the-deadline/
+ */
+
+/**
+ * Check for Updates
+ *
+ * @uses WP_Http
+ * @return string|bool Returns version if update is available, else false
+ */
+function atd_update_check() {
+	$latest_ver = trim( wp_remote_retrieve_body( wp_remote_request( $url, array( 'user-agent' => 'AtD/bbPress v' . ATD_VER ) ) ) );
+	if( $latest_ver && version_compare( $latest_ver, ATD_VER, '>' ) )
+		return $latest_ver;
+	
+	return false;
+}
+
+/**
+ * Makes a settings page for the plugin
+ * 
+ * @uses bb_option_form_element() to generate the page
+ */
+function atd_options() {
+	global $atd_plugopts, $atd_supported_langs;
+	if ( $_POST['atd_opts_submit'] == 1 ) { /* Settings have been received, now save them! */
+		
+		bb_check_admin_referer( 'atd-save-chk' ); /* Security Check */
+		
+		$atd_plugopts['lang'] = ( in_array( $_POST['lang'], array_keys( $atd_supported_langs ) ) ) ? $_POST['lang'] : 'en';
+
+		/* Save the options and notify user */
+		bb_update_option( ATD_OPTIONS, $atd_plugopts );
+		bb_admin_notice( __( 'The options have been successfully saved!', 'after-the-deadline' ) );
+	}
+	
+	/* Check for updates and if available, then notify */
+	if( $ver = atd_update_check() )
+		bb_admin_notice( sprintf( __( 'New version (v. %1$s) of After the Deadline is available! Please download the latest version from <a href="%2$s">here</a>.', 'after-the-deadline' ), $ver, 'http://bbpress.org/plugins/topic/after-the-deadline/' ), 'error' );
+	
+	/* Options in an array to be printed */
+	$atd_options = array(
+		'lang'	=> array(
+			'title'		=> __( 'Language', 'after-the-deadline' ),
+			'value' 	=> ( in_array( $atd_plugopts['lang'], array_keys( $atd_supported_langs ) ) ) ? $atd_plugopts['lang'] : 'en',
+			'type'		=> 'select',
+			'options'	=> $atd_supported_langs,
+			'note'		=> sprintf( __( 'Proofreading should be done for which language? The plugin currently supports the following languages - %s.', 'after-the-deadline' ), implode( ', ', $atd_supported_langs ) )
+		)
+	);
+	?>
+	
+	<h2><?php _e( 'After the Deadline Options', 'after-the-deadline' ); ?></h2>
+	<?php do_action( 'bb_admin_notices' ); ?>
+	<form method="post" class="settings options">
+		<fieldset>
+			<?php
+			foreach ( $atd_options as $option => $args )
+				bb_option_form_element( $option, $args );
+			?>
+		</fieldset>
+		<fieldset class="submit">
+			<?php bb_nonce_field( 'atd-save-chk' ); ?>
+			<input type="hidden" name="atd_opts_submit" value="1"></input>
+			<input class="submit" type="submit" name="submit" value="Save Changes" />
+		</fieldset>
+		<p><?php printf( __( 'Happy with the plugin? Why not <a href="%1$s">buy the author a cup of coffee or two</a> or get him something from his <a href="%2$s">wishlist</a>?', 'after-the-deadline' ), 'http://gaut.am/donate/AtD/', 'http://gaut.am/wishlist/' ); ?></p>
+	</form>
+<?php
+}
+
+/**
+ * Adds a menu link to the setting's page in the Settings section
+ *
+ * @uses bb_admin_add_submenu()
+ */
+function atd_menu_link() {
+	bb_admin_add_submenu( __( 'After the Deadline', 'after-the-deadline' ), 'administrate', 'atd_options', 'options-general.php' );
+}
+
+add_action( 'bb_admin_menu_generator', 'atd_menu_link', 3 ); /* Adds a menu link to setting's page */
---
+++ after-the-deadline/tags/1.5/includes/public.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @package After the Deadline
+ * @subpackage Admin Section
+ * @author Gautam Gupta (www.gaut.am)
+ * @link http://gaut.am/bbpress/plugins/after-the-deadline/
+ */
+
+/**
+ * Enqueue the Javascript and CSS
+ *
+ * @uses wp_enqueue_script()
+ * @uses wp_enqueue_style()
+ * @uses wp_localize_script()
+ */
+function atd_css_js() {
+	if ( bb_is_user_logged_in() || ( function_exists( 'bb_is_login_required' ) && !bb_is_login_required() ) ) {
+		global $atd_plugopts, $atd_supported_langs;
+		$i18n = array(
+				'rpc'				=> ATD_PLUGPATH . 'includes/check.php'										,
+				'api_key'			=> 'BB-' . md5( bb_get_uri() )											,
+				'lang'				=> ( in_array( $atd_plugopts['lang'], array_keys( $atd_supported_langs ) ) ) ? $atd_plugopts['lang'] : 'en'	,
+				'button_proofread'		=> __( 'Proofread'									, 'after-the-deadline' ),
+				'button_edit_text'		=> __( 'Edit Text'									, 'after-the-deadline' ),
+				'button_ok'			=> __( 'OK'										, 'after-the-deadline' ),
+				'button_cancel'			=> __( 'Cancel'										, 'after-the-deadline' ),
+				'menu_title_spelling'		=> __( 'Spelling'									, 'after-the-deadline' ),
+				'menu_title_repeated_word'	=> __( 'Repeated Word'									, 'after-the-deadline' ),
+				'menu_title_no_suggestions'	=> __( 'No suggestions'									, 'after-the-deadline' ),
+				'menu_option_explain'		=> __( 'Explain...'									, 'after-the-deadline' ),
+				'menu_option_ignore_once'	=> __( 'Ignore suggestion'								, 'after-the-deadline' ),
+				'menu_option_ignore_all'	=> __( 'Ignore all'									, 'after-the-deadline' ),
+				'menu_option_ignore_always'	=> __( 'Ignore always'									, 'after-the-deadline' ),
+				'menu_option_edit_selection'	=> __( 'Edit Selection...'								, 'after-the-deadline' ),
+				'message_no_errors_found'	=> __( 'No writing errors were found.'							, 'after-the-deadline' ),
+				'message_error_no_text'		=> __( 'Please enter some text in the post textbox to be checked!'			, 'after-the-deadline' ),
+				'message_error'			=> __( 'Error!'										, 'after-the-deadline' ),
+				'message_no_errors_found'	=> __( 'No writing errors were found!'							, 'after-the-deadline' ),
+				'message_no_errors'		=> __( 'No errors!'									, 'after-the-deadline' ),
+				'message_server_error_short'	=> __( 'There was a problem communicating with the After the Deadline service.'		, 'after-the-deadline' ),
+				'dialog_replace_selection'	=> __( 'Replace selection with'								, 'after-the-deadline' ),
+				'dialog_replace'		=> __( 'Replace'									, 'after-the-deadline' )
+			);
+		//wp_enqueue_script('after-the-deadline', ATD_PLUGPATH . 'scripts/atd.dev.js', array('jquery'), ATD_VER, true);
+		wp_enqueue_script(	'after-the-deadline', ATD_PLUGPATH . 'scripts/atd.js'	, array( 'jquery' )	, ATD_VER	);
+		wp_localize_script(	'after-the-deadline', 'AtD'				, $i18n					);
+		wp_enqueue_style(	'after-the-deadline', ATD_PLUGPATH . 'css/atd.css'	, false			, ATD_VER, 'all');
+	}
+}
+
+add_action( 'wp_print_scripts', 'atd_css_js', 2 ); /* Enqueues Script and Style */

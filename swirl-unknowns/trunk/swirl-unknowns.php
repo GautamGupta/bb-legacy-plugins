285b8badcd9d7c48a1fe95be2c86cdc05302f2e8
mr_pelle <mr_pelle@330c2647-7620-0410-81c2-8fc9ee8a0def>
2010-05-04 09:17:22 +0000

    Swirl Unknowns 0.5

--- swirl-unknowns/trunk/swirl-unknowns.php
+++ swirl-unknowns/trunk/swirl-unknowns.php
@@ -3,74 +3,75 @@
 Plugin Name: Swirl Unknowns
 Plugin URI: http://bbpress.org/plugins/topic/swirl-unknowns/
 Description: Non-logged-in users get redirected to a page of your choice. Based on <a href="http://blogwaffe.com/">Michael D. Adams</a>' <a href="http://bbpress.org/forums/topic/117">Force Login</a> plugin plus the <a href="http://bbpress.org/forums/topic/force-login-for-bbpress-101">voodoo code from Trent Adams and Sam Bauers</a>.
+Version: 0.5
 Author: mr_pelle
-Author URI:
-Version: 0.4.2.2
-Requires at least: 1.0.2
-Tested up to: 1.0.2
+Author URI: mailto:francesco.pelle@gmail.com
 */
 
 
 // global vars
-$default_swirl_page = bb_get_option('path')."index.php";
-$aap_confirmation = '';
-$swirl_confirmation = '';
+$su_default_redir_page = bb_get_option('path')."index.php";
+$redir_confirmation = '';
+$allow_confirmation = '';
 
 
 /**
  * Add admin page
  **/
-function swirl_unknowns_add_admin_page() {
-	bb_admin_add_submenu(__('Swirl Unknowns'), 'use_keys', 'swirl_unknowns_admin_page');
+function su_add_admin_page()
+{
+	bb_admin_add_submenu(__('Swirl Unknowns'), 'use_keys', 'su_admin_page');
 }
 
 
 /**
  * Admin page
  **/
-function swirl_unknowns_admin_page() {
-	global $aap_confirmation;
-	global $default_swirl_page;
-	global $swirl_confirmation;
+function su_admin_page()
+{
+	global $su_default_redir_page;
+	global $redir_confirmation;
+	global $allow_confirmation;
 	$path = bb_get_option('path');
 ?>
-	<link rel="stylesheet" href="<?=bb_get_plugin_uri(bb_plugin_basename(__FILE__))?>swirl-unknowns.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo bb_get_plugin_uri(bb_plugin_basename(__FILE__)) ?>swirl-unknowns.css" type="text/css" />
 	<h2><?php _e('Swirl Unknowns'); ?></h2>
 	<div id="container">
-	<h3><?php _e('Enter the address of the page non-logged-in users will be redirected to:'); ?></h3>
-	<form method="post" id="swirl_form"><fieldset>
-	<input type="hidden" name="action" value="swirl" />
-	<label for="swirl_page"><?php _e('Page address:'); ?></label>
-	<input type="text" name="swirl_page" id="swirl_page" value="<?=bb_get_option('swirl_page')?>" /><br /><br />
-	<label for="disable"><?php _e('Disable plugin?'); ?></label> <input type="checkbox" name="disable" id="disable" />
-	<input type="submit" name="submit" id="submit" value="Update" /><span id="swirl_confirmation"><?=$swirl_confirmation?></span>
-	</fieldset></form>
+	<h3><?php _e('Enter the address of the page non-logged-in users will be redirected to'); ?></h3>
+	<form method="post" id="redir_form">
+	<fieldset>
+		<label for="redir_page"><?php _e('Page address'); ?></label>
+		<input type="text" name="redir_page" id="redir_page" value="<?php echo bb_get_option('su_redir_page') ?>" /><br /><br />
+		<label for="disable"><?php _e('Disable plugin?'); ?></label> <input type="checkbox" name="disable" id="disable" />
+		<input type="submit" name="submit" id="submit" value="Update" /><span id="redir_confirmation"><?php echo $redir_confirmation ?></span>
+		<input type="hidden" name="action" value="redir" />
+	</fieldset>
+	</form>
 	<br />
-	<strong><?php _e('Notes:'); ?></strong>
+	<strong><?php _e('Notes'); ?></strong>
 	<ul class="notes">
-		<li><?php printf('Example: <code>%s</code>', $path."custom-page.php"); ?></li>
-		<li><?php printf('Example (no permalinks): <code>%s</code>', $path."topic.php?id=1"); ?></li>
-		<li><?php printf('Example (numeric permalinks activated): <code>%s</code>', $path."topic/1"); ?></li>
-		<li><?php printf('Example (name based permalinks activated): <code>%s</code>', $path."topic/topic-name"); ?></li>
-		<li><?php printf('You may also enter just <code>/</code> to redirect to the website root (i.e. <code>%s</code>).', bb_get_option('domain')."/"); ?></li>
-		<li><?php printf('If no address is entered, default page (<code>%s</code>) will be used.', $default_swirl_page); ?></li>
+		<li><?php printf('Example (no permalinks): <code>%s</code>.', $path."topic.php?id=1"); ?></li>
+		<li><?php printf('Example (numeric permalinks activated): <code>%s</code>.', $path."topic/1"); ?></li>
+		<li><?php printf('Example (name based permalinks activated): <code>%s</code>.', $path."topic/topic-name"); ?></li>
+		<li><?php _e('You may also enter <code>/custom-page.php</code> to redirect out of the forum.'); ?></li>
+		<li><?php printf('If no address is entered, default page (<code>%s</code>) will be used.', $su_default_redir_page); ?></li>
 	</ul>
 	<br />
-	<h3><?php _e('Allowed pages:'); ?></h3>
+	<h3><?php _e('Allowed pages'); ?></h3>
 	<ul class="allowed_pages">
 		<li><?php _e('bb-login.php'); ?></li>
 		<li><?php _e('bb-reset-password.php'); ?></li>
 		<li><?php _e('register.php'); ?></li>
 		<li><?php _e('xmlrpc.php'); ?></li>
 		<li><?php printf('%s', $path."bb-admin/"); ?></li>
-		<li><form method="post" id="aap_form">
-		<input type="hidden" name="action" value="allow_page" />
-		<input type="text" name="another_allowed_page" id="another_allowed_page" value="<?=bb_get_option('$another_allowed_page')?>" />
-		<span id="aap_confirmation"><?=$aap_confirmation?></span>
+		<li><form method="post" id="allow_form">
+			<input type="text" name="allowed_page" id="allowed_page" value="<?php echo bb_get_option('$su_allowed_page') ?>" />
+			<span id="allow_confirmation"><?php echo $allow_confirmation ?></span>
+			<input type="hidden" name="action" value="allow_page" />
 		</form></li>
 	</ul>
 	<br />
-	<strong><?php _e('Notes:'); ?></strong>
+	<strong><?php _e('Notes'); ?></strong>
 	<ul class="notes">
 		<li><?php _e('Allowed page address follows the rules above.'); ?></li>
 		<li><?php _e('If no address is entered, last entered page will be removed from allowed pages list.'); ?></li>
@@ -81,45 +82,46 @@ function swirl_unknowns_admin_page() {
 
 
 /**
- * Process admin page forms
+ * Process admin page
  **/
-function swirl_unknowns_admin_page_process() {
+function su_admin_page_process()
+{
 	if ($_POST)
 	{
-		if ($_POST['action'] == 'swirl') // request coming from "page address" form
+		if ($_POST['action'] == 'redir')
 		{
-			global $swirl_confirmation;
+			global $redir_confirmation;
 
 			if ($_POST['disable']) // disable plugin
 			{
-				bb_delete_option('swirl_page');
-				bb_delete_option('another_allowed_page'); // not really needed, but it's better to remove any trace of the plugin
-				$swirl_confirmation = "Plugin disabled";
+				bb_delete_option('su_redir_page');
+				bb_delete_option('su_allowed_page'); // not really needed, but it's better to remove any trace of the plugin
+				$redir_confirmation = "Plugin disabled";
 			}
-			else // update swirl page
+			else // update redirection page
 			{
-				global $default_swirl_page;
-				$swirl_page = ($_POST['swirl_page']) ?
-													$_POST['swirl_page'] :
-													$default_swirl_page;
+				global $su_default_redir_page;
 
-				bb_update_option('swirl_page', $swirl_page);
-				$swirl_confirmation = "Page updated";
+				// if $_POST['redir_page'] is not defined, use default redirection page
+				$redir_page = ($_POST['redir_page']) ? $_POST['redir_page'] : $su_default_redir_page;
+
+				bb_update_option('su_redir_page', $redir_page);
+				$redir_confirmation = "Page updated";
 			}
 		}
-		elseif ($_POST['action'] == 'allow_page') // request coming from "allow page" form
+		elseif ($_POST['action'] == 'allow')
 		{
-			global $aap_confirmation;
+			global $allow_confirmation;
 
-			if ($_POST['another_allowed_page']) // add another allowed page
+			if ($_POST['allowed_page']) // update allowed page
 			{
-				bb_update_option('another_allowed_page', $_POST['another_allowed_page']);
-				$aap_confirmation = "Page added";
+				bb_update_option('su_allowed_page', $_POST['allowed_page']);
+				$allow_confirmation = "Page added";
 			}
-			else // remove another allowed page
+			else // remove allowed page
 			{
-				bb_delete_option('another_allowed_page');
-				$aap_confirmation = "Page removed";
+				bb_delete_option('su_allowed_page');
+				$allow_confirmation = "Page removed";
 			}
 		}
 	}
@@ -127,10 +129,11 @@ function swirl_unknowns_admin_page_process() {
 
 
 /**
- * Redirect non-logged-in users to "swirl page", if the option is defined
+ * Redirection function
  **/
-function swirl_unknowns() {
-	if ($swirl_page = bb_get_option('swirl_page')) // plugin is active
+function swirl_unknowns()
+{
+	if ($redir_page = bb_get_option('su_redir_page')) // plugin is active
 	{
 		$path = bb_get_option('path');
 		$server_uri = $_SERVER['REQUEST_URI'];
@@ -142,26 +145,24 @@ function swirl_unknowns() {
 			&& strcasecmp($server_uri, $path.'xmlrpc.php') != 0
 			&& strcasecmp($server_uri, $path.'bb-admin/') != 0
 			&& strcasecmp($server_uri, $path."bb-login.php?re=".$path."bb-admin/") != 0
-			&& strcasecmp($server_uri, bb_get_option('another_allowed_page')) != 0 // no problem even if the option is not defined, as `$server_uri` will never be NULL
-			&& strcasecmp($server_uri, $swirl_page) != 0 // user is on swirl page
+			&& strcasecmp($server_uri, bb_get_option('su_allowed_page')) != 0 // no problem if the option is not defined
+			&& strcasecmp($server_uri, $redir_page) != 0 // user is already on the redirection page
 			)
 		{
 			nocache_headers();
-			header("HTTP/1.1 302 Moved Temporarily");
-			bb_safe_redirect($swirl_page);
-			header("Status: 302 Moved Temporarily");
+			bb_safe_redirect($redir_page);
 			exit();
 		}
 		else ; // user is requesting an allowed page
 	}
-	else ; // plugin disabled
+	else ; // plugin is disabled
 }
 
 
 /*
  * Add bbPress actions
  */
-add_action('bb_admin_menu_generator', 'swirl_unknowns_add_admin_page');
-add_action('bb_admin-header.php', 'swirl_unknowns_admin_page_process');
+add_action('bb_admin_menu_generator', 'su_add_admin_page');
+add_action('bb_admin-header.php', 'su_admin_page_process');
 add_action('bb_init', 'swirl_unknowns');
 ?>

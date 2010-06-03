6d2a1b4eb611473df2e6e2ec9a847eaa9f536424
xiligroup.dev <xiligroup.dev@330c2647-7620-0410-81c2-8fc9ee8a0def>
2010-06-03 06:56:26 +0000

    092

--- xili-new-post-notification/trunk/xili-bb-npn.php
+++ xili-new-post-notification/trunk/xili-bb-npn.php
@@ -2,18 +2,18 @@
 /**
  * Plugin Name: xili New Post Notification (xnpn)
  * Plugin Description: Sends a notification to a selected email when a new post is created.
- * Author: M from dev.xiligroup.com
+ * Author: michelwppi
  * Author URI: http://dev.xiligroup.com
- * Plugin URI: http://dev.xiligroup.com
- * Version: 0.9
+ * Plugin URI: http://forum2.dev.xiligroup.com/forum.php?id=4
+ * Version: 0.9.2
  */
 
 
 /*
 Changes :
-
+- 0.9.2 - 100509 - Add option to include content of post in email - thanks to Marcus -
 */
-define('XNPN_VER','0.9'); /* used in admin UI*/
+define('XNPN_VER','0.9.2'); /* used in admin UI*/
 
 // Add filters for the admin area
 add_action('bb_admin_menu_generator', 'bb_xnpn_configuration_page_add');
@@ -32,13 +32,20 @@ function bb_xnpn_configuration_page() {
 	<form class="options" method="post" action="">
 		<fieldset style="margin:2px; padding:12px 6px; border:1px solid #ccc; "><legend><?php _e("Email receiving notifications",'xnpn'); ?></legend>
 			<label for="xnpn_email">
-				<?php _e('Email address:') ?>
+				<?php _e('Email address:','xnpn'); ?>
 			</label>
 			
 				<input class="text" name="xnpn_email" id="xnpn_email" value="<?php echo $xnpnemail;  ?>" />
 			
 		
 		<p>&nbsp;</p>
+			<label for="xnpn_email_content">
+				<?php _e('Add content in email:','xnpn'); 
+				if('' != bb_get_option('xnpn_email_content')) $check = 'checked="checked"'; ?>
+				
+				<input id="xnpn_email_content" name="xnpn_email_content" type="checkbox" value="addcontent" <?php echo $check; ?>  />
+			</label>
+			<p>&nbsp;</p>
 		<p><?php 
 		if ('' == bb_get_option('xnpn_email')) {
 			_e('This email <b>(admin default to change)</b> will receive ID of added posts','xnpn');
@@ -80,13 +87,19 @@ function bb_xnpn_configuration_page_process() {
 			bb_delete_option( 'xnpn_email' );
 		}
 		
+		if ($_POST['xnpn_email_content']) {
+			bb_update_option( 'xnpn_email_content', $_POST['xnpn_email_content'] );
+		} else {
+			bb_delete_option( 'xnpn_email_content' );
+		}
+		// next version - all settings in array !
 		$goback = add_query_arg('xnpn-updated', 'true', wp_get_referer());
 		bb_safe_redirect($goback);
 		exit;
 	}
 	
 	if ($_GET['xnpn-updated']) {
-		bb_admin_notice( __( '<strong>Notification email saved.</strong>','xnpn') );
+		bb_admin_notice( __( '<strong>Notification email settings saved.</strong>','xnpn') );
 	}
 }
 
@@ -94,18 +107,21 @@ function bb_xnpn_configuration_page_process() {
 
 
  
-function admin_notification_new_post() {
-	global $bbdb, $topic_id, $bb_current_user;
+function admin_notification_new_post($post_id = 0) { // thanks Markus of comment suggest.
+	global $bbdb, $topic_id, $bb_current_user ;
 	
 	$topic = get_topic($topic_id);
 	
-	$header = 'From: '.bb_get_option('from_email')."\n"; // edit changed from admin_email to from_email
+	$header = 'From: '.bb_get_option('from_email')."\n"; 
 	$header .= 'MIME-Version: 1.0'."\n";
 	$header .= 'Content-Type: text/plain; charset="'.BBDB_CHARSET.'"'."\n";
 	$header .= 'Content-Transfer-Encoding: 7bit'."\n";
 	
 	$subject = __('There is a new post on: ','xnpn').$topic->topic_title;
 	$msg = __('Hello,','xnpn')."\n".get_user_name($bb_current_user->ID).__(' has posted here:','xnpn')."\n\n".get_topic_link($topic_id);
+	if ('' != bb_get_option('xnpn_email_content'))
+	 $msg .= "\n\n"."Content:"."\n\n".strip_tags(get_post_text($post_id)); // thanks Markus of comment suggest.
+	
 	if ('' == bb_get_option('xnpn_email')) {
 		$msg = __('Hi Keymaster, don\'t forget to visit New Post Notification settings in forum admin UI and save a right email.','xnpn').$msg;
 		bb_mail(bb_get_option('from_email'), $subject, $msg, $header);

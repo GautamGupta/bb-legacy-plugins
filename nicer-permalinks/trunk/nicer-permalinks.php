2ce6f86ab61ba0a09ec962e704b4ba33d0cafe8b
mr_pelle <mr_pelle@330c2647-7620-0410-81c2-8fc9ee8a0def>
2010-07-12 16:20:50 +0000

    updating trunk

--- nicer-permalinks/trunk/nicer-permalinks.php
+++ nicer-permalinks/trunk/nicer-permalinks.php
@@ -3,7 +3,7 @@
 Plugin Name: Nicer Permalinks
 Plugin URI: http://bbpress.org/plugins/topic/nicer-permalinks/
 Description: Rewrites every bbPress URI removing the words "forum" and "topic" and emphasizes hierarchy. Based on <a href="http://www.technospot.net/blogs/">Ashish Mohta</a> and <a href="http://markroberthenderson.com/">Mark R. Henderson</a>'s <a href="http://blog.markroberthenderson.com/getting-rid-of-forums-and-topic-from-bbpress-permalinks-updated-plugin/">Remove Forum Topic</a> plugin.
-Version: 4.1
+Version: 4.2
 Author: mr_pelle
 Author URI: mailto:francesco.pelle@gmail.com
 */
@@ -12,49 +12,34 @@ Author URI: mailto:francesco.pelle@gmail.com
  * @license CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
  */
 
+
 /**
  * Define constants
  */
 define( 'NICER_PERMALINKS_ID',   'nicer-permalinks' );
 define( 'NICER_PERMALINKS_NAME', 'Nicer Permalinks' );
 
+
 // Create text domain for translations
 bb_load_plugin_textdomain( NICER_PERMALINKS_ID, dirname( __FILE__ ) . '/languages' );
 
-/**
- * Global vars
- */
-
-/*
-$notices is an array of arrays used for feedback.
-
-To access its data:
-foreach ( $notices as $notice )
-
-$notice[0] is notice content (string).
-$notice[1] is notice class (string).
-
-e.g.
-( 'updated notice', '' )
-( 'error notice', 'error' )
-*/
-$notices = array();
 
 if ( bb_is_admin() ) // Load admin.php if on admin area
 	require_once( 'includes/admin.php' );
 
-if ( nicer_permalinks_enabled() ) { // Load plugin core if plugin is on
+if ( true === (bool) nicer_permalinks_enabled() ) { // Load plugin core if plugin is enabled
 	require_once( 'includes/nicer-filters.php' );
 
 	// Add plugin filters
-	add_filter( 'get_forum_link',           'get_nicer_forum_link_filter', 10, 2 );
-	add_filter( 'bb_get_forum_bread_crumb', 'bb_get_nicer_forum_bread_crumb_filter', 10, 2 );
-	add_filter( 'get_topic_link',           'get_nicer_topic_link_filter', 10, 2 );
-	add_filter( 'get_post_link',            'get_nicer_post_link_filter', 10, 3 );
+	add_filter( 'get_forum_link',           'get_forum_link_nicer_filter', 10, 2 );
+	add_filter( 'bb_get_forum_bread_crumb', 'bb_get_forum_bread_crumb_nicer_filter', 10, 2 );
+	add_filter( 'get_topic_link',           'get_topic_link_nicer_filter', 10, 2 );
+	add_filter( 'get_post_link',            'get_post_link_nicer_filter', 10, 3 );
 }
 
-// Run function on plugin deactivation
-bb_register_plugin_deactivation_hook( __FILE__, 'nicer_permalinks_deactivate' );
+// Call uninstaller on plugin deactivation
+bb_register_plugin_deactivation_hook( __FILE__, 'nicer_permalinks_uninstall' );
+
 
 /**
  * Functions
@@ -65,10 +50,8 @@ bb_register_plugin_deactivation_hook( __FILE__, 'nicer_permalinks_deactivate' );
  *
  * @uses bb_get_option()
  *
- * @return int
+ * @return boolean
  */
 function nicer_permalinks_enabled() {
-	$status = (string) bb_get_option( 'nicer_permalinks_enabled' );
-
-	return ( '1' == $status ) ? 1 : 0;
+	return (bool) bb_get_option( 'nicer_permalinks_enabled' );
 }

cb5f2910d913ab6c78d967831adbcc090ec51a5e
_ck_ <_ck_@330c2647-7620-0410-81c2-8fc9ee8a0def>
2010-07-20 12:52:03 +0000

    0.0.4 updated to support additional cookie changes in WordPress 2.8, 2.9, 3.0

--- freshly-baked-cookies/trunk/_freshly-baked-cookies.php
+++ freshly-baked-cookies/trunk/_freshly-baked-cookies.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name:  Freshly Baked Cookies for bbPress
-Description:  Allows bbPress 0.9 to use WordPress 2.7 or 2.8 cookies during stand-alone (simple) integration.
+Description:  Allows bbPress 0.9 to use WordPress 2.6, 2.7 or 2.8, 2.9, 3.0 cookies during stand-alone (simple) integration.
 Plugin URI:  http://bbpress.org/plugins/topic/freshly-baked-cookies
 Author: _ck_
 Author URI: http://bbshowcase.org
-Version: 0.0.3
+Version: 0.0.4
 */ 
 
 define('LOGGED_IN_KEY',   'replace this with the same key from WP');	// get this from wp-config.php
@@ -17,11 +17,15 @@ define('AUTH_SALT', 'replace this from WP options');				// get this from http://
 
 /*   stop editing here   */
 
+define('AUTH_COOKIE_VERSION', 2 );						//  Version 1 is for WordPress 2.6-2.7,  Version 2 is for Wordpress 2.8-3.0
 define('COOKIEHASH', md5(rtrim($bb->wp_siteurl, "/")));			// if you set a custom COOKIEHASH for some reason, you'll have to edit this
 define('LOGGED_IN_COOKIE', 'wordpress_logged_in_' . COOKIEHASH);	// you only have to edit this if you changed WordPress's login cookie name
 define('AUTH_COOKIE', 'wordpress_' . COOKIEHASH);				// you only have to edit this if you changed WordPress's auth cookie name
 
+
 /*   seriously, stop editing here   */
+
+function fbc_frag(& $user) {if (AUTH_COOKIE_VERSION >1 && !empty($user->user_pass)) {return substr( $user->user_pass, 8, 4 );} else {return '';}}
 
 function wp_validate_auth_cookie( $cookie = null, $scheme = 'logged_in_cookie' ) {	
 	
@@ -38,16 +42,16 @@ function wp_validate_auth_cookie( $cookie = null, $scheme = 'logged_in_cookie' )
 
 	if ( $expired < time() ) {return false;}
 	
-	$data = $username . '|' . $expiration;
-	$salt = apply_filters('salt', LOGGED_IN_KEY . LOGGED_IN_SALT, $scheme);		
-	$key=hash_hmac('md5', $data, $salt);
-	$hash = hash_hmac('md5', $username . '|' . $expiration, $key);
-	
-	if ( $hmac != $hash ) {return false;}
-	
 	$user = bb_get_user_by_name($username);
 	if ( !$user ) {return $user;}
-
+	
+	$data = $username . fbc_frag($user) . '|' . $expiration;
+	$salt = apply_filters('salt', LOGGED_IN_KEY . LOGGED_IN_SALT, $scheme);		
+	$key = hash_hmac('md5', $data, $salt);
+	$hash = hash_hmac('md5', $username . '|' . $expiration, $key);
+	
+	if ( $hmac != $hash ) {return false;}	
+	
 	return $user->ID;
 }
 
@@ -55,9 +59,9 @@ function wp_generate_auth_cookie( $user_id, $expiration, $scheme = 'logged_in' )
 	$user = bb_get_user( $user_id ); 
 	if (!$user) {return $user;}
 
-	$data = $user->user_login . '|' . $expiration;	
+	$data = $user->user_login . fbc_frag($user) . '|' . $expiration;	
 	$salt = ( $scheme == 'logged_in') ? apply_filters('salt', LOGGED_IN_KEY . LOGGED_IN_SALT, $scheme) : apply_filters('salt', AUTH_KEY . AUTH_SALT, $scheme);
-	$key=hash_hmac('md5', $data, $salt);
+	$key = hash_hmac('md5', $data, $salt);
 	$hash = hash_hmac('md5', $user->user_login . '|' . $expiration, $key);
 	$cookie = $user->user_login . '|' . $expiration . '|' . $hash;
 
@@ -75,11 +79,11 @@ function wp_set_auth_cookie($user_id, $remember = false) {
 	do_action('set_auth_cookie', $cookie, $expire);
 	do_action('set_logged_in_cookie', $cookie, $expire, $expiration, $user_id, 'logged_in');
 	
-	setcookie(LOGGED_IN_COOKIE, $cookie, $expire, $bb->cookiepath, $bb->cookiedomain);		// . '; HttpOnly'
+	setcookie(LOGGED_IN_COOKIE, $cookie, $expire, $bb->cookiepath, $bb->cookiedomain . '; HttpOnly');
 	setcookie(AUTH_COOKIE, $auth_cookie, $expire, $bb->cookiepath .'wp-admin', $bb->cookiedomain);
 	setcookie(AUTH_COOKIE, $auth_cookie, $expire, $bb->cookiepath .'wp-content/plugins', $bb->cookiedomain);
 	if ( $bb->cookiepath != $bb->sitecookiepath ) {
-		setcookie(LOGGED_IN_COOKIE, $cookie, $expire, $bb->sitecookiepath, $bb->cookiedomain);
+		setcookie(LOGGED_IN_COOKIE, $cookie, $expire, $bb->sitecookiepath, $bb->cookiedomain . '; HttpOnly');
 		setcookie(AUTH_COOKIE, $auth_cookie, $expire, $bb->sitecookiepath .'wp-admin', $bb->cookiedomain);
 		setcookie(AUTH_COOKIE, $auth_cookie, $expire, $bb->sitecookiepath .'wp-content/plugins', $bb->cookiedomain);
 	}
